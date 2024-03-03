68489949018852fbf11c55e676e33eee3f1ca81d
bally <bally.lomibao@howickltd.com>
2024-03-04 11:45:07 +1300

    Add animation components

---
+++ src/component/animate/index.js
@@ -0,0 +1,12 @@
+export * from './variants'
+
+export { default as DialogAnimate } from './dialog-animate'
+export { default as TextAnimate } from './text-animate'
+
+export { default as FabButtonAnimate } from './fab-button-animate'
+export { default as IconButtonAnimate } from './icon-button-animate'
+
+export { default as MotionViewport } from './motion-viewport'
+export { default as MotionContainer } from './motion-container'
+
+export { default as MotionLazyContainer } from './motion-lazy-container'
