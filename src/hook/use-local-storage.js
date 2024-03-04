fa99771587bed9cb5e5ca14bfbdbad2d4dcdf6e4
bally <bally.lomibao@howickltd.com>
2024-03-04 14:11:57 +1300

    Add use-responsive hook and refactor use-local-storage hook

--- src/hook/use-local-storage.js
+++ src/hook/use-local-storage.js
@@ -1,42 +1,43 @@
-import { useState, useEffect } from 'react';
-// utils
-import localStorageAvailable from '../utils/localStorageAvailable';
+import { useState, useEffect } from 'react'
+import { localStorageAvailable } from 'util'
 
 // ----------------------------------------------------------------------
 
-export default function useLocalStorage(key, defaultValue) {
-  const storageAvailable = localStorageAvailable();
+function useLocalStorage(key, defaultValue) {
+  const storageAvailable = localStorageAvailable()
 
   const [value, setValue] = useState(() => {
-    const storedValue = storageAvailable ? localStorage.getItem(key) : null;
+    const storedValue = storageAvailable ? localStorage.getItem(key) : null
 
-    return storedValue === null ? defaultValue : JSON.parse(storedValue);
-  });
+    return storedValue === null ? defaultValue : JSON.parse(storedValue)
+  })
 
   useEffect(() => {
     const listener = (e) => {
       if (e.storageArea === localStorage && e.key === key) {
-        setValue(e.newValue ? JSON.parse(e.newValue) : e.newValue);
+        setValue(e.newValue ? JSON.parse(e.newValue) : e.newValue)
       }
-    };
-    window.addEventListener('storage', listener);
+    }
+    window.addEventListener('storage', listener)
 
     return () => {
-      window.removeEventListener('storage', listener);
-    };
-  }, [key, defaultValue]);
+      window.removeEventListener('storage', listener)
+    }
+  }, [key, defaultValue])
 
   const setValueInLocalStorage = (newValue) => {
     setValue((currentValue) => {
-      const result = typeof newValue === 'function' ? newValue(currentValue) : newValue;
+      const result = typeof newValue === 'function' ? newValue(currentValue) : newValue
 
       if (storageAvailable) {
-        localStorage.setItem(key, JSON.stringify(result));
+        localStorage.setItem(key, JSON.stringify(result))
       }
 
-      return result;
-    });
-  };
+      return result
+    })
+  }
 
-  return [value, setValueInLocalStorage];
+  return [value, setValueInLocalStorage]
 }
+
+export default useLocalStorage
