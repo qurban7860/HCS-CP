5f1896b51c0efe7d714ccee8593027d5aa4ba95b
bally <bally.lomibao@howickltd.com>
2024-03-04 14:12:04 +1300

    Add compact layout components

---
+++ src/layout/compact/compact-layout.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from 'react-router-dom';
+// @mui
+import { Stack, Container } from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+export default function CompactLayout() {
+  
+  return (
+    <Container component="main">
+      <Stack
+        sx={{
+          py: 12,
+          m: 'auto',
+          maxWidth: 400,
+          minHeight: '100vh',
+          textAlign: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Outlet />
+      </Stack>
+    </Container>
+  );
+}
