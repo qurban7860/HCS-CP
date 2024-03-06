6946412e56a65511493b1a4628d66adc64f38cbd
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-06 15:50:51 +1300

    Remove unused imports and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-// i18n
 import 'locale/i18n'
-// map
 import 'util/mapboxgl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-// lazy image
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-// import { PersistGate } from 'redux-persist/lib/integration/react'
-// @mui
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import ErrorBoundary from 'util/error-boundary'
 import Router from 'route'
-// theme
 import ThemeProvider from 'theme'
-// locales
 import { ThemeLocalization } from 'locale'
-// components
-// import { StyledChart } from 'component/chart'
 import { SnackbarProvider } from 'component/snackbar'
 import { ScrollToTop } from 'component/scroll-to-top'
 import { MotionLazyContainer } from 'component/animate'
