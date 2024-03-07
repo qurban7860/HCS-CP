0147e40cabe4f41893c665239a590864b99cd5fd
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-08 08:01:56 +1300

    Refactor Logo component

--- src/component/branding/logo.jsx
+++ src/component/branding/logo.jsx
@@ -1,47 +1,43 @@
-import PropTypes from 'prop-types';
-import { forwardRef } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import PropTypes from 'prop-types'
+import { forwardRef } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 // @mui
 // import { useTheme } from '@mui/material/styles';
-import { Box, Link } from '@mui/material';
+import { Box, Link } from '@mui/material'
 
-// ----------------------------------------------------------------------
+const Logo = forwardRef(
+  ({ width = 150, disabledLink = false, sx, src = '/logo/logo.svg', ...other }, ref) => {
+    // const theme = useTheme();
 
-const Logo = forwardRef(({width = 150, disabledLink = false, sx, src="/logo/logo.svg", ...other }, ref) => {
-  // const theme = useTheme();
+    // const PRIMARY_LIGHT = theme.palette.primary.light;
 
-  // const PRIMARY_LIGHT = theme.palette.primary.light;
+    // const PRIMARY_MAIN = theme.palette.primary.main;
 
-  // const PRIMARY_MAIN = theme.palette.primary.main;
+    // const PRIMARY_DARK = theme.palette.primary.dark;
 
-  // const PRIMARY_DARK = theme.palette.primary.dark;
+    // OR using local (public folder)
+    // -------------------------------------------------------
+    const logo = (
+      <Box component="img" src={src} sx={{ width, height: 'auto', cursor: 'pointer', ...sx }} />
+    )
 
-  // OR using local (public folder)
-  // -------------------------------------------------------
-  const logo = (
-    <Box
-      component="img"
-      src={src}
-      sx={{ width, height: 'auto', cursor: 'pointer', ...sx }}
-    />
-  );
+    if (disabledLink) {
+      return logo
+    }
 
-  if (disabledLink) {
-    return logo;
+    return (
+      <Link component={RouterLink} to="/" sx={{ display: 'contents' }}>
+        {logo}
+      </Link>
+    )
   }
-
-  return (
-    <Link component={RouterLink} to="/" sx={{ display: 'contents' }}>
-      {logo}
-    </Link>
-  );
-});
+)
 
 Logo.propTypes = {
   sx: PropTypes.object,
   disabledLink: PropTypes.bool,
   src: PropTypes.string,
-  width: PropTypes.number
-};
+  width: PropTypes.number,
+}
 
-export default Logo;
+export default Logo
