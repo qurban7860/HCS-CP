77a0f0d708c5448a69e5b9abb85279ab77d013b9
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-08 14:55:18 +1300

    Refactor Logo component to use config values for width and src

--- src/component/logo/logo.jsx
+++ src/component/logo/logo.jsx
@@ -2,27 +2,16 @@ import { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { Box, Link } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
+import { ASSET, BRAND } from 'config'
 
 const Logo = forwardRef(
-  ({ width = 150, disabledLink = false, sx, src = '/logo/logo.svg', ...other }, ref) => {
-    // const theme = useTheme();
-
-    // const PRIMARY_LIGHT = theme.palette.primary.light;
-
-    // const PRIMARY_MAIN = theme.palette.primary.main;
-
-    // const PRIMARY_DARK = theme.palette.primary.dark;
-
-    // OR using local (public folder)
-    // -------------------------------------------------------
+  ({ width = BRAND.NAV_ICON, disabledLink = false, sx, src = ASSET.ICON, ...other }, ref) => {
     const logo = (
       <Box component="img" src={src} sx={{ width, height: 'auto', cursor: 'pointer', ...sx }} />
     )
-
     if (disabledLink) {
       return logo
     }
-
     return (
       <Link component={RouterLink} to="/" sx={{ display: 'contents' }}>
         {logo}
