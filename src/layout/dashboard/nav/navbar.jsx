e90c539e35beca4a8aaf0131e7da555d9d703ed9
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-12 11:06:04 +1300

    HPS 1102 Cleanup Nav components
    (cherry picked from commit a79538131905720ff12a2aad306561544cd7832f)

---
+++ src/layout/dashboard/nav/navbar.jsx
@@ -0,0 +1,48 @@
+import { memo } from 'react'
+import PropTypes from 'prop-types'
+import { useTheme } from '@mui/material/styles'
+import { AppBar, Box, Toolbar } from '@mui/material'
+import { HEADER } from 'config'
+import { bgBlur } from 'theme/style'
+
+function Navbar() {
+  const theme = useTheme()
+  return (
+    <AppBar
+      component="nav"
+      color="transparent"
+      sx={{
+        boxShadow: 0,
+      }}
+    >
+      <Shadow />
+    </AppBar>
+  )
+}
+
+export default memo(Navbar)
+
+Shadow.propTypes = {
+  sx: PropTypes.object,
+}
+
+function Shadow({ sx, ...other }) {
+  return (
+    <Box
+      sx={{
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 24,
+        zIndex: -1,
+        width: 1,
+        m: 'auto',
+        borderRadius: '50%',
+        position: 'absolute',
+        boxShadow: (theme) => theme.customShadow.z8,
+        ...sx,
+      }}
+      {...other}
+    />
+  )
+}
