a9382311218d6be422ad377a814b7be7b8e9c06b
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-12 11:02:45 +1300

    Add dynamic asset paths for Octicons and SVGs

--- src/config/asset-directory.js
+++ src/config/asset-directory.js
@@ -2,6 +2,8 @@ const ASSET = {
   BG_LOGO: '/assets/illustrations/illustration_bg_logo.svg',
   ICON: '/asset/brand/icon.svg',
   LOGO: '/asset/brand/logo.svg',
+  OCTICON: (icon) => `/asset/octicon/${icon}.svg`,
+  SVG: (icon) => `/asset/svg/${icon}.svg`,
 }
 
 export default ASSET
