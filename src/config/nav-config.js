5fadfad4d21245905f3c5efb5c1b582402cd1a14
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-12 11:03:05 +1300

    Add NavConfiguration component to handle navigation configuration

---
+++ src/config/nav-config.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+// import { ICON } from 'theme/icon'
+
+function NavConfiguration() {
+  const [navConfig, setNavConfig] = useState([
+    {
+      subheader: 'general',
+      items: [
+        { title: 'Customer', path: '/customer', children: null },
+        { title: 'Machine', path: '/machine', children: null },
+        { title: 'Document', path: '/document', children: null },
+        { title: 'Support', path: '/support', children: null },
+      ],
+    },
+  ])
+
+  return navConfig
+}
+
+export default NavConfiguration
