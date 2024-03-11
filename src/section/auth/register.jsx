55a357de3b3fbfa83ff5a45d4c127c32b920b548
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-11 15:40:52 +1300

    Refactor register.jsx component

--- src/section/auth/register.jsx
+++ src/section/auth/register.jsx
@@ -12,38 +12,20 @@ function Register() {
         <Typography
           sx={{
             backgroundColor: GLOBAL.VERSION_COLOR,
-            borderRadius: '2px',
             textAlign: 'center',
-            fontWeight: 'bold',
-            p: 1,
             py: 0.1,
             mb: 1,
           }}
         >
-          {GLOBAL.ENV}
-          {'  '}
-          {GLOBAL.VERSION}
+          &nbsp;
         </Typography>
       </Grid>
-      <Stack
-        spacing={2}
-        sx={{
-          mb: 2,
-          position: 'relative',
-          backgroundImage: (theme) =>
-            `linear-gradient(to right, ${theme.palette.primary.lighter} ,  white)`,
-          color: 'primary.contrastText',
-          p: 1,
-        }}
-      >
-        <Typography variant="h3">Sign Up for your account</Typography>
-      </Stack>
       <AuthRegisterForm />
       <Stack direction="row" spacing={0.5} sx={{ mt: 2, justifyContent: 'center' }}>
         <Typography variant="body2"> Already have an account? </Typography>
 
         <Link component={RouterLink} to={PATH_AUTH.login} variant="subtitle">
-          Sign in here
+          Log in here
         </Link>
       </Stack>
       <Typography
