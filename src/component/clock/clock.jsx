3f511c57c09f4330acce4f2060d43037be283adb
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-12 13:31:38 +1300

    Work on Clock component

---
+++ src/component/clock/clock.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import { Typography, Box } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import { StyledClockBox, StyledBoxFlex } from 'theme/style'
+import { RADIUS, TIME } from 'config'
+import { KEY, LOCALE, TIMEZONE } from 'constant'
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date())
+  const [aucklandTime, setAucklandTime] = useState(
+    new Date().toLocaleString(LOCALE.en, TIMEZONE.AUCKLAND)
+  )
+  const [timeZone, setTimeZone] = useState(Intl.DateTimeFormat().resolvedOptions().timeZone)
+  const theme = useTheme()
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(timerID)
+    }
+  }, [])
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setAucklandTime(new Date().toLocaleString(LOCALE.en, TIMEZONE.AUCKLAND))
+    }, 1000)
+    return () => {
+      clearInterval(timerID)
+    }
+  }, [])
+
+  return (
+    <StyledBoxFlex gap={2}>
+      <StyledClockBox sx={RADIUS.BORDER}>
+        <span>{timeZone}: &nbsp;</span> <Typography variant="h6">{TIME.DAY_CLOCK(time)}</Typography>
+      </StyledClockBox>
+      <StyledClockBox sx={RADIUS.BORDER}>
+        <span>{KEY.AUCKLAND}: &nbsp;</span>
+        <Typography variant="h6">{TIME.DAY_CLOCK(new Date(aucklandTime))}</Typography>
+      </StyledClockBox>
+    </StyledBoxFlex>
+  )
+}
+
+export default Clock
