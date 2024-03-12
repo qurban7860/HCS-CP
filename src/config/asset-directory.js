0186734d8775d91cafe93b646532d8668d9fa920
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-12 15:55:44 +1300

    Refactor asset directory paths to use helper functions

--- src/config/asset-directory.js
+++ src/config/asset-directory.js
@@ -1,6 +1,28 @@
+function _background(image) {
+  return `/asset/background/${image}`
+}
+
+function _brand(image) {
+  return `/asset/brand/${image}`
+}
+
+function _sponsor(image) {
+  return `/asset/other/${image}`
+}
+
 const ASSET = {
-  BG_LOGO: '/asset/background/howick-logo-2.svg',
-  ICON: '/asset/brand/icon.svg',
+  BG_LOGO: _background('howick-logo-2.svg'),
+  BRAND_LOGO_FULL: _brand('logo.svg'),
+  COMPANY_HOWICK: _sponsor('howick.png'),
+  COMPANY_RFS: _sponsor('rfs.png'),
+  COMPANY_CFS: _sponsor('cfs.svg'),
+  EVENT_STEEL_HORIZONS: _sponsor('steelhorizons.png'),
+  BRAND_TIPABIN: _sponsor('tipabin.png'),
+  BRAND_FRAMA: _sponsor('frama.svg'),
+  BRAND_XCALIBR: _sponsor('xcalibr.png'),
+  BRAND_XTENDA: _sponsor('xtenda.png'),
+  BRAND_SPEEDFLOOR: _sponsor('speedfloor.png'),
+  ICON: _brand('icon.svg'),
   LOGO: '/asset/brand/logo.svg',
   OCTICON: (icon) => `/asset/octicon/${icon}.svg`,
   SVG: (icon) => `/asset/svg/${icon}.svg`,
