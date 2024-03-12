a7f68d0384a7f1d6a22b7053e410ad975cce73fb
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-13 11:53:01 +1300

    Add brand constants for products, companies, and events

---
+++ src/constant/brand.js
@@ -0,0 +1,17 @@
+import { ASSET } from 'config'
+
+export const PRODUCT = [
+  { name: 'frama', image: ASSET.BRAND_FRAMA },
+  { name: 'xcalibr', image: ASSET.BRAND_XCALIBR },
+  { name: 'xtenda', image: ASSET.BRAND_XTENDA },
+  { name: 'speedfloor', image: ASSET.BRAND_SPEEDFLOOR },
+  { name: 'tipabin', image: ASSET.BRAND_TIPABIN_2 },
+]
+
+export const COMPANY = [
+  { name: 'howick', image: ASSET.COMPANY_HOWICK },
+  { name: 'rfs', image: ASSET.COMPANY_RFS },
+  { name: 'cfs', image: ASSET.COMPANY_CFS },
+]
+
+export const EVENT = [{ name: 'steelhorizons', image: ASSET.EVENT_STEEL_HORIZONS }]
