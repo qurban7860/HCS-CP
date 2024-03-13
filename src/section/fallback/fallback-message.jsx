269a2c2773b4f8b2a4fef5cb9bad4d544e4472d2
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-14 09:40:30 +1300

    Add error code to fallback message component

--- src/section/fallback/fallback-message.jsx
+++ src/section/fallback/fallback-message.jsx
@@ -1,23 +1,42 @@
 import { m } from 'framer-motion'
 import PropTypes from 'prop-types'
-import { Typography } from '@mui/material'
+import { Typography, Grid } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+import { LABEL } from 'constant'
 
-export const FallbackMessage = ({ value }) => {
+const FallbackMessage = ({ value, code }) => {
   const theme = useTheme()
   return (
     <m.div>
       <Typography
         variant="p"
-        sx={{ color: theme.palette.grey[500], fontSize: 16, p: 19, pt: 0, pb: 2 }}
+        sx={{ color: theme.palette.grey[500], fontSize: 16, p: 19, pt: 0, pb: 1 }}
         paragraph
       >
         {value.message}
       </Typography>
+      <Grid
+        container
+        sx={{
+          display: 'flex',
+          alignItems: 'right',
+          justifyContent: 'flex-end',
+          pb: 2,
+        }}
+      >
+        <Grid item lg={3}>
+          <Typography variant="overline">
+            {LABEL.ERROR_CODE} {code}
+          </Typography>
+        </Grid>
+      </Grid>
     </m.div>
   )
 }
 
 FallbackMessage.propTypes = {
   value: PropTypes.object,
+  code: PropTypes.number,
 }
+
+export default FallbackMessage
