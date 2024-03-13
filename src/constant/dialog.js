08ee3791536605b902114e82aa01e83c6f245be8
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-13 15:39:55 +1300

    Add fallback dialog for page not found

--- src/constant/dialog.js
+++ src/constant/dialog.js
@@ -16,6 +16,13 @@ const DIALOG = {
     content: 'Are you sure you want to Delete?',
     title: 'Delete',
   },
+
+  // fallback dialogs
+  NOT_FOUND: {
+    title: 'Ooppps... page not found',
+    message:
+      "Sorry, we couldn't find the page you're looking for. Perhaps you've mistyped the URL? Be sure to check your spelling",
+  },
 }
 
 export default DIALOG
