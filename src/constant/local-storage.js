5c8598bdcdcfc0a812af1cb15f4e224859d460b6
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-14 09:38:23 +1300

    Add local storage keys constant file

---
+++ src/constant/local-storage.js
@@ -0,0 +1,11 @@
+/**
+ * @constant LOCAL_STORAGE_KEY
+ * @description - local storage keys used in the applcation are stored here
+
+ */
+const LOCAL_STORAGE_KEY = {
+  ERROR_PAGE: 'ERROR-PAGES',
+  CONFIGURATION: 'configurations',
+}
+
+export default LOCAL_STORAGE_KEY
