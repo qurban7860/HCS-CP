4619c4c81056feae045c24868c5d3b08b5747959
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-15 11:02:34 +1300

    Delete customer-style.jsx, document-style.jsx, and machine-style.jsx

--- src/theme/style/machine-style.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material/styles';
-import { Card } from '@mui/material';
-
-// @root - Machine - settings
-export const StyledSettingsCardContainer = styled(Card)(({ theme }) => ({
-  margin: '1rem 0',
-  padding: '1rem 0',
-  color: theme.palette.text.disabled,
-}));
