6013a667d5ded728394eaa0cda10b1a5be0ac3e6
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-15 10:59:04 +1300

    Add dark mode logo support

--- src/component/logo/logo-icon.jsx
+++ src/component/logo/logo-icon.jsx
@@ -2,19 +2,28 @@ import { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { Box, Link } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
+import { useSettingContext } from 'component/setting'
+import { useIcon } from 'component/iconify'
 import { ASSET, BRAND } from 'config'
+import { KEY } from 'constant'
 
 const LogoIcon = forwardRef(
   ({ width = BRAND.NAV_ICON, disabledLink = false, sx, src = ASSET.ICON, ...other }, ref) => {
+    const { themeMode } = useSettingContext()
+    const { Icon: LogoIcon, iconSrc } = useIcon('HOWICK_LOGO')
+
     const logo = (
       <Box component="img" src={src} sx={{ width, height: 'auto', cursor: 'pointer', ...sx }} />
     )
+
+    const logoDark = <LogoIcon icon={iconSrc} alt="howick-logo" color="#FFF" p={1} m={1} />
+
     if (disabledLink) {
       return logo
     }
     return (
       <Link component={RouterLink} to="/" sx={{ display: 'contents' }}>
-        {logo}
+        {themeMode === KEY.LIGHT ? logo : logoDark}
       </Link>
     )
   }
