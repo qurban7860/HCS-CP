58a6f280fcb3c81194c6341bdeff6bb165a69683
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-15 11:02:21 +1300

    Update SvgColor src to icon in octicon.jsx

--- src/theme/icon/octicon.jsx
+++ src/theme/icon/octicon.jsx
@@ -3,7 +3,7 @@ import { SvgColor } from 'component/svg-color'
 import { Iconify } from 'component/iconify'
 
 export const octicon = (icon) => (
-  <SvgColor src={`/asset/octicon/${icon}/`} alt={icon} sx={{ width: 1, height: 1 }} />
+  <SvgColor icon={`/asset/octicon/${icon}/`} alt={icon} sx={{ width: 1, height: 1 }} />
 )
 
 export const ICON = {
