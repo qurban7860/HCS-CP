c897462cf6fff6f7f8bc36cdf1ca20c6bb5373e5
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-18 09:57:26 +1300

    Refactor ThemeSettings component

--- src/component/setting/theme-setting.jsx
+++ src/component/setting/theme-setting.jsx
@@ -1,22 +1,10 @@
 import PropTypes from 'prop-types'
 import ThemeContrast from './theme-contrast'
-import ThemeRtlLayout from './theme-rtl-layout'
-import ThemeColorPresets from './theme-color-preset'
-import SettingsDrawer from './drawer'
 
 ThemeSettings.propTypes = {
   children: PropTypes.node,
 }
 
 export default function ThemeSettings({ children }) {
-  return (
-    <ThemeColorPresets>
-      <ThemeContrast>
-        <ThemeRtlLayout>
-          {children}
-          <SettingsDrawer />
-        </ThemeRtlLayout>
-      </ThemeContrast>
-    </ThemeColorPresets>
-  )
+  return <ThemeContrast>{children}</ThemeContrast>
 }
