64494c35f4b3708cc6222bdbc2995b9b600161c8
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-18 09:58:06 +1300

    Add fullscreen labels

--- src/constant/label.js
+++ src/constant/label.js
@@ -2,6 +2,8 @@ const LABEL = {
   OUR_PRODUCT: 'OUR PRODUCTS: ',
   OUR_COMPANY: 'OUR COMPANIES: ',
   ERROR_CODE: 'Error Code:',
+  FULLSCREEN: 'Fullscreen',
+  FULLSCREEN_EXIT: 'Exit Fullscreen',
 }
 
 export default LABEL
