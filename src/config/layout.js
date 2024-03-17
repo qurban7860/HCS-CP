7ca2c9b7407a794493fdc5d768ea280cc479ccc3
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-18 09:57:54 +1300

    Update icon directory in config file

--- src/config/layout.js
+++ src/config/layout.js
@@ -20,6 +20,10 @@ export const NAV = {
   H_DASHBOARD_ITEM_SUB: 36,
 
   H_DASHBOARD_ITEM_HORIZONTAL: 25,
+  // popover
+  SPACING: 2,
+  H_OPTION: 50,
+  H_ACCOUNT_POPOVER: 380,
 }
 
 export const ICON = {
@@ -40,6 +44,6 @@ export const BRAND = {
 
 export const RADIUS = {
   BORDER: {
-    borderRadius: 0.4,
+    borderRadius: 2,
   },
 }
