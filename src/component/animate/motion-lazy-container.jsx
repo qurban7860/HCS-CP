6a4ad2f9f0d78d5b4337d89d19bdbd2b077132e8
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-21 15:48:02 +1300

    Refactor MotionLazyContainer component

--- src/component/animate/motion-lazy-container.jsx
+++ src/component/animate/motion-lazy-container.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import { LazyMotion, m } from 'framer-motion'
+import { Box } from '@mui/material'
 
 // eslint-disable-next-line import/extensions
 const loadFeatures = () => import('./feature.js').then((res) => res.default)
@@ -11,7 +12,10 @@ MotionLazyContainer.propTypes = {
 export default function MotionLazyContainer({ children }) {
   return (
     <LazyMotion strict features={loadFeatures}>
-      <m.div style={{ height: '100%' }}> {children} </m.div>
+      {/* <m.div style={{ height: '100%' }}> {children} </m.div> */}
+      <Box component={m.div} style={{ height: '100vh' }}>
+        {children}
+      </Box>
     </LazyMotion>
   )
 }
