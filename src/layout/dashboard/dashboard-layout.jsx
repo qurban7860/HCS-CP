15d9d905250488c18ab66d1db03cdb64591b97e7
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-21 15:59:02 +1300

    Add Fragment and GStyledContainer to DashboardLayout

--- src/layout/dashboard/dashboard-layout.jsx
+++ src/layout/dashboard/dashboard-layout.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { useResponsive } from 'hook'
+import { GStyledContainer } from 'theme/style'
 import { useSettingContext } from 'component/setting'
 import { GLOBAL } from 'global'
 import Main from './main'
@@ -9,50 +10,20 @@ import Header from './header'
 import { Navbar } from './nav'
 
 function DashboardLayout() {
-  const { themeLayout } = useSettingContext()
-  const isDesktop = useResponsive('up', 'lg')
-  const [open, setOpen] = useState(false)
-
-  // NOTE: keep it implemented like this for now, just in case we implement layout switching
-  const isNavHorizontal = themeLayout === 'horizontal'
-
-  const handleOpen = () => {
-    setOpen(true)
-  }
-
-  const handleClose = () => {
-    setOpen(false)
-  }
-
-  const renderNavVertical = <Navbar openNav={open} onCloseNav={handleClose} />
-  const bgcolor = GLOBAL.VERSION_COLOR
-  if (isNavHorizontal) {
-    return (
-      <>
-        <Header onOpenNav={handleOpen} />
-        <Navbar />
-        <Main>
-          <Outlet />
-        </Main>
-      </>
-    )
-  }
+  const { themeMode } = useSettingContext()
+  // KEEP: for future use
+  // const isDesktop = useResponsive('up', 'lg')
 
   return (
-    <>
-      <Header onOpenNav={handleOpen} />
-      <Box
-        sx={{
-          display: { lg: 'flex' },
-          minHeight: { sm: 1 },
-        }}
-      >
+    <Fragment>
+      <Header />
+      <GStyledContainer maxWidth="xl" themeMode={themeMode}>
         <Navbar />
         <Main>
           <Outlet />
         </Main>
-      </Box>
-    </>
+      </GStyledContainer>
+    </Fragment>
   )
 }
 
