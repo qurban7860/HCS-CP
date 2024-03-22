dc399e6aa476e254a3a2f7d70beeb33cf3801850
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-22 14:44:55 +1300

    Update user email and add additional user details

--- src/_mock/user.js
+++ src/_mock/user.js
@@ -1,9 +1,22 @@
 const mockUser = [
   {
     id: 1,
-    email: 'software@howickltd.com',
+    email: 'johndoe@steelcompany.com',
+    displayName: 'John Doe',
+    organization: 'SteelCompany',
+    firstName: 'John',
+    lastName: 'Doe',
+    phone: '123-456-7890',
+    role: 'customer',
+    location: {
+      address: '123 Test Blvd.',
+      postalCode: '12345',
+      state: 'Lagos',
+      city: 'Testing City',
+      country: 'TS',
+    },
+    badge: 'https://cdn3.vectorstock.com/i/1000x1000/15/12/gold-arrow-chart-business-finance-logo-vector-14921512.jpg',
     photoURL: 'https://mighty.tools/mockmind-api/content/cartoon/11.jpg',
-    displayName: 'Howick Ltd.',
   },
 ]
 
