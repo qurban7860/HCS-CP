353569968c6900e794327a7dbd06de9e5ad73d21
ballyalley-o <bally.lomibao@howickltd.com>
2024-03-25 12:28:57 +1300

    🌈 style(adapted prettierrc code fromatting ):

--- src/config/global.js
+++ src/config/global.js
@@ -20,19 +20,19 @@ export const GLOBAL = {
   MESSAGE_LOGIN_USER: import.meta.env.VITE_MESSAGE_LOGIN_USER,
   GOOGLE_MAPS_API_KEY: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   IDLE_TIME: import.meta.env.VITE_IDLE_TIME,
-  COMPOSITE_TOOL_CONFIG_MAX_LENGTH: (import.meta.env.COMPOSITE_TOOL_CONFIG_MAX_LENGTH = 10),
+  COMPOSITE_TOOL_CONFIG_MAX_LENGTH: (import.meta.env.COMPOSITE_TOOL_CONFIG_MAX_LENGTH = 10)
 }
 
 export const HOST_API_KEY = import.meta.env.VITE_HOST_API_KEY || ''
 
 export const COGNITO_API = {
   userPoolId: import.meta.env.VITE_AWS_COGNITO_USER_POOL_ID,
-  clientId: import.meta.env.VITE_AWS_COGNITO_CLIENT_ID,
+  clientId: import.meta.env.VITE_AWS_COGNITO_CLIENT_ID
 }
 
 export const AUTH0_API = {
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  domain: import.meta.env.VITE_AUTH0_DOMAIN
 }
 
 export const MAP_API = import.meta.env.VITE_MAPBOX_API
