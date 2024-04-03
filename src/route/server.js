2a34c7ab8592bf9954b7223a2c2a681fca106f20
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-03 15:59:02 +1300

    Add API routes for user management

--- src/route/server.js
+++ src/route/server.js
@@ -7,10 +7,23 @@ function _url(...param) {
   return _baseUrl + '/' + conNex(...param)
 }
 
+/**
+ * @path {baseUrl}/api/{apiVersion}{PATH_SERVER}
+ */
 export const PATH_SERVER = {
-  // auth
+  // /security/getToken
   LOGIN: _url('security', 'getToken'),
+  // /security/refreshToken
   MFA: _url('security', 'multifactorverifyCode'),
+  // /security/register
   REGISTER: _url('security', 'register'),
-  LOGOUT: (userId) => _url('security', 'logout', userId)
+  // /security/logout/:userId
+  LOGOUT: (userId) => _url('security', 'logout', userId),
+  // /security/users
+  USER: {
+    // /
+    list: _url('security', 'users'),
+    // /:id
+    detail: (userId) => _url('security', 'users', userId)
+  }
 }
