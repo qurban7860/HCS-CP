450f2bdf53165d3d10a2f20239e1778cb9e88bd9
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-03 15:57:49 +1300

    Add AUTH_CONTEXT error message to RESPONSE constant

--- src/constant/response.js
+++ src/constant/response.js
@@ -1,6 +1,7 @@
 const RESPONSE = {
   error: {
-    REDUX_PERSIST: 'Error clearing persisted states:'
+    REDUX_PERSIST: 'Error clearing persisted states:',
+    AUTH_CONTEXT: 'useAuthContext context must be use inside AuthProvider'
   }
 }
 
