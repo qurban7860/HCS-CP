197c2e0253747187ceef3959c527bdda0e5d717e
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-05 07:33:06 +1300

    rtk: authActionSlice to handle login and logout actions

---
+++ src/store/slice/auth/endpoint.js
@@ -0,0 +1,22 @@
+import { apiSlice } from 'store/slice'
+import { PATH_SERVER } from 'route/server'
+
+export const authActionSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: PATH_SERVER.LOGIN,
+        method: 'POST',
+        body: data
+      })
+    }),
+    logout: builder.mutation({
+      query: (id) => ({
+        url: PATH_SERVER.LOGOUT(id),
+        method: 'POST'
+      })
+    })
+  })
+})
+
+export const { useLoginMutation, useLogoutMutation } = authActionSlice
