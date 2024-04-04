bab2c5ecd65ad4d8c0957b7ac650db7b411c3947
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-05 07:36:11 +1300

    HPS-1052 - RTK: exports for auth and security user endpoints

--- src/store/slice/index.js
+++ src/store/slice/index.js
@@ -1,2 +1,7 @@
 export { apiSlice } from './api'
+// export { default as authReducer } from './auth/auth'
 export { default as authReducer } from './auth/auth'
+export * from './auth/endpoint'
+
+// security/user
+export * from './security/user'
