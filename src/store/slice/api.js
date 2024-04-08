64dc2fd7aa8b9da68719686e4cc524ed5e3f9825
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-08 15:43:50 +1200

    HPS-1141: Update API configuration and add authorization header

--- src/store/slice/api.js
+++ src/store/slice/api.js
@@ -1,12 +1,26 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
-import { GLOBAL } from 'config'
+import { GLOBAL } from '../../config/global'
+import { getAuthToken } from 'auth/util.js'
 
-const baseQuery = fetchBaseQuery({ baseUrl: GLOBAL.SERVER_DEV_URL })
+const extFetchBase = (args, api, extraOptions, authToken) => {
+  const headers = {
+    Authorization: `Bearer ${authToken}`
+  }
+
+  return fetchBaseQuery({
+    baseUrl: GLOBAL.SERVER_URL,
+    prepareHeaders: (headers, { getState }) => {
+      return headers
+    }
+  })(args, api, {
+    ...extraOptions,
+    headers
+  })
+}
 
 const apiSlice = createApi({
-  baseQuery,
+  baseQuery: (args, api, extraOptions) => extFetchBase(args, api, extraOptions, getAuthToken()),
   tagTypes: ['Machine', 'User', 'Customer', 'Document'],
-
   endpoints: (builder) => ({})
 })
 
