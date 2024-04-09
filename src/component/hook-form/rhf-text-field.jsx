ddbd97a5a73b7617f4c898039d2b8e8dc5ca8585
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-10 08:44:55 +1200

    Refactor RHFTextField component to support theme mode

--- src/component/hook-form/rhf-text-field.jsx
+++ src/component/hook-form/rhf-text-field.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types'
 import { useFormContext, Controller } from 'react-hook-form'
 import { TextField } from '@mui/material'
+import { useSettingContext } from 'component/setting'
 
 RHFTextField.propTypes = {
   name: PropTypes.string,
   helperText: PropTypes.node,
-  Error: PropTypes.bool,
+  Error: PropTypes.bool
 }
 
 export default function RHFTextField({ name, helperText, Error, ...other }) {
   const { control } = useFormContext()
+  const { themeMode } = useSettingContext()
 
   return (
     <Controller
@@ -27,20 +29,35 @@ export default function RHFTextField({ name, helperText, Error, ...other }) {
             '& .MuiOutlinedInput-notchedOutline': {
               borderColor: 'grey.600',
               borderRadius: 0.4,
-              color: 'grey.500',
+              color: themeMode === 'light' ? 'grey.900' : 'grey.0'
             },
             '& .MuiInputBase-root': {
               '&:hover': {
                 '& .MuiOutlinedInput-notchedOutline': {
-                  borderColor: '#000',
+                  borderColor: 'common.white'
                 },
+                '& .MuiInputBase-input': {
+                  color: themeMode === 'light' ? 'grey.900' : 'grey.0'
+                }
+              },
+              // color of text when disabled
+              '& .MuiInputBase-input': {
+                color: themeMode === 'light' ? 'grey.900' : 'grey.0'
               },
               '&.Mui-focused': {
                 '& .MuiOutlinedInput-notchedOutline': {
-                  borderColor: 'primary.main',
-                },
+                  borderColor: 'primary.main'
+                }
               },
-            },
+              // disabled
+              '&.Mui-disabled': {
+                color: themeMode === 'light' ? 'grey.800' : 'grey.200',
+                '& .MuiOutlinedInput-notchedOutline': {
+                  borderColor: 'bronze.main'
+                },
+                '& .MuiInputBase-input': {}
+              }
+            }
           }}
         />
       )}
