fbe09ae6608e9a34b29935590d197d80a26993fd
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-10 15:27:32 +1200

    HPS-1145: Refactor fallback page component for under development icon

--- src/page/fallback/fallback.jsx
+++ src/page/fallback/fallback.jsx
@@ -1,10 +1,16 @@
 import { useMemo } from 'react'
 import PropTypes from 'prop-types'
-import { LogoGrayProps, GStyledFallbackWrapperGrid } from 'theme/style'
+import { useIcon, ICON_NAME } from 'hook'
+import { Box } from '@mui/material'
 import { MotionContainer } from 'component/animate'
 import { FallbackTitle, FallbackMessage, FallbackButton } from 'section/fallback'
 import { Logo } from 'component/logo'
-import { HTTP_CODE, LOCAL_STORAGE_KEY } from 'constant'
+import { LogoGrayProps, GStyledFallbackWrapperGrid, motionRelativeBoxOption, iconPropsOption } from 'theme/style'
+import { HTTP_CODE, LOCAL_STORAGE_KEY, FALLBACK } from 'constant'
+
+const {
+  UNDER_DEVELOPMENT: { code: CODE }
+} = FALLBACK
 
 const Fallback = ({ code, title, message }) => {
   const configurations = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY.CONFIGURATION))
@@ -16,11 +22,18 @@ const Fallback = ({ code, title, message }) => {
     }),
     [content?.value, content?.notes]
   )
+
+  const { Icon, iconSrc: trafficCone } = useIcon(ICON_NAME.TRAFFIC_CONE)
+  const isUnderDevelopment = code === CODE
+
   return (
     <MotionContainer>
       <GStyledFallbackWrapperGrid>
         <FallbackTitle value={defaultValue} />
-        <Logo sx={LogoGrayProps} />
+        <Box {...motionRelativeBoxOption}>
+          <Logo sx={LogoGrayProps} />
+          {isUnderDevelopment && <Icon icon={trafficCone} {...iconPropsOption} />}
+        </Box>
         <FallbackMessage value={defaultValue} code={code} />
         <FallbackButton />
       </GStyledFallbackWrapperGrid>
