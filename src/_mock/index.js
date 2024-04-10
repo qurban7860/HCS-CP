a12d106f46ce3ecd749dc69a5b2014d49090a53a
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-10 15:45:04 +1200

    HPS-1048: Add machine mock data

--- src/_mock/index.js
+++ src/_mock/index.js
@@ -1,1 +1,2 @@
 export { default as mockUser } from './user'
+export * from './machine'
---
+++ src/_mock/machine.js
@@ -0,0 +1,100 @@
+export const mockMachine = [
+  {
+    accountManager: [
+      {
+        _id: '6523dcd9ca0613008d12b71c',
+        name: 'John Doe'
+      }
+    ],
+    projectManager: [
+      {
+        _id: '6523dcd9ca0613008d12b71c',
+        name: 'John Doe'
+      }
+    ],
+    supportManager: [
+      {
+        _id: '6523dcd9ca0613008d12b71c',
+        name: 'John Doe'
+      }
+    ],
+
+    isActive: true,
+    _id: '647d694ae85ca661138042aa',
+    serialNo: '05522',
+    profile: {
+      default: {
+        _id: '647d694ae85ca661138042aa',
+        size: '41.30X89.00',
+        name: 'Swage'
+      },
+      sets: [
+        {
+          _id: '647d694ae85ca661138042aa',
+          size: '41.30X89.00',
+          name: 'Dimple'
+        }
+      ]
+    },
+    financialCompany: {
+      _id: '647d694ae85ca661138042aa',
+      name: 'Financing Company'
+    },
+    name: 'Machine Control Software',
+    createdAt: '2023-06-05T04:49:14.115Z',
+    customer: {
+      _id: '64f8087df924854f6174d812',
+      name: '090623_Max',
+      clientCode: '123213'
+    },
+    connectedMachine: {
+      _id: '647d694ae85ca661138042aa',
+      serialNo: '20193',
+      name: 'Machine Connection 1'
+    },
+    machineModel: {
+      _id: '647efab166431f8e4519697f',
+      name: 'FRAMA™ 3200',
+      category: '647eb5a867fcb181a3b51202'
+    },
+    machineConnections: [
+      {
+        _id: '647d694ae85ca661138042aa',
+        serialNo: '20192',
+        name: 'Machine Connection 1'
+      },
+      {
+        _id: '647d694ae85ca661138042ad',
+        serialNo: '20193',
+        name: 'Decoiler 5T'
+      }
+    ],
+    status: {
+      _id: '64b68269297db31ed2e0f45a',
+      name: '17_7',
+      slug: 'slug slug slug'
+    },
+    verifications: [
+      {
+        _id: '649331174783b3e621737b30',
+        verifiedBy: '64208c09ebad86149771a62e',
+        verifiedDate: '2023-06-21T17:19:19.123Z'
+      }
+    ],
+    installationDate: '2023-12-25T19:00:00.000Z',
+    shippingDate: '2023-12-18T19:00:00.000Z'
+  }
+]
+
+export const mockMachineConnections = [
+  {
+    _id: '647d694ae85ca661138042asda',
+    serialNo: '20194',
+    name: 'TIPABIN'
+  },
+  {
+    _id: '647d694ae85ca661138042ad',
+    serialNo: '20193',
+    name: 'Decoiler 5T'
+  }
+]
