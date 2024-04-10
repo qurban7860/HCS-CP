72deb87d61ac387471d0cf50216039cfb1a06adf
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-10 15:26:19 +1200

    Update home path in nav-config.js

--- src/config/nav-config.js
+++ src/config/nav-config.js
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-// import { ICON } from 'theme/icon'
 
 function NavConfiguration() {
   const [navConfig, setNavConfig] = useState([
     {
       subheader: 'general',
       items: [
-        { title: 'Home', path: '/customer', children: null },
+        { title: 'Home', path: '/dashboard/app', children: null },
         { title: 'Machine', path: '/product/machine', children: null },
         { title: 'Document', path: '/document', children: null },
         { title: 'Support', path: '/support', children: null }
