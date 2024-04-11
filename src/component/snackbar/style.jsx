010e63dcd2b1d13529d19decef2e341b3a591a1a
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-11 15:52:35 +1200

    HPS-1146: Style-1 Snackbar component styles

--- src/component/snackbar/style.jsx
+++ src/component/snackbar/style.jsx
@@ -1,4 +1,3 @@
-// @mui
 import { useTheme } from '@mui/material/styles'
 import { GlobalStyles } from '@mui/material'
 
@@ -19,18 +18,17 @@ export default function StyledNotistack() {
             borderRadius: theme.shape.borderRadius,
             color: isLight ? theme.palette.common.white : theme.palette.grey[800],
             backgroundColor: isLight ? theme.palette.grey[900] : theme.palette.common.white,
-            '&.SnackbarItem-variantSuccess, &.SnackbarItem-variantError, &.SnackbarItem-variantWarning, &.SnackbarItem-variantInfo':
-              {
-                color: theme.palette.text.primary,
-                backgroundColor: theme.palette.background.paper,
-              },
-            [theme.breakpoints.up('md')]: {
-              minWidth: 240,
+            '&.SnackbarItem-variantSuccess, &.SnackbarItem-variantError, &.SnackbarItem-variantWarning, &.SnackbarItem-variantInfo': {
+              color: theme.palette.text.primary,
+              backgroundColor: theme.palette.background.paper
             },
+            [theme.breakpoints.up('md')]: {
+              minWidth: 240
+            }
           },
           '.SnackbarItem-message': {
             padding: '0 !important',
-            fontWeight: theme.typography.fontWeightMedium,
+            fontWeight: theme.typography.fontWeightMedium
           },
           '.SnackbarItem-action': {
             marginRight: 0,
@@ -38,10 +36,10 @@ export default function StyledNotistack() {
 
             '& svg': {
               width: 20,
-              height: 20,
-            },
-          },
-        },
+              height: 20
+            }
+          }
+        }
       }}
     />
   )
