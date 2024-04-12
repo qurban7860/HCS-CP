aea4209cce3b79655196cc04bbf0eda6d5462c0c
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-12 12:24:40 +1200

    code styling, re: prettierrc

--- src/section/auth/auth-verify-code-form.jsx
+++ src/section/auth/auth-verify-code-form.jsx
@@ -5,7 +5,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { Stack, FormHelperText } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 import { PATH_DASHBOARD } from 'route/path'
-import { useSnackbar } from 'component/snackbar'
+import { useSnackbar } from 'hook'
 import FormProvider, { RHFCodes } from 'component/hook-form'
 
 function AuthVerifyCodeForm() {
@@ -19,7 +19,7 @@ function AuthVerifyCodeForm() {
     code3: Yup.string().required('Code is required'),
     code4: Yup.string().required('Code is required'),
     code5: Yup.string().required('Code is required'),
-    code6: Yup.string().required('Code is required'),
+    code6: Yup.string().required('Code is required')
   })
 
   const defaultValues = {
@@ -28,18 +28,18 @@ function AuthVerifyCodeForm() {
     code3: '',
     code4: '',
     code5: '',
-    code6: '',
+    code6: ''
   }
 
   const methods = useForm({
     mode: 'onChange',
     resolver: yupResolver(VerifyCodeSchema),
-    defaultValues,
+    defaultValues
   })
 
   const {
     handleSubmit,
-    formState: { isSubmitting, errors },
+    formState: { isSubmitting, errors }
   } = methods
 
   const onSubmit = async (data) => {
@@ -59,25 +59,13 @@ function AuthVerifyCodeForm() {
       <Stack spacing={3}>
         <RHFCodes keyName="code" inputs={['code1', 'code2', 'code3', 'code4', 'code5', 'code6']} />
 
-        {(!!errors.code1 ||
-          !!errors.code2 ||
-          !!errors.code3 ||
-          !!errors.code4 ||
-          !!errors.code5 ||
-          !!errors.code6) && (
+        {(!!errors.code1 || !!errors.code2 || !!errors.code3 || !!errors.code4 || !!errors.code5 || !!errors.code6) && (
           <FormHelperText error sx={{ px: 2 }}>
             Code is required
           </FormHelperText>
         )}
 
-        <LoadingButton
-          fullWidth
-          size="large"
-          type="submit"
-          variant="contained"
-          loading={isSubmitting}
-          sx={{ mt: 3 }}
-        >
+        <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={isSubmitting} sx={{ mt: 3 }}>
           Verify
         </LoadingButton>
       </Stack>
