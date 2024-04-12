f89e68b5a41f6e54fc86439d74b58c4e37f7852e
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-12 12:23:01 +1200

    HPS-1146: use-snack.jsx component

--- src/hook/use-snack/use-snack.jsx
+++ src/hook/use-snack/use-snack.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types'
 import { Fragment } from 'react'
-import { m, AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import { SnackbarProvider as NotistackProvider, useSnackbar } from 'notistack'
-import { alpha } from '@mui/material/styles'
-import { Box, Slide, IconButton } from '@mui/material'
+import { Slide, IconButton } from '@mui/material'
 import { useIcon, ICON_NAME } from 'hook'
-import { ICON_WEB_NAME } from 'config'
+import { ICON_WEB_NAME } from 'config/icon-directory'
 import { COLOR, KEY } from 'constant'
-import StyledNotistack from './style'
-import { Iconify } from 'component/iconify'
+import { StyledSnackContent, StyledSnackIconMDiv } from './style'
 
 SnackProvider.propTypes = {
   children: PropTypes.node
@@ -21,29 +19,31 @@ export default function SnackProvider({ children }) {
     closeSnackbar(key)
   }
 
-  const { Icon, iconSrc: closeIcon } = useIcon(ICON_NAME.CLOSE)
+  const { Icon, iconSrc: closeIcon } = useIcon(ICON_NAME.CHEVRON_RIGHT)
 
   return (
     <Fragment>
-      <StyledNotistack />
-
       <NotistackProvider
         dense
         maxSnack={5}
         preventDuplicate
         autoHideDuration={3000}
         TransitionComponent={Slide}
-        variant={COLOR.INFO}
         anchorOrigin={{ vertical: KEY.TOP, horizontal: KEY.CENTER }}
+        Components={{
+          default: StyledSnackContent,
+          warning: StyledSnackContent,
+          success: StyledSnackContent,
+          error: StyledSnackContent
+        }}
         iconVariant={{
           info: <SnackIcon icon={ICON_WEB_NAME.INFO} color={COLOR.INFO} />,
-          success: <SnackIcon icon={ICON_WEB_NAME.CHECK_CICLE_OUTLINE} color={COLOR.SUCCESS} />,
+          success: <SnackIcon icon={ICON_WEB_NAME.CHECK_CIRCLE} color={COLOR.SUCCESS} />,
           warning: <SnackIcon icon={ICON_WEB_NAME.WARNING} color={COLOR.WARNING} />,
-          error: <SnackIcon icon={ICON_WEB_NAME.ALERT_OUTLINE} color={COLOR.ERROR} />
+          error: <SnackIcon icon={ICON_WEB_NAME.ALERT_OCTAGON} color={COLOR.ERROR} />
         }}
-        // With close as default
         action={(key) => (
-          <IconButton size="small" onClick={onClose(key)} sx={{ p: 0.5 }}>
+          <IconButton size="small" onClick={onClose(key)}>
             <Icon icon={closeIcon} />
           </IconButton>
         )}>
@@ -53,32 +53,11 @@ export default function SnackProvider({ children }) {
   )
 }
 
-SnackIcon.propTypes = {
-  icon: PropTypes.string,
-  color: PropTypes.oneOf(Object.values(COLOR))
-}
-
 function SnackIcon({ icon, color }) {
   const { Icon, iconSrc } = useIcon(icon)
-
   return (
-    <m.div
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      transition={{ duration: 0.3 }}
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: `${color}.main`,
-        width: 40,
-        height: 40,
-        borderRadius: 12,
-        '& .MuiTypography-root': {
-          fontSize: '3rem'
-        }
-      }}>
+    <StyledSnackIconMDiv initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.3 }}>
       <Icon icon={iconSrc} width={18} />
-    </m.div>
+    </StyledSnackIconMDiv>
   )
 }
