6734072a1c35c9ce0d60d1e174f859f302edc118
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-26 13:39:49 +1200

    HPS-1163: Add constant.js file for machine header labels

---
+++ src/section/product/machines/constant.js
@@ -0,0 +1,10 @@
+export const HEADER = [
+  // { key: 'id', label: 'ID' },
+  { key: 'serialNo', label: 'Serial No' },
+  { key: 'name', label: 'Name' },
+  { key: 'model', label: 'Model' },
+  { key: 'installationDate', label: 'Installation Date' },
+  { key: 'shippingDate', label: 'Shipping Date' },
+  { key: 'status', label: 'Status' },
+  { key: 'isActive', label: 'Active' }
+]
