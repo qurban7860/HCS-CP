5061b0bec74193d2d5d4de68e813c68e13e6f20d
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-26 13:37:32 +1200

    Add search functionality to the application

--- src/component/index.js
+++ src/component/index.js
@@ -2,3 +2,6 @@
 export { default as ScrollToTop } from './scroll-to-top'
 // @breadcrumb
 export { default as Breadcrumb } from './breadcrumb'
+
+// @search
+export { default as SearchBox } from './search-box'
