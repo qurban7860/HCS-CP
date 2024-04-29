8748a5e273ab18b99a9aaca56779f0fd9571cb77
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-30 11:41:23 +1200

    Clean-up : Refactor machines-header.jsx to remove unused imports and use StyledHeaderTableCell component

--- src/section/product/machines/machines-header.jsx
+++ src/section/product/machines/machines-header.jsx
@@ -1,24 +1,15 @@
-import { TableHead, TableCell, TableRow } from '@mui/material'
+import { TableHead, TableRow } from '@mui/material'
+import { StyledHeaderTableCell } from './style'
 import { HEADER } from './constant'
-import { KEY } from 'constant'
 
 const MachineHeader = ({ mode }) => {
   return (
     <TableHead>
       <TableRow>
         {HEADER.map((item) => (
-          <TableCell
-            key={item.key}
-            sx={{
-              backgroundColor: 'gray.100',
-              color: mode === KEY.DARK ? 'common.white' : 'common.black',
-              margin: 0,
-              fontWeight: 'bold',
-              fontSize: '1.1rem',
-              padding: '0.5rem 1rem'
-            }}>
+          <StyledHeaderTableCell key={item.key} item={item} mode={mode}>
             {item.label}
-          </TableCell>
+          </StyledHeaderTableCell>
         ))}
       </TableRow>
     </TableHead>
