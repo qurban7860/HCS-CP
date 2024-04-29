73243b964646b1ffb5744441a010eb43c09b6522
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-29 14:39:43 +1200

    Refactor component imports in index.js

--- src/component/index.js
+++ src/component/index.js
@@ -1,7 +1,13 @@
 // @scroll to top
-export { default as ScrollToTop } from './scroll-to-top'
+export { default as ScrollToTop } from './scroll-to-top/scroll-to-top'
 // @breadcrumb
-export { default as Breadcrumb } from './breadcrumb'
-
+export { default as Breadcrumb } from './breadcrumb/breadcrumb'
+// @image
+export { default as Image } from './image/image'
 // @search
-export { default as SearchBox } from './search-box'
+export { default as SearchBox } from './search/search-box'
+// @default
+export { default as EmptyContent } from './default/empty-content'
+// @table
+export { default as TableNoData } from './table/no-data'
+export { default as TableSkeleton } from './table/skeleton'
