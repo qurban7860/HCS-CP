ee34d40aefa7ac5ccd666544165e42cba31791ab
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-29 14:50:02 +1200

    HPS-1164: TableSkeleton component in skeleton.jsx

--- src/component/table/skeleton.jsx
+++ src/component/table/skeleton.jsx
@@ -7,12 +7,13 @@ const TableSkeleton = ({ ...other }) => {
       <TableRow {...other}>
         <TableCell colSpan={12}>
           <Stack spacing={3} direction="row" alignItems="center">
-            <Skeleton variant="rectangular" width={40} height={40} sx={{ borderRadius: 1, flexShrink: 0 }} />
-            <Skeleton variant="text" width="100%" height={20} />
+            <Skeleton variant="text" width={100} height={20} />
+            <Skeleton variant="text" width="40%" height={20} />
             <Skeleton variant="text" width={160} height={20} />
             <Skeleton variant="text" width={160} height={20} />
             <Skeleton variant="text" width={160} height={20} />
             <Skeleton variant="text" width={160} height={20} />
+            <Skeleton variant="rounded" width={40} height={40} sx={{ flexShrink: 0 }} />
           </Stack>
         </TableCell>
       </TableRow>
