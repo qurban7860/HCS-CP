e50286879bda31604588ba23f861d3caee5ddd2a
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-30 11:41:48 +1200

    HPS-1163: [Clean-up] Refactor machines-pagination.jsx to use StyledTablePagination component and update labelRowsPerPage to use LABEL.ROWS constant

--- src/section/product/machines/machines-pagination.jsx
+++ src/section/product/machines/machines-pagination.jsx
@@ -2,7 +2,8 @@ import { memo } from 'react'
 import PropTypes from 'prop-types'
 import { m } from 'framer-motion'
 import { Box, TablePagination } from '@mui/material'
-import { KEY } from 'constant'
+import { StyledTablePagination } from './style'
+import { LABEL } from 'constant'
 
 const MachineListPagination = ({
   rowsPerPageOptions = [10, 20, 40, 50, 100],
@@ -19,55 +20,21 @@ const MachineListPagination = ({
       sx={{
         position: 'relative'
       }}>
-      <TablePagination
+      <StyledTablePagination
         count={data?.length ?? 0}
-        labelRowsPerPage="Rows:"
-        colSpan={2}
-        rowsPerPageOptions={rowsPerPageOptions}
         component={m.div}
+        colSpan={2}
+        data={data}
+        mode={mode}
         page={page}
+        labelRowsPerPage={LABEL.ROWS}
+        rowsPerPageOptions={rowsPerPageOptions}
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
         rowsPerPage={rowsPerPage}
         showLastButton
         showFirstButton
         {...other}
-        sx={{
-          '.MuiTablePagination-toolbar': {
-            height: '5px',
-            width: '!important 200px',
-            '& .MuiTablePagination-actions': {
-              '& .MuiIconButton-root': {
-                '&:first-of-type': {
-                  color: page <= 0 ? (mode === KEY.LIGHT ? 'grey.300' : 'grey.700') : mode === KEY.LIGHT ? 'grey.800' : 'howick.orange'
-                },
-                '&:nth-of-type(2)': {
-                  color: page <= 0 ? (mode === KEY.LIGHT ? 'grey.300' : 'grey.700') : mode === KEY.LIGHT ? 'grey.800' : 'howick.orange'
-                },
-                '&:nth-of-type(3)': {
-                  color:
-                    page === Math.ceil((data?.length ?? 0) / rowsPerPage) - 1
-                      ? mode === KEY.LIGHT
-                        ? 'grey.300'
-                        : 'grey.700'
-                      : mode === KEY.LIGHT
-                      ? 'grey.800'
-                      : 'howick.orange'
-                },
-                '&:last-of-type': {
-                  color:
-                    page === Math.ceil((data?.length ?? 0) / rowsPerPage) - 1
-                      ? mode === KEY.LIGHT
-                        ? 'grey.300'
-                        : 'grey.700'
-                      : mode === KEY.LIGHT
-                      ? 'grey.800'
-                      : 'howick.orange'
-                }
-              }
-            }
-          }
-        }}
       />
     </Box>
   )
