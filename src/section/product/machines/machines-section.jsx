e72ed3add9083d5a0bfd058ae9127aa2dde3a867
ballyalley-o <bally.lomibao@howickltd.com>
2024-04-29 14:41:14 +1200

    HPS-1165: pagination implementation and  MachineListSection component to use useTable hook for table functionality and add pagination component

--- src/section/product/machines/machines-section.jsx
+++ src/section/product/machines/machines-section.jsx
@@ -1,20 +1,38 @@
 import { useEffect, useState } from 'react'
-import { snack } from 'hook'
-import { Table, Typography, Grid } from '@mui/material'
+import { snack, useTable } from 'hook'
+import { Table, TableBody, Typography, Grid } from '@mui/material'
 import { GStyledTableHeaderBox } from 'theme/style'
 import { useGetAllMachineQuery } from 'store/slice'
 import { MotionLazyContainer } from 'component/animate'
 import { useSettingContext } from 'component/setting'
 import { SearchBox } from 'component/search'
+import { TableNoData, TableSkeleton } from 'component/table'
 import { MachineTable, MachineHeader, MachineListPagination } from 'section/product'
 import { MARGIN, ASSET } from 'config'
 import { COLOR, KEY, TITLE } from 'constant'
 
 const MachineListSection = () => {
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [tableData, setTableData] = useState([])
+
   const [searchTerm, setSearchTerm] = useState('')
   const { data: allMachineData, isLoading, isError, refetch: refetchAllMachine } = useGetAllMachineQuery()
 
   const { themeMode } = useSettingContext()
+
+  const {
+    order,
+    orderBy,
+    setPage: setTablePage,
+    selected,
+    onSelectRow,
+    onSelectAllRows,
+    onSort
+  } = useTable({
+    defaultOrderBy: 'createdAt',
+    defaultOrder: 'desc'
+  })
 
   useEffect(() => {
     if (isError) {
@@ -24,6 +42,7 @@ const MachineListSection = () => {
     } else {
       snack('Fetched All Machine Data', { variant: COLOR.SUCCESS })
       refetchAllMachine()
+      setTableData(allMachineData)
     }
     refetchAllMachine()
   }, [refetchAllMachine, allMachineData, isLoading, isError])
@@ -32,25 +51,49 @@ const MachineListSection = () => {
     setSearchTerm(event.target.value)
   }
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event) => {
+    setPage(0)
+    setRowsPerPage(parseInt(event.target.value, 10))
+  }
   const filteredData =
-    allMachineData &&
-    allMachineData.filter((row) => Object.values(row)?.some((value) => value?.toString().toLowerCase().includes(searchTerm.toLowerCase())))
+    tableData && tableData.filter((row) => Object.values(row)?.some((value) => value?.toString().toLowerCase().includes(searchTerm.toLowerCase())))
+  const isNotFound = !isLoading && !filteredData.length
 
   return (
     <MotionLazyContainer display="flex">
-      <Typography variant="h2">{TITLE.MACHINE_LIST}</Typography>
+      <Typography variant="h2" color="common.white">
+        {TITLE.MACHINE_LIST}
+      </Typography>
       <SearchBox term={searchTerm} mode={themeMode} handleSearch={handleSearch} />
       <Grid container flexDirection="row" {...MARGIN.PAGE_PROP}>
         <Grid item lg={12}>
           <Grid container mb={2}>
             <Grid item lg={12} sm={12} mb={2} bgcolor="background.paper">
               <GStyledTableHeaderBox bgcolor={themeMode === KEY.LIGHT ? 'success.main' : 'grey.800'} flex={1} px={2} pt={2} />
-              <MachineListPagination count={allMachineData ? allMachineData?.length : 0} page={1} />
+              <MachineListPagination
+                mode={themeMode}
+                data={filteredData}
+                page={page}
+                rowsPerPage={rowsPerPage}
+                handleChangePage={handleChangePage}
+                handleChangeRowsPerPage={handleChangeRowsPerPage}
+              />
               <Table>
                 <MachineHeader mode={themeMode} />
-                {filteredData?.map((machine) => (
-                  <MachineTable key={machine._id} machine={machine} mode={themeMode} />
-                ))}
+                {(isLoading ? [...Array(rowsPerPage)] : filteredData)
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, index) =>
+                    row ? (
+                      <MachineTable key={row._id} machine={row} mode={themeMode} />
+                    ) : (
+                      !isNotFound && <TableSkeleton key={index} sx={{ height: denseHeight }} />
+                    )
+                  )}
+                <TableNoData isNotFound={isNotFound} />
               </Table>
             </Grid>
           </Grid>
