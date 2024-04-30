dc7483deb37610f887e1ebeb9e4d0a643b996963
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-01 10:18:06 +1200

    HPS-1163 : Update machines-table.jsx to add link functionality to machine serial number

--- src/section/product/machines/machines-table.jsx
+++ src/section/product/machines/machines-table.jsx
@@ -1,25 +1,38 @@
 import { Fragment } from 'react'
 import { m } from 'framer-motion'
+import { useNavigate } from 'react-router-dom'
 import { ICON_NAME, useIcon } from 'hook'
 import { Box, TableBody, TableCell } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+import { LinkTableCell } from 'component/table-tool'
+import { PATH_MACHINE } from 'route/path'
 import { fDate } from 'util'
 import { KEY } from 'constant'
-import { StyledIconListItemText, StyledPopTableCell, StyledTableRow } from './style'
+import { StyledIconListItemText, StyledTableRow } from './style'
 
 const MachineTable = ({ machine, mode, index }) => {
   const theme = useTheme()
+  const navigate = useNavigate()
   const { Icon, iconSrc: activeSrc } = useIcon(ICON_NAME.ACTIVE)
   const { iconSrc: inactiveSrc } = useIcon(ICON_NAME.INACTIVE)
 
   const activeColor = mode === KEY.DARK ? theme.palette.howick.burnIn : theme.palette.burnIn.altDark
   const inactiveColor = theme.palette.howick.error
 
+  const handleOnClick = (id) => {
+    navigate(PATH_MACHINE.machines.view(id))
+  }
+
   return (
     <Fragment>
       <TableBody>
         <StyledTableRow index={index} mode={mode} item={machine}>
-          <StyledPopTableCell>{machine?.serialNo}</StyledPopTableCell>
+          <LinkTableCell
+            param={machine?.serialNo}
+            onClick={() => {
+              handleOnClick(machine?._id)
+            }}
+          />
           <TableCell>{machine?.name}</TableCell>
           <TableCell>
             <Box>{machine?.machineModel?.name}</Box>
