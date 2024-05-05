c1899deb3f4892c8cb1eca39c6e49a0688a59edc
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-06 09:08:31 +1200

    HPS-1210: feat: create audit-box.jsx and src/component/audit/index.js Add AuditBox component and related files

---
+++ src/component/audit/audit-box.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+import { Box, Typography } from '@mui/material'
+import { GStyledSpanBox } from 'theme/style'
+import { VARIANT, LABEL } from 'constant'
+import { fDate } from 'util/format'
+
+const { CREATED_BY, UPDATED_BY } = LABEL.AUDIT
+const { TYPOGRAPHY } = VARIANT
+
+const AuditBox = ({ value }) => {
+  const { createdAt, createdBy, updatedAt, updatedBy, createdIP, updatedIP } = value
+
+  return (
+    <Box>
+      <GStyledSpanBox>
+        <Typography variant={TYPOGRAPHY.OVERLINE} color="grey.500">
+          {CREATED_BY}
+        </Typography>
+        <Typography variant={TYPOGRAPHY.BODY2} color="grey.500">
+          {createdBy && createdBy} {createdAt && `${' / ' + fDate(createdAt)}`} {createdIP && `/ ${createdIP}`}
+        </Typography>
+      </GStyledSpanBox>
+      <GStyledSpanBox>
+        <Typography variant={TYPOGRAPHY.OVERLINE} color="grey.500">
+          {UPDATED_BY}
+        </Typography>
+        <Typography variant={TYPOGRAPHY.BODY2} color="grey.500">
+          {updatedBy} {updatedAt && `${' / ' + fDate(updatedAt)}`} {updatedIP && `/ ${updatedIP}`}
+        </Typography>
+      </GStyledSpanBox>
+    </Box>
+  )
+}
+
+AuditBox.propTypes = {
+  value: PropTypes.object
+}
+
+export default AuditBox
