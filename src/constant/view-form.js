d5c7a2c9b36caf1a2774f7cc016b9a9479337c1c
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-06 15:51:21 +1200

    Add constants for view form fields

---
+++ src/constant/view-form.js
@@ -0,0 +1,45 @@
+/**
+ * @description: This file contains the constant heading values for the view form.
+ */
+
+export const VIEW_FORM = {
+  NAME: 'Name',
+  EMAIL: 'Email',
+  PHONE: 'Phone',
+  DESCRIPTION: 'Description',
+  ORGANIZATION: 'Organization',
+  STATUS: 'Status',
+
+  ADDRESS: {
+    ADDRESS: 'Address',
+    STREET: 'Street',
+    CITY: 'City',
+    STATE: 'State',
+    ZIP: 'Zip',
+    COUNTRY: 'Country'
+  },
+
+  MACHINE: {
+    SERIAL_NO: 'Serial Number',
+    SUPPLIER: 'Supplier',
+    MODEL: 'Machine Model',
+    DEFAULT_PROFILE: 'Default Profile',
+    FINANCING_COMPANY: 'Financing Company',
+    WORK_ORDER: 'Work Order / Purchase Order',
+    LANDMARK: 'Landmark for Installation Site',
+    SUPPLIER: 'Supplier',
+    SUPPORT_EXPIRATION: 'Support Expiration',
+    PURCHASE_DATE: 'Purchase Date',
+    MANUFACTURE_DATE: 'Manufacture Date',
+    SHIPPING_DATE: 'Shipping Date',
+    INSTALLATION_DATE: 'Installation Date',
+    BILLING_SITE: 'Billing Site',
+    INSTALLATION_SITE: 'Installation Site'
+  },
+
+  HOWICK_RESOURCES: {
+    ACCOUNT_MANAGER: 'Account Manager',
+    PROJECT_MANAGER: 'Project Manager',
+    SUPPORT_MANAGER: 'Support Manager'
+  }
+}
