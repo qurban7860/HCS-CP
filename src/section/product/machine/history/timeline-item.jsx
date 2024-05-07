4b818d36d2dd620d7be0f3460daa005f13b58ae8
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-07 13:29:15 +1200

    HPS-1214: feat: Update HistoryTimelineItem component to include status information

--- src/section/product/machine/history/timeline-item.jsx
+++ src/section/product/machine/history/timeline-item.jsx
@@ -7,7 +7,7 @@ import { GStyledSpanBox } from 'theme/style'
 
 const { TYPOGRAPHY } = VARIANT
 
-const HistoryTimelineItem = ({ date, icon, story, color, transferredTo }) => {
+const HistoryTimelineItem = ({ date, status, icon, story, color, transferredTo }) => {
   // const { Icon: IconTo, iconSrc } = useIcon(icon)
   const { Icon: RightIcon, iconSrc: rightSrc } = useIcon(ICON_NAME.CHEVRON_RIGHT)
 
@@ -21,25 +21,36 @@ const HistoryTimelineItem = ({ date, icon, story, color, transferredTo }) => {
     case ICON_NAME.TRANSFER:
       color = 'error.dark'
       break
+    case ICON_NAME.STATUS:
+      color = 'howick.orange'
+      break
     default:
       color = 'grey.300'
   }
 
   const isTransfer = icon === ICON_NAME.TRANSFER
   const isPurchased = icon === ICON_NAME.PURCHASED
-  const background = isTransfer ? 'error.main' : 'grey.500'
+  const isStatus = icon === ICON_NAME.STATUS
+  const background = isTransfer ? 'error.main' : isStatus ? 'howick.orange' : 'grey.500'
 
   return (
     <TimelineItem>
       <TimelineSeparator>
-        {!isPurchased && <TimelineConnector />}
+        {/* {!isPurchased && <TimelineConnector />} */}
+        {/* TODO: make collapsible, and display the latest status when collapsed
+
+            - we cannot mark it in the timeline unless we have a date.
+            - while working on that feature, we just display the latest status at the end of the timeline.
+
+         */}
         <TimelineDot sx={{ backgroundColor: background }} />
-        {!isTransfer && <TimelineConnector />}
+        {/* if not isTransfer or !isStatus  */}
+        {(!isStatus && <TimelineConnector />) || null}
       </TimelineSeparator>
       <TimelineContent align={KEY.LEFT}>
         <Box>
           <Typography variant={TYPOGRAPHY.OVERLINE1} fontWeight="bold" color="grey.500">
-            {story}
+            {story || status}
           </Typography>
           <Typography variant={TYPOGRAPHY.BODY1} fontWeight="bold" color="grey.700">
             {date}
@@ -49,6 +60,13 @@ const HistoryTimelineItem = ({ date, icon, story, color, transferredTo }) => {
               <RightIcon icon={rightSrc} color={color} />
               <Typography variant={TYPOGRAPHY.BODY2} color="grey.500">
                 {transferredTo || 'Unspecified'}
+              </Typography>
+            </GStyledSpanBox>
+          )}
+          {isStatus && (
+            <GStyledSpanBox>
+              <Typography variant={TYPOGRAPHY.BODY2} color="grey.500">
+                {status}
               </Typography>
             </GStyledSpanBox>
           )}
