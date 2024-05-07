cf17f99fd1c94e674c34751f0c315ffe08cbb579
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-07 13:30:06 +1200

    feat: Add styled components for site tabs in Machine component

--- src/section/product/machine/style.jsx
+++ src/section/product/machine/style.jsx
@@ -1,5 +1,5 @@
 import { styled } from '@mui/material/styles'
-import { Divider, ListItemText, Chip } from '@mui/material'
+import { Divider, ListItemText, Chip, Tabs, Tab, Box, alpha } from '@mui/material'
 import { KEY } from 'constant'
 import { ASSET } from 'config'
 
@@ -21,6 +21,35 @@ export const StyledStatusChip = styled(({ theme, isActive, ...other }) => <Chip 
   backgroundColor: isActive ? theme.palette.howick.burnIn : theme.palette.howick.error,
   color: isActive ? theme.palette.common.black : theme.palette.common.white,
   fontWeight: 'bold'
+}))
+
+export const StyledSiteTabs = styled(({ theme, mode, ...other }) => <Tabs {...other} />)(({ theme, mode }) => ({
+  '& .MuiTabs-root': {
+    padding: 0
+  },
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  '&:hover': {
+    color: '#40a9ff',
+    opacity: 1
+  }
+}))
+
+export const StyledSiteTabBox = styled(Box)(({ theme }) => ({
+  borderBottom: `${theme.spacing(0.05)} solid`,
+  borderColor: alpha(theme.palette.grey[500], 0.5)
+}))
+
+export const StyledSiteTab = styled(({ theme, mode, ...other }) => <Tab {...other} />)(({ theme, mode }) => ({
+  '&.Mui-selected': {
+    color: mode === KEY.LIGHT ? theme.palette.howick.darkBlue : theme.palette.howick.orange,
+    fontWeight: 'bold'
+  },
+  '&.Mui-focusVisible': {
+    backgroundColor: 'howick.darkBlue'
+  }
 }))
 
 export const CardOption = (mode) => {
