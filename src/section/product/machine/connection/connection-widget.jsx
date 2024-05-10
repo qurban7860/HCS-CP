3b76dcc424e1e6c6af4d28cf1f72e46e14185b18
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-10 12:10:58 +1200

    HPS-1223: MachineConnectionWidget component to improve code readability and add parent connection functionality

--- src/section/product/machine/connection/connection-widget.jsx
+++ src/section/product/machine/connection/connection-widget.jsx
@@ -1,9 +1,13 @@
-import { Fragment, useEffect, useState, useCallback } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import { useNavigate } from 'react-router-dom'
 import { useIcon, ICON_NAME } from 'hook'
-import { Grid, Typography } from '@mui/material'
+import { Grid, Typography, IconButton } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-import { FormHeader } from 'component'
+import { GStyledTooltip } from 'theme/style'
+import { useSettingContext } from 'component/setting'
+import { FormHeader, IconTooltip } from 'component'
+import { PATH_MACHINE } from 'route/path'
 import { GStyledCenterBox, GStyledListItemText, GStyledSpanBox } from 'theme/style'
 import { VARIANT, SIZE, LABEL, KEY, DECOILER, FILTER_TYPE } from 'constant'
 import { normalizer } from 'util/format'
@@ -16,6 +20,7 @@ const { THREE_TON } = FILTER_TYPE
 const MachineConnectionWidget = ({ value }) => {
   const [icon, setIcon] = useState(null)
   const theme = useTheme()
+  const { themeMode } = useSettingContext()
   const isActive = (status) => (status ? LABEL.ACTIVE : LABEL.INACTIVE)
 
   const isDecoiler1_5T = (machineName) => normalizer(machineName)?.includes(ONE_HALF_T)
@@ -45,6 +50,22 @@ const MachineConnectionWidget = ({ value }) => {
 
   useEffect(() => {
     const machineNames = value?.machineConnection?.map((mach) => mach?.connectedMachine?.name)
+    const parentMachineNames = value?.parentConnection?.map((mach) => mach?.connectedMachine?.name)
+
+    const isConnected = (mach) => Object.keys(mach).some((key) => mach[key])
+    const isParent = (mach) => Object.keys(mach).some((key) => mach[key])
+
+    const checkConnected = isConnected(value?.machineConnection)
+    const checkParent = isParent(value?.parentConnection)
+
+    // console.log('value', value)
+    // console.log('checkConnected', checkConnected)
+    // console.log('checkParent', checkParent)
+
+    if (checkParent) {
+      setIcon(ICON_NAME.PARENT)
+    }
+
     machineNames.forEach((machineName) => {
       if (checkDecoilerType(machineName) === THREE_T) {
         setIcon(ICON_NAME.DECOILER_3T)
@@ -77,8 +98,29 @@ const MachineConnectionWidget = ({ value }) => {
                       mach.connectedMachine &&
                       mach.connectedMachine.name && (
                         <GStyledSpanBox>
-                          {mach?.connectedMachine?.serialNo}
-                          <LocIcon icon={iconSrc} color={theme.palette.grey[500]} sx={{ height: 15 }} />
+                          <IconButton
+                            onClick={() => window.open(PATH_MACHINE.machines.view(mach.connectedMachine._id), KEY.BLANK)}
+                            size={SIZE.MEDIUM}
+                            color={themeMode === KEY.LIGHT ? 'grey.800' : 'common.white'}
+                            aria-label="view"
+                            target="_blank"
+                            sx={{
+                              padding: 0.5,
+                              borderRadius: 2,
+                              m: 0
+                            }}>
+                            <Typography variant={value?.machineConnection?.length > 1 ? TYPOGRAPHY.H4 : TYPOGRAPHY.H3}>
+                              {mach?.connectedMachine?.serialNo}
+                            </Typography>
+                          </IconButton>
+                          <GStyledTooltip
+                            title={LABEL.DECOILER_DEF}
+                            disableFocusListener
+                            placement={KEY.TOP}
+                            tooltipcolor={theme.palette.grey[500]}
+                            color={theme.palette.grey[500]}>
+                            <LocIcon icon={iconSrc} color={theme.palette.grey[500]} sx={{ height: 15 }} />
+                          </GStyledTooltip>
                         </GStyledSpanBox>
                       )
                     }
@@ -92,6 +134,55 @@ const MachineConnectionWidget = ({ value }) => {
                       size={SIZE.SMALL}
                       variant={VARIANT.OUTLINED}
                       isActive={mach?.isActive}
+                    />
+                  </GStyledCenterBox>
+                </Grid>
+              </Fragment>
+            ))
+          ) : value?.parentConnection?.length > 0 ? (
+            value?.parentConnection?.map((mach, index) => (
+              <Fragment key={index}>
+                <Grid item xs={8}>
+                  <GStyledListItemText
+                    primary={
+                      mach && (
+                        <GStyledSpanBox>
+                          <IconButton
+                            onClick={() => window.open(PATH_MACHINE.machines.view(mach.machine._id), KEY.BLANK)}
+                            size={SIZE.MEDIUM}
+                            color={themeMode === KEY.LIGHT ? 'grey.800' : 'common.white'}
+                            aria-label="view"
+                            target={KEY.BLANK}
+                            sx={{
+                              padding: 0.5,
+                              borderRadius: 2,
+                              m: 0
+                            }}>
+                            <Typography variant={value?.parentConnection?.length > 1 ? TYPOGRAPHY.H4 : TYPOGRAPHY.H3}>
+                              {mach?.machine?.serialNo}
+                            </Typography>
+                          </IconButton>
+
+                          <GStyledTooltip
+                            title={LABEL.PARENT}
+                            disableFocusListener
+                            placement={KEY.TOP}
+                            tooltipcolor={theme.palette.grey[500]}
+                            color={theme.palette.grey[500]}>
+                            <LocIcon icon={iconSrc} color={theme.palette.grey[500]} sx={{ height: 15 }} />
+                          </GStyledTooltip>
+                        </GStyledSpanBox>
+                      )
+                    }
+                  />
+                </Grid>
+                <Grid item xs={4} flex={1} justifyContent={KEY.CENTER} alignContent={KEY.CENTER}>
+                  <GStyledCenterBox>
+                    <StyledStatusChip
+                      label={<Typography variant={TYPOGRAPHY.H6}>{isActive(true)}</Typography>}
+                      size={SIZE.SMALL}
+                      variant={VARIANT.OUTLINED}
+                      isActive
                     />
                   </GStyledCenterBox>
                 </Grid>
