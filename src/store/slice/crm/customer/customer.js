ece7585c71a8830b8fda247df32606e8b0d74d06
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-13 12:21:05 +1200

    HPS-1222: Add customer endpoint and related queries/mutations
    This commit adds the customer endpoint and related queries/mutations to the CRM customer module. The endpoint is imported from the `store/slice/crm/customer/endpoint.js` file and the queries/mutations are injected using the `apiSlice.injectEndpoints` method. This change enhances the CRM functionality by providing the necessary API integration for retrieving, updating, and listing customer data.

--- src/store/slice/crm/customer/customer.js
+++ src/store/slice/crm/customer/customer.js
@@ -1,41 +1,199 @@
-import { apiSlice } from 'store/slice'
-import { PATH_SERVER } from 'route/server'
-import { getAuthToken } from 'auth/util.js'
-import { METHOD } from 'constant'
+import { createSlice } from '@reduxjs/toolkit'
+// utils
+import axios from 'util/axios'
+import { GLOBAL } from 'config/global'
 
-const { GET } = METHOD
+const initialState = {
+  initial: false,
+  customerTab: 'info',
+  customerEditFormFlag: false,
+  responseMessage: null,
+  success: false,
+  isLoading: false,
+  error: null,
+  customers: [],
+  activeCustomers: [],
+  financialCompanies: [],
+  allActiveCustomers: [],
+  spCustomers: [],
+  customer: {},
+  customerDialog: false,
+  newMachineCustomer: null,
+  filterBy: '',
+  verified: 'all',
+  excludeReporting: 'included',
+  page: 0,
+  rowsPerPage: 10
+}
 
-export const customerSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    getCustomer: builder.query({
-      query: (id) => ({
-        url: PATH_SERVER.CRM.CUSTOMER.detail(id),
-        method: GET,
-        headers: {
-          Authorization: `Bearer ${getAuthToken()}`
-        }
-      })
-    }),
-    getAllCustomer: builder.query({
-      query: () => ({
-        url: PATH_SERVER.CRM.CUSTOMER.list,
-        method: GET,
-        headers: {
-          Authorization: `Bearer ${getAuthToken()}`
-        }
-      })
-    }),
-    updateCustomer: builder.mutation({
-      query: (id, data) => ({
-        url: PATH_SERVER.USER.detail(id),
-        method: PUT,
-        body: data,
-        headers: {
-          Authorization: `Bearer ${getAuthToken()}`
-        }
-      })
-    })
-  })
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    // START LOADING
+    startLoading(state) {
+      state.isLoading = true
+    },
+    // STOP LOADING
+    stopLoading(state) {
+      state.isLoading = false
+    },
+
+    // SET NEW MACHINE CUSTOMER
+    setNewMachineCustomer(state, action) {
+      state.newMachineCustomer = action.payload
+    },
+
+    // SET CUSTOMER TAB
+    setCustomerTab(state, action) {
+      state.customerTab = action.payload
+    },
+
+    // HAS ERROR
+    hasError(state, action) {
+      state.isLoading = false
+      state.error = action.payload
+      state.initial = true
+    },
+
+    // GET Customers
+    getCustomersSuccess(state, action) {
+      state.isLoading = false
+      state.success = true
+      state.customers = action.payload
+      state.initial = true
+    },
+
+    // GET Active Customers
+    getActiveCustomersSuccess(state, action) {
+      state.isLoading = false
+      state.success = true
+      state.activeCustomers = action.payload
+      state.initial = true
+    },
+
+    // GET Active Customers
+    getFinancialCompaniesSuccess(state, action) {
+      state.isLoading = false
+      state.success = true
+      state.financialCompanies = action.payload
+      state.initial = true
+    },
+
+    // GET ALL Customers
+    getAllActiveCustomersSuccess(state, action) {
+      state.isLoading = false
+      state.success = true
+      state.allActiveCustomers = action.payload
+      state.initial = true
+    },
+
+    // GET Active Customers
+    getActiveSPCustomersSuccess(state, action) {
+      state.isLoading = false
+      state.success = true
+      state.spCustomers = action.payload
+      state.initial = true
+    },
+
+    // GET Customer
+    getCustomerSuccess(state, action) {
+      state.isLoading = false
+      state.success = true
+      state.customer = action.payload
+      state.initial = true
+    },
+
+    setResponseMessage(state, action) {
+      state.responseMessage = action.payload
+      state.isLoading = false
+      state.success = true
+      state.initial = true
+    },
+
+    // RESET CUSTOMER
+    resetCustomer(state) {
+      state.customer = {}
+      state.responseMessage = null
+      state.success = false
+      state.isLoading = false
+    },
+
+    // RESET CUSTOMERS
+    resetCustomers(state) {
+      state.customers = []
+      state.responseMessage = null
+      state.success = false
+      state.isLoading = false
+    },
+    // RESET Active CUSTOMERS
+    resetActiveCustomers(state) {
+      state.activeCustomers = []
+      state.responseMessage = null
+      state.success = false
+      state.isLoading = false
+    },
+
+    // RESET ALL CUSTOMERS
+    resetAllActiveCustomers(state) {
+      state.allActiveCustomers = []
+      state.responseMessage = null
+      state.success = false
+      state.isLoading = false
+    },
+
+    // RESET FINANCING COMPANIES
+    resetFinancingCompanies(state) {
+      state.financialCompanies = []
+      state.responseMessage = null
+      state.success = false
+      state.isLoading = false
+    },
+
+    // Set FilterBy
+    setFilterBy(state, action) {
+      state.filterBy = action.payload
+    },
+
+    // Set FilterBy
+    setVerified(state, action) {
+      state.verified = action.payload
+    },
+
+    // Set Excluded
+    setExcludeReporting(state, action) {
+      state.excludeReporting = action.payload
+    },
+
+    // Set PageRowCount
+    ChangeRowsPerPage(state, action) {
+      state.rowsPerPage = action.payload
+    },
+    // Set PageNo
+    ChangePage(state, action) {
+      state.page = action.payload
+    }
+  }
 })
 
-export const { useGetAllCustomerQuery, useGetCustomerQuery, useUpdateCustomerMutation } = customerSlice
+// Reducer
+export default customerSlice.reducer
+
+// Actions
+export const {
+  setNewMachineCustomer,
+  setCustomerTab,
+  setCustomerEditFormVisibility,
+  resetCustomer,
+  resetCustomers,
+  resetActiveCustomers,
+  resetAllActiveCustomers,
+  resetFinancingCompanies,
+  setResponseMessage,
+  setFilterBy,
+  setVerified,
+  setExcludeReporting,
+  ChangeRowsPerPage,
+  ChangePage,
+  setCustomerDialog
+} = customerSlice.actions
