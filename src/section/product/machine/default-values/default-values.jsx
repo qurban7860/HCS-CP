4d7f1674eb92e094bf888fc8c6b8420b2fa52879
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-16 09:24:06 +1200

    Rename:  machineDefaultValues from machineSchema
    This commit adds the machineDefaultValues function to the `default-values.jsx` file in the `src/section/product/machine/default-values` directory. The function is responsible for generating default values for a machine based on the provided machine data. This enhancement improves the product functionality by providing a centralized and reusable function for setting default values.

---
+++ src/section/product/machine/default-values/default-values.jsx
@@ -0,0 +1,55 @@
+import { useMemo } from 'react'
+import { fDate, parseAddress } from 'util'
+
+export default function machineDefaultValues(machineData) {
+  return useMemo(() => {
+    return {
+      serialNo: machineData?.serialNo || '',
+      name: machineData?.name || '',
+      description: machineData?.description || '',
+      alias: machineData?.alias || [],
+      profile: machineData?.profile?.default?.size || '',
+      parentSerialNo: machineData?.parentMachine || '',
+      previousMachine: machineData?.parentMachine?.name || '',
+      supplier: machineData?.supplier?.name || '',
+      category: machineData?.machineModel?.category || '',
+      machineModel: machineData?.machineModel?.name || '',
+      customer: machineData?.customer?.name || '',
+      clientCode: machineData?.customer?.clientCode || '',
+      financialCompany: machineData?.financialCompany?.name || '',
+      parentConnection: machineData?.parentMachines || [],
+      machineConnection: machineData?.machineConnections || [],
+      transferredHistory: machineData?.transferredHistory || [],
+      transferredMachine: machineData?.transferredToMachine?.serialNo || '',
+      transferredCustomer: machineData?.transferredToMachine?.customer?.name || '',
+      status: machineData?.status?.name || '',
+      workOrderRef: machineData?.workOrderRef || '',
+      installationSite: parseAddress(machineData?.instalationSite?.address) || '',
+      billingSite: parseAddress(machineData?.billingSite?.address) || '',
+      installationSiteCountry: machineData?.instalationSite?.address?.country || '',
+      billingSiteCountry: machineData?.billingSite?.address?.country || '',
+      installationSiteLat: machineData?.instalationSite?.lat || '',
+      installationSiteLong: machineData?.instalationSite?.long || '',
+      billingSiteLat: machineData?.instalationSite?.lat || '',
+      billingSiteLong: machineData?.instalationSite?.long || '',
+      installationDate: fDate(machineData?.installationDate) || '',
+      manufactureDate: fDate(machineData?.manufactureDate) || '',
+      purchaseDate: fDate(machineData?.purchaseDate) || '',
+      transferredDate: fDate(machineData?.transferredDate) || '',
+      shippingDate: fDate(machineData?.shippingDate) || '',
+      landmark: machineData?.siteMilestone || '',
+      projectManager: machineData?.projectManager || '',
+      supportManager: machineData?.supportManager || '',
+      accountManager: machineData?.accountManager || '',
+      supportExpireDate: fDate(machineData?.supportExpireDate) || null,
+      description: machineData?.description || '',
+      isActive: machineData?.isActive || false,
+      createdIP: machineData?.createdIP || '',
+      updatedIP: machineData?.updatedIP || '',
+      createdBy: machineData?.createdBy?.name || '',
+      updatedBy: machineData?.updatedBy?.name || '',
+      createdAt: fDate(machineData?.createdAt) || '',
+      updatedAt: fDate(machineData?.updatedAt) || ''
+    }
+  }, [machineData])
+}
