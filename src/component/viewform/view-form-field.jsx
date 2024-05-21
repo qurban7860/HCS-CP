ed6314369cfa93cedc826b6c2aae4744d4468aea
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-22 07:35:24 +1200

    feat: Add optional link functionality to ViewFormField component
    This commit modifies the ViewFormField component in the src/component/viewform/view-form-field.jsx file to include optional link functionality. It introduces a new prop, "link", which, when provided, displays a link icon next to the field value. Clicking on the link icon opens the provided link in a new tab. This enhancement improves the user experience by allowing users to easily navigate to external resources related to the field value.

--- src/component/viewform/view-form-field.jsx
+++ src/component/viewform/view-form-field.jsx
@@ -1,16 +1,40 @@
-import { memo } from 'react'
+import { useState, useEffect, memo } from 'react'
 import PropTypes from 'prop-types'
 import { m } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
+import { Typography, Chip, IconButton } from '@mui/material'
+import { GStyledSpanBox, GStyledPopover } from 'theme/style'
 import { useSettingContext } from 'component/setting'
-import { Typography, Grid, Chip } from '@mui/material'
+import { useIcon, ICON_NAME } from 'hook'
 import { SkeletonViewFormField } from 'component/skeleton'
-import { TYPOGRAPHY_VARIANT, SIZE, VARIANT } from 'constant'
+import { TYPOGRAPHY_VARIANT, SIZE, VARIANT, KEY } from 'constant'
 import { StyledDefaultTypography, StyledFieldGrid, StyledChipGrid, StyledFieldChip } from './style'
 
 const { TYPOGRAPHY } = VARIANT
 
-const ViewFormField = ({ children, heading, variant, isLoading, gridSize, contact, isWidget, isOrg, alias }) => {
+const ViewFormField = ({ children, heading, variant, isLoading, gridSize, contact, isWidget, isOrg, alias, link }) => {
+  const [open, setOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
   const { themeMode } = useSettingContext()
+  const { pathname } = useLocation()
+
+  const { Icon: WebIcon, iconSrc: openInSrc } = useIcon(ICON_NAME.OPEN_IN_NEW)
+
+  useEffect(() => {
+    if (open) {
+      handleClose()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname])
+
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget)
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   return (
     <StyledFieldGrid item xs={!isWidget && 12} sm={gridSize} mode={themeMode} isOrg={isOrg}>
@@ -23,7 +47,38 @@ const ViewFormField = ({ children, heading, variant, isLoading, gridSize, contac
         </m.div>
       ) : (
         <>
-          <StyledDefaultTypography variant={variant}>&nbsp;{children}</StyledDefaultTypography>
+          {link ? (
+            <GStyledSpanBox>
+              <StyledDefaultTypography variant={variant}> &nbsp; {link} </StyledDefaultTypography> &nbsp;
+              <GStyledPopover
+                open={open}
+                anchorEl={anchorEl}
+                anchorOrigin={{ vertical: KEY.CENTER, horizontal: KEY.RIGHT }}
+                transformOrigin={{ vertical: KEY.CENTER, horizontal: KEY.LEFT }}>
+                <Typography variant={TYPOGRAPHY.OVERLINE} color={themeMode === KEY.DARK ? 'howick.orange' : 'howick.blue'}>
+                  Go to {link} &nbsp;
+                </Typography>
+              </GStyledPopover>
+              <IconButton
+                onClick={() => window.open(`https://` + link, KEY.BLANK)}
+                size={SIZE.MEDIUM}
+                color={themeMode === KEY.LIGHT ? 'grey.800' : 'common.white'}
+                onMouseEnter={handleOpen}
+                onMouseLeave={handleClose}
+                aria-label="view"
+                target={KEY.BLANK}
+                sx={{
+                  padding: 0.5,
+                  borderRadius: 2,
+                  m: 0
+                }}>
+                <WebIcon icon={openInSrc} width={15} />
+              </IconButton>
+            </GStyledSpanBox>
+          ) : (
+            <StyledDefaultTypography variant={variant}>&nbsp; {children}</StyledDefaultTypography>
+          )}
+
           {contact && typeof contact === 'object' && contact.length > 0 ? (
             <StyledChipGrid container>
               {contact.map((chip, index) => (
@@ -67,7 +122,8 @@ ViewFormField.propTypes = {
   isWidget: PropTypes.bool,
   contact: PropTypes.oneOfType([PropTypes.object, PropTypes.array, PropTypes.string]),
   alias: PropTypes.oneOfType([PropTypes.object, PropTypes.array, PropTypes.string]),
-  variant: PropTypes.oneOf(Object.values(TYPOGRAPHY_VARIANT))
+  variant: PropTypes.oneOf(Object.values(TYPOGRAPHY_VARIANT)),
+  link: PropTypes.string
 }
 
 ViewFormField.defaultProps = {
