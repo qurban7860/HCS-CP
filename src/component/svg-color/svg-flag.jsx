6ab792d291dcf3f8b17a5c604eeb4d8293b1574a
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-21 16:01:21 +1200

    HPS-1263: Add SvgFlagIcon component for displaying country flags

--- src/component/svg-color/svg-flag.jsx
+++ src/component/svg-color/svg-flag.jsx
@@ -1,19 +1,34 @@
-import { SvgIcon } from '@mui/material'
+import countries from 'i18n-iso-countries'
+import enLocale from 'i18n-iso-countries/langs/en.json'
+import { useSettingContext } from 'component/setting'
+import { Button } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
 
-function _getCountryIcon(icon) {
-  return `/asset/svg/flag/${icon}.svg`
+import { GStyledTooltip } from 'theme/style'
+
+countries.registerLocale(enLocale)
+
+function _getCountryIcon(country) {
+  const countryCode = countries.getAlpha2Code(country, 'en')?.toLocaleLowerCase()
+  return `/asset/svg/flag/${countryCode}.svg`
 }
 
-const SvgFlagIcon = ({ country, dimension = 40, ...other }) => {
+const SvgFlagIcon = ({ country, dimension = 24, color, ...other }) => {
   return (
-    <img
-      src={_getCountryIcon(country)}
-      alt={country + 'icon'}
-      {...other}
-      style={{
-        borderRadius: 10
-      }}
-    />
+    country !== undefined && (
+      <Button
+        variant="filled"
+        sx={{
+          cursor: 'default',
+          color,
+          borderColor: color
+        }}
+        {...other}>
+        <GStyledTooltip title={country} placement="top" disableFocusListener tooltipcolor={color} color={color}>
+          <img src={_getCountryIcon(country)} alt={country} style={{ borderRadius: 2 }} />
+        </GStyledTooltip>
+      </Button>
+    )
   )
 }
 
