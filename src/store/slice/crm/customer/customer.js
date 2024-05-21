b1489eab0d46a98dabdaf836e8e5892d63f9b58e
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-22 07:42:45 +1200

    refactor: Update customer slice to use consistent naming conventions: FIXES naming clashes for the slices.
    This commit updates the customer slice in src/store/slice/crm/customer/customer.js to use consistent naming conventions. The changes include renaming variables and functions to match the established conventions in the repository. This refactor improves code clarity and maintainability by aligning with the standard naming practices.

--- src/store/slice/crm/customer/customer.js
+++ src/store/slice/crm/customer/customer.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit'
-// utils
 import axios from 'util/axios'
 import { GLOBAL } from 'config/global'
 
@@ -19,84 +18,65 @@ const initialState = {
   customer: {},
   customerDialog: false,
   newMachineCustomer: null,
-  filterBy: '',
+  customerFilterBy: '',
   verified: 'all',
   excludeReporting: 'included',
-  page: 0,
-  rowsPerPage: 10
+  customerPage: 0,
+  customerRowsPerPage: 10
 }
 
 const customerSlice = createSlice({
   name: 'customer',
   initialState,
   reducers: {
-    // START LOADING
     startLoading(state) {
       state.isLoading = true
     },
-    // STOP LOADING
     stopLoading(state) {
       state.isLoading = false
     },
 
-    // SET NEW MACHINE CUSTOMER
     setNewMachineCustomer(state, action) {
       state.newMachineCustomer = action.payload
     },
-
-    // SET CUSTOMER TAB
     setCustomerTab(state, action) {
       state.customerTab = action.payload
     },
-
-    // HAS ERROR
     hasError(state, action) {
       state.isLoading = false
       state.error = action.payload
       state.initial = true
     },
-
-    // GET Customers
     getCustomersSuccess(state, action) {
       state.isLoading = false
       state.success = true
       state.customers = action.payload
       state.initial = true
     },
-
-    // GET Active Customers
     getActiveCustomersSuccess(state, action) {
       state.isLoading = false
       state.success = true
       state.activeCustomers = action.payload
       state.initial = true
     },
-
-    // GET Active Customers
     getFinancialCompaniesSuccess(state, action) {
       state.isLoading = false
       state.success = true
       state.financialCompanies = action.payload
       state.initial = true
     },
-
-    // GET ALL Customers
     getAllActiveCustomersSuccess(state, action) {
       state.isLoading = false
       state.success = true
       state.allActiveCustomers = action.payload
       state.initial = true
     },
-
-    // GET Active Customers
     getActiveSPCustomersSuccess(state, action) {
       state.isLoading = false
       state.success = true
       state.spCustomers = action.payload
       state.initial = true
     },
-
-    // GET Customer
     getCustomerSuccess(state, action) {
       state.isLoading = false
       state.success = true
@@ -110,76 +90,56 @@ const customerSlice = createSlice({
       state.success = true
       state.initial = true
     },
-
-    // RESET CUSTOMER
     resetCustomer(state) {
       state.customer = {}
       state.responseMessage = null
       state.success = false
       state.isLoading = false
     },
-
-    // RESET CUSTOMERS
     resetCustomers(state) {
       state.customers = []
       state.responseMessage = null
       state.success = false
       state.isLoading = false
     },
-    // RESET Active CUSTOMERS
     resetActiveCustomers(state) {
       state.activeCustomers = []
       state.responseMessage = null
       state.success = false
       state.isLoading = false
     },
-
-    // RESET ALL CUSTOMERS
     resetAllActiveCustomers(state) {
       state.allActiveCustomers = []
       state.responseMessage = null
       state.success = false
       state.isLoading = false
     },
-
-    // RESET FINANCING COMPANIES
     resetFinancingCompanies(state) {
       state.financialCompanies = []
       state.responseMessage = null
       state.success = false
       state.isLoading = false
     },
-
-    // Set FilterBy
-    setFilterBy(state, action) {
-      state.filterBy = action.payload
-    },
-
-    // Set FilterBy
     setVerified(state, action) {
       state.verified = action.payload
     },
-
-    // Set Excluded
     setExcludeReporting(state, action) {
       state.excludeReporting = action.payload
     },
-
-    // Set PageRowCount
-    ChangeRowsPerPage(state, action) {
-      state.rowsPerPage = action.payload
+    setCustomerFilterBy(state, action) {
+      state.customerFilterBy = action.payload
     },
-    // Set PageNo
-    ChangePage(state, action) {
-      state.page = action.payload
+    ChangeCustomerRowsPerPage(state, action) {
+      state.customerRowsPerPage = action.payload
+    },
+    ChangeCustomerPage(state, action) {
+      state.customerPage = action.payload
     }
   }
 })
 
-// Reducer
 export default customerSlice.reducer
 
-// Actions
 export const {
   setNewMachineCustomer,
   setCustomerTab,
@@ -190,10 +150,9 @@ export const {
   resetAllActiveCustomers,
   resetFinancingCompanies,
   setResponseMessage,
-  setFilterBy,
-  setVerified,
+  setCustomerFilterBy,
   setExcludeReporting,
-  ChangeRowsPerPage,
-  ChangePage,
+  ChangeCustomerRowsPerPage,
+  ChangeCustomerPage,
   setCustomerDialog
 } = customerSlice.actions
