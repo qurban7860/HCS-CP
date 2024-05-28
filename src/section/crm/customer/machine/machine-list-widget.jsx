2e00e98120266b900ccbd0c429246154cbe3dd38
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-29 08:30:39 +1200

    HPS-1272: Add MachineDialog component
    This code change adds a new component, MachineDialog, to handle the display of machine details in a dialog. The MachineDialog component imports necessary dependencies and utilizes state and selector hooks from the store. It renders a dialog with various sections for displaying machine information, including model, default profile, organization, status, work order, financing company, supplier, support expiration, purchase date, manufacture date, shipping date, installation date, and description. This enhancement improves the user experience by providing a convenient way to view and interact with machine details.

--- src/section/crm/customer/machine/machine-list-widget.jsx
+++ src/section/crm/customer/machine/machine-list-widget.jsx
@@ -5,32 +5,31 @@ import { useIcon, ICON_NAME } from 'hook'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { dispatch } from 'store'
-import { setIsDecoiler, getCustomerMachines, resetCustomerMachines, getMachineModel, getMachineModels } from 'store/slice'
+import { getCustomerMachines, resetCustomerMachines, getMachineModel, getMachineModels } from 'store/slice'
 import { Grid, Typography, IconButton, Divider } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { GStyledTooltip } from 'theme/style'
 import { useSettingContext } from 'component/setting'
-import { FormHeader, SkeletonViewFormField } from 'component'
+import { FormHeader, SkeletonViewFormField, IconTooltip, MachineDialog } from 'component'
 import { PATH_MACHINE } from 'route/path'
 import { GStyledCenterBox, GStyledListItemText, GStyledSpanBox } from 'theme/style'
-import { VARIANT, SIZE, LABEL, KEY, DECOILER, FILTER_TYPE } from 'constant'
+import { VARIANT, SIZE, LABEL, KEY, DECOILER, DECOILER_TYPE_ARR, FLEX } from 'constant'
 import { normalizer } from 'util/format'
 import { StyledStatusChip } from '../style'
 
 const { TYPOGRAPHY } = VARIANT
 const { ONE_HALF_T, THREE_T, FIVE_T, SIX_T } = DECOILER
 
-const MachineListWidget = ({ value }) => {
+const MachineListWidget = ({ value, handleMachineDialog }) => {
+  const { id } = useParams()
   const [icon, setIcon] = useState(null)
   const [loading, setLoading] = useState(false)
-  const { id } = useParams()
   const theme = useTheme()
   const { themeMode } = useSettingContext()
   const { customerMachines, isLoading } = useSelector((state) => state.machine)
   const { machineModels, machineModel } = useSelector((state) => state.machinemodel)
 
   const isActive = (status) => (status ? LABEL.ACTIVE : LABEL.INACTIVE)
-  const decoilers = ['1.5T', '3.0T', '5.0T', '6.0T']
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,46 +40,7 @@ const MachineListWidget = ({ value }) => {
     fetchData()
   }, [dispatch, id])
 
-  useEffect(() => {
-    const model = customerMachines?.map((mach) => mach?.machineModel?.name)
-
-    model.forEach((machineName) => {
-      if (checkDecoilerType(machineName) === THREE_T) {
-        setIcon(ICON_NAME.DECOILER_3T)
-      } else if (checkDecoilerType(machineName) === FIVE_T) {
-        setIcon(ICON_NAME.DECOILER_5T)
-      } else if (checkDecoilerType(machineName) === SIX_T) {
-        setIcon(ICON_NAME.DECOILER_6T)
-      } else if (checkDecoilerType(machineName) === ONE_HALF_T) {
-        setIcon(ICON_NAME.DECOILER_1_5T)
-      } else {
-        setIcon(null)
-      }
-    })
-  }, [customerMachines, machineModel])
-
-  const isDecoiler1_5T = (machineName) => normalizer(machineName)?.includes(ONE_HALF_T)
-  const isDecoiler3T = (machineName) => normalizer(machineName)?.includes(THREE_T)
-  const isDecoiler5T = (machineName) => normalizer(machineName)?.includes(FIVE_T)
-  const isDecoiler6T = (machineName) => normalizer(machineName)?.includes(SIX_T)
-
-  const checkDecoilerType = (machineName) => {
-    const normalizedMachineName = normalizer(machineName)
-
-    if (isDecoiler1_5T(normalizedMachineName)) {
-      return ONE_HALF_T
-    } else if (isDecoiler3T(normalizedMachineName)) {
-      return THREE_T
-    } else if (isDecoiler5T(normalizedMachineName)) {
-      return FIVE_T
-    } else if (isDecoiler6T(normalizedMachineName)) {
-      return SIX_T
-    } else {
-      return null
-    }
-  }
-
-  const { Icon: LocIcon, iconSrc } = useIcon(icon)
+  const { Icon: LocIcon, iconSrc } = useIcon(ICON_NAME.DECOILER_DEF)
 
   return (
     <Grid container mb={2}>
@@ -99,7 +59,7 @@ const MachineListWidget = ({ value }) => {
                       mach && (
                         <GStyledSpanBox>
                           <IconButton
-                            onClick={() => window.open(PATH_MACHINE.machines.view(mach._id), KEY.BLANK)}
+                            onClick={() => handleMachineDialog(mach._id)}
                             size={SIZE.MEDIUM}
                             color={themeMode === KEY.LIGHT ? 'grey.800' : 'common.white'}
                             aria-label="view"
@@ -113,7 +73,7 @@ const MachineListWidget = ({ value }) => {
                               {mach?.serialNo}
                             </Typography>
                           </IconButton>
-                          {decoilers.some((type) => mach?.machineModel?.name?.includes(type)) && (
+                          {DECOILER_TYPE_ARR.some((type) => mach?.machineModel?.name?.includes(type)) && (
                             <GStyledTooltip
                               title={LABEL.DECOILER_DEF}
                               disableFocusListener
@@ -133,15 +93,28 @@ const MachineListWidget = ({ value }) => {
                     }
                   />
                 </Grid>
-                <Grid item xs={4} flex={1} justifyContent={KEY.CENTER} alignContent={KEY.CENTER}>
-                  <GStyledCenterBox>
-                    <StyledStatusChip
-                      label={<Typography variant={TYPOGRAPHY.H6}>{isActive(mach?.isActive)}</Typography>}
-                      size={SIZE.SMALL}
-                      variant={VARIANT.OUTLINED}
-                      isActive
+                <Grid item xs={4} flex={1} justifyContent={KEY.FLEX_END} alignContent={KEY.RIGHT}>
+                  <GStyledSpanBox justifyContent={FLEX.FLEX_END}>
+                    <IconTooltip
+                      title={LABEL.NAVIGATE_TO(mach?.serialNo)}
+                      icon={ICON_NAME.OPEN_IN_NEW}
+                      dimension={20}
+                      onClick={() => window.open(PATH_MACHINE.machines.view(mach?._id), KEY.BLANK)}
+                      color={themeMode === KEY.LIGHT ? theme.palette.grey[500] : theme.palette.howick.lightGray}
                     />
-                  </GStyledCenterBox>
+                    {value?.isActive ? (
+                      <IconTooltip
+                        title={LABEL.ACTIVE}
+                        icon={ICON_NAME.ACTIVE}
+                        dimension={20}
+                        isActiveIcon
+                        color={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
+                        px={0}
+                      />
+                    ) : (
+                      <IconTooltip title={LABEL.INACTIVE} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} dimension={20} />
+                    )}
+                  </GStyledSpanBox>
                 </Grid>
                 {/* if in the last index dont add divider */}
                 {index !== customerMachines?.length - 1 && <Divider variant="fullWidth" style={{ width: '100%', marginBottom: '10px' }} />}
