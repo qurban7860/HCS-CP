d15b49cc7db661419e02f0e314c8c6a337994d05
ballyalley-o <bally.lomibao@howickltd.com>
2024-05-29 08:31:27 +1200

    HPS-1272 / HPS-1273 / HPS-1276: Add MachineDialog component
    This code change adds a new component, MachineDialog, to handle the display of machine details in a dialog. The MachineDialog component imports necessary dependencies and utilizes state and selector hooks from the store. It renders a dialog with various sections for displaying machine information, including model, default profile, organization, status, work order, financing company, supplier, support expiration, purchase date, manufacture date, shipping date, installation date, and description. This enhancement improves the user experience by providing a convenient way to view and interact with machine details.

--- src/section/crm/customer/customer-layout.jsx
+++ src/section/crm/customer/customer-layout.jsx
@@ -2,27 +2,26 @@ import { memo, useEffect, useLayoutEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useSelector, dispatch } from 'store'
 import { useParams } from 'react-router-dom'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
-import { ICON_NAME, snack } from 'hook'
+import { ICON_NAME } from 'hook'
+import { getCustomerMachines, getConnectedMachineDialog, getCustomer, setMachineDialog } from 'store/slice'
 import { Box, Grid, Card, Divider } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { GStyledTopBorderDivider, GStyledSpanBox, GStyledFlexEndBox } from 'theme/style'
-import { getCustomerMachines, useGetCustomerQuery } from 'store/slice'
 import { customerDefaultValues } from 'section/crm'
 import { HowickResources } from 'section/common'
-import { IconTooltip, BackButton, AuditBox, GridViewField, GridViewTitle, SvgFlagIcon, BadgeCardMedia } from 'component'
+import { IconTooltip, BackButton, AuditBox, GridViewField, GridViewTitle, SvgFlagIcon, BadgeCardMedia, MachineDialog } from 'component'
 import { ViewFormField } from 'component/viewform'
 import { MotionLazyContainer } from 'component/animate'
 import { useSettingContext } from 'component/setting'
 import { MARGIN } from 'config'
 import { KEY, TITLE, LABEL, RESPONSE, COLOR, VIEW_FORM, VARIANT, FLEX_DIR, FLEX } from 'constant'
 import { MachineListWidget, ContactListWidget, CardOption } from 'section/crm/customer'
+import { truncate } from 'util/truncate'
 
 const CustomerLayout = () => {
   const { id } = useParams()
-  const { customerMachines } = useSelector((state) => state.machine)
-  const { data: customerData, isLoading, error: customerError, refetch: customerRefetch } = useGetCustomerQuery(id)
+  const { customerMachines, connectedMachineDialog } = useSelector((state) => state.machine)
+  const { customer, isLoading } = useSelector((state) => state.customer)
 
   const theme = useTheme()
   const { themeMode } = useSettingContext()
@@ -30,15 +29,10 @@ const CustomerLayout = () => {
   const { CUSTOMER, SITE, ADDRESS } = VIEW_FORM
 
   useEffect(() => {
-    if (customerError) {
-      snack(RESPONSE.error.FETCH, { variant: COLOR.ERROR })
-    } else if (isLoading) {
-      // snack(RESPONSE.FETCH_LOADING)
-    } else {
-      // snack(RESPONSE.success.FETCH_DATA, { variant: COLOR.SUCCESS })
+    if (id) {
+      dispatch(getCustomer(id))
     }
-    customerRefetch()
-  }, [customerRefetch, id, customerData, isLoading, customerError])
+  }, [dispatch, id])
 
   useEffect(() => {
     if (id) {
@@ -46,31 +40,12 @@ const CustomerLayout = () => {
     }
   }, [id, dispatch])
 
-  const defaultValues = customerDefaultValues(customerData, customerMachines)
+  const defaultValues = customerDefaultValues(customer, customerMachines)
 
-  const methods = useForm({
-    resolver: yupResolver(customerDefaultValues),
-    defaultValues
-  })
-
-  // const {
-  //   reset,
-  //   control,
-  //   handleSubmit,
-  //   formState: { isSubmitting, errors }
-  // } = methods
-
-  // const onSubmit = async (data) => {
-  //   try {
-  //     // await dispatch(updateSecurityUser(data, securityUser._id))
-  //     // await dispatch(getSecurityUser(securityUser._id))
-  //     reset()
-  //     navigate(PATH_SECURITY.users.profile)
-  //   } catch (error) {
-  //     enqueueSnackbar(error, { variant: `error` })
-  //     console.log('Error:', error)
-  //   }
-  // }
+  const handleConnectedMachineDialog = (machineId) => {
+    dispatch(getConnectedMachineDialog(machineId))
+    dispatch(setMachineDialog(true))
+  }
 
   // TODO: #HPS-1062 when JIRA api integated, replace this mock data
 
@@ -87,7 +62,7 @@ const CustomerLayout = () => {
         </Grid>
         {/* TODO: HPS-1246: Machine List widget */}
         <Grid item lg={3}>
-          <MachineListWidget value={defaultValues} />
+          <MachineListWidget value={defaultValues} handleMachineDialog={handleConnectedMachineDialog} />
           <ContactListWidget value={defaultValues} />
         </Grid>
 
@@ -100,20 +75,21 @@ const CustomerLayout = () => {
                 <Grid item lg={8}>
                   <GStyledSpanBox my={2}>
                     <BadgeCardMedia />
-                    <ViewFormField heading={VIEW_FORM.ORGANIZATION} isLoading={isLoading} isMachineView>
-                      {defaultValues?.name}
+                    <ViewFormField variant={VARIANT.TYPOGRAPHY.H4} heading={VIEW_FORM.ORGANIZATION} isLoading={isLoading} isMachineView>
+                      {truncate(defaultValues?.name, 50)}
                     </ViewFormField>
                   </GStyledSpanBox>
                 </Grid>
                 <Grid item lg={4}>
                   <Grid container justifyContent={FLEX.FLEX_END} flexDirection="column" alignContent={FLEX.FLEX_END}>
                     <Grid item xs={12} justifyContent={FLEX.FLEX_END} mt={2}>
-                      <Grid container justifyContent={FLEX.FLEX_END}>
+                      <Grid container justifyContent={FLEX.FLEX_END} gap={1} alignItems={KEY.CENTER}>
                         <SvgFlagIcon
                           country={defaultValues?.country}
                           color={themeMode === KEY.LIGHT ? theme.palette.howick.midBlue : theme.palette.howick.bronze}
                           dimension={24}
                         />
+
                         {defaultValues?.isActive ? (
                           <IconTooltip
                             title={LABEL.ACTIVE}
@@ -130,11 +106,10 @@ const CustomerLayout = () => {
               </Grid>
               <Grid container spacing={2} px={1.5} mb={5}>
                 <Grid item lg={12} sm={12}>
-                  <Grid container spacing={5} p={2} pb={5}>
-                    <GridViewField heading={CUSTOMER.CUSTOMER_NAME} isLoading={isLoading} children={defaultValues?.name} />
+                  <Grid container spacing={2} p={2} pb={5}>
                     <GridViewField heading={CUSTOMER.CUSTOMER_CODE} isLoading={isLoading} children={defaultValues?.code} />
-                    <GridViewField heading={CUSTOMER.TRADING_NAME} isLoading={isLoading} alias={defaultValues?.tradingName} />
                     <GridViewField heading={VIEW_FORM.STATUS} isLoading={isLoading} children={defaultValues?.status} />
+                    <GridViewField heading={CUSTOMER.TRADING_NAME} isLoading={isLoading} alias={defaultValues?.tradingName} gridSize={12} />
                     <GridViewField heading={VIEW_FORM.WEBSITE} isLoading={isLoading} link={defaultValues?.website} />
                   </Grid>
                 </Grid>
@@ -149,7 +124,7 @@ const CustomerLayout = () => {
 
                 <Divider variant="middle" style={{ width: '100%', marginBottom: '20px' }} />
                 <Grid item lg={12} sm={12}>
-                  <Grid container spacing={5} p={2} pb={5}>
+                  <Grid container spacing={2} p={2} pb={5}>
                     <GridViewField heading={SITE.SITE_NAME} isLoading={isLoading} children={defaultValues?.name} />
                     <GridViewField heading={ADDRESS.STREET} isLoading={isLoading} children={defaultValues?.street} />
                     <GridViewField heading={ADDRESS.SUBURB} isLoading={isLoading} children={defaultValues?.suburb} />
@@ -185,6 +160,7 @@ const CustomerLayout = () => {
         </Grid>
       </Grid>
       {/* </FormProvider> */}
+      {connectedMachineDialog && <MachineDialog />}
     </MotionLazyContainer>
   )
 }
