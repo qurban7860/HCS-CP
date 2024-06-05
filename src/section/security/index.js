250c0662f1e194c13eb060fb9a6d88bafd1b21d4
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-05 15:28:16 +1200

    Exports: Add security section with UserProfileLayout, ProfileAvatar, and userDefaultValues components

---
+++ src/section/security/index.js
@@ -0,0 +1,3 @@
+export { default as UserProfileLayout } from './user/user-profile-layout'
+export { default as ProfileAvatar } from './user/profile-avatar'
+export { default as userDefaultValues } from './default-values/default-values'
