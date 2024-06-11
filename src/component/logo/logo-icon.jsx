e1941c23c7ac4ed88007bd91482e8f4856f71477
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-11 13:23:28 +1200

    This updates the loading screen logo.
    This code change adds the branding logo to the welcome component in the widget directory. The logo is displayed based on the theme mode (dark or light) and improves the branding consistency of the application.

--- src/component/logo/logo-icon.jsx
+++ src/component/logo/logo-icon.jsx
@@ -1,24 +1,25 @@
 import { forwardRef } from 'react'
 import PropTypes from 'prop-types'
-import { Box, Link } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
-import { useIcon, useSettingContext } from 'hook'
+import { useIcon, useSettingContext, ICON_NAME } from 'hook'
+import { Box, Link } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
 import { ASSET, BRAND } from 'config'
 import { KEY } from 'constant'
 
 const LogoIcon = forwardRef(({ width = BRAND.NAV_ICON, disabledLink = false, sx, src = ASSET.ICON, ...other }, ref) => {
+  const theme = useTheme()
   const { themeMode } = useSettingContext()
-  const { Icon: LogoIcon, iconSrc } = useIcon('HOWICK_LOGO')
+  const { Icon: LogoIcon, iconSrc } = useIcon(ICON_NAME.HOWICK_LOGO)
 
-  const logo = <Box component="img" src={src} sx={{ width, height: 'auto', cursor: 'pointer', ...sx }} />
-
-  const logoDark = <LogoIcon icon={iconSrc} alt="howick-logo" color="#FFF" p={1} m={1} />
+  const logo = <Box component={KEY.IMG} src={src} sx={{ width, height: KEY.AUTO, cursor: 'pointer', ...sx }} />
+  const logoDark = <LogoIcon icon={iconSrc} alt="howick-logo" color={theme.palette.common.white} p={1} m={1} />
 
   if (disabledLink) {
     return logo
   }
   return (
-    <Link component={RouterLink} to="/" sx={{ display: 'contents' }}>
+    <Link component={RouterLink} to="/" sx={{ display: 'contents' }} {...other}>
       {themeMode === KEY.LIGHT ? logo : logoDark}
     </Link>
   )
