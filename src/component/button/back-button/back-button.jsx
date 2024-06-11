9bf0a44d0c0433766cec2c1e01ccb3386d20991c
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-11 13:06:19 +1200

    Add alongTab prop to BackButton component
    This code change adds a new prop called alongTab to the BackButton component. When alongTab is set to true, the BackButton component will be rendered as an IconTooltip component, providing a tooltip with the label "Go Back". This change enhances the user experience by allowing more flexibility in rendering the BackButton component in different scenarios.

--- src/component/button/back-button/back-button.jsx
+++ src/component/button/back-button/back-button.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect, useRef, Fragment } from 'react'
 import { m } from 'framer-motion'
+import PropTypes from 'prop-types'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { ICON_NAME, useIcon, useSettingContext } from 'hook'
-import { Typography } from '@mui/material'
+import { Typography, IconButton, Button } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
+import { IconTooltip } from 'component'
 import { KEY, LABEL, VARIANT } from 'constant'
 import { GStyledPopover } from 'theme/style'
 import { StyledBackIconButton } from '../style'
@@ -12,7 +14,7 @@ const {
   TYPOGRAPHY: { OVERLINE1 }
 } = VARIANT
 
-const BackButton = () => {
+const BackButton = ({ alongTab, width = 50 }) => {
   const [open, setOpen] = useState(false)
   const [anchorEl, setAnchorEl] = useState(null)
 
@@ -21,6 +23,7 @@ const BackButton = () => {
   const navigate = useNavigate()
   const navRef = useRef(null)
   const theme = useTheme()
+
   const { Icon, iconSrc: backIconSrc } = useIcon(ICON_NAME.BACK)
 
   const handleClick = (event) => {
@@ -65,30 +68,48 @@ const BackButton = () => {
 
   return (
     <Fragment>
-      <StyledBackIconButton
-        disabled={window.history.length === 1}
-        onClick={handleClick}
-        aria-label={LABEL.GO_BACK}
-        onMouseEnter={handleOpen}
-        onMouseLeave={handleClose}>
-        <Icon icon={backIconSrc} width={50} />
-      </StyledBackIconButton>
-      <GStyledPopover
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: KEY.CENTER, horizontal: KEY.RIGHT }}
-        transformOrigin={{ vertical: KEY.CENTER, horizontal: KEY.LEFT }}
-        sx={{
-          marginLeft: 3
-        }}>
-        <m.div>
-          <Typography variant={OVERLINE1} color={themeMode === KEY.LIGHT ? theme.palette.grey[700] : theme.palette.grey[400]}>
-            {LABEL.GO_BACK}
-          </Typography>
-        </m.div>
-      </GStyledPopover>
+      {alongTab ? (
+        <IconTooltip
+          title={LABEL.GO_BACK}
+          icon={ICON_NAME.A_BACK}
+          color={themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[400]}
+          onClick={handleClick}
+          cursor
+          iconOnly
+        />
+      ) : (
+        <Fragment>
+          <StyledBackIconButton
+            disabled={window.history.length === 1}
+            onClick={handleClick}
+            aria-label={LABEL.GO_BACK}
+            onMouseEnter={handleOpen}
+            onMouseLeave={handleClose}>
+            <Icon icon={backIconSrc} width={width} />
+          </StyledBackIconButton>
+          <GStyledPopover
+            open={open}
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: KEY.CENTER, horizontal: KEY.RIGHT }}
+            transformOrigin={{ vertical: KEY.CENTER, horizontal: KEY.LEFT }}
+            sx={{
+              marginLeft: 3
+            }}>
+            <m.div>
+              <Typography variant={OVERLINE1} color={themeMode === KEY.LIGHT ? theme.palette.grey[700] : theme.palette.grey[400]}>
+                {LABEL.GO_BACK}
+              </Typography>
+            </m.div>
+          </GStyledPopover>
+        </Fragment>
+      )}
     </Fragment>
   )
+}
+
+BackButton.propTypes = {
+  alongTab: PropTypes.bool,
+  width: PropTypes.number
 }
 
 export default BackButton
