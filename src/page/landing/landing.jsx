574b27587910f49b0e781e41424c35acc046b1ea
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-11 13:14:20 +1200

    housekeeping:  Add new constants and update typography variants
    This code change adds new constants for font weights in the 'variant.js' file and updates the typography variants to use these new options. The added font weight options include 'light', 'regular', 'medium', 'bold', and 'black'. This change provides more flexibility and customization options for typography styles in the application.

--- src/page/landing/landing.jsx
+++ src/page/landing/landing.jsx
@@ -5,7 +5,7 @@ import { useTheme } from '@mui/material/styles'
 import { GStyledBrandOverlayBox, GStyledBottomPolygonDiv, GStyledTopPolygonDiv, StylendLandingContainerBox, ButtonProps } from 'theme/style'
 import { useAuthContext } from 'auth'
 import { ASSET, GLOBAL, BRAND } from 'config'
-import { LABEL, PRODUCT, COMPANY, BUTTON } from 'constant'
+import { LABEL, PRODUCT, COMPANY, BUTTON, TYPOGRAPHY, KEY, FLEX_DIR, FLEX } from 'constant'
 
 const Landing = () => {
   const { isAuthenticated } = useAuthContext()
@@ -16,26 +16,25 @@ const Landing = () => {
     <StylendLandingContainerBox>
       <Grid container>
         <Grid container>
-          <Grid item xs={12} sx={{ marginRight: 10, display: 'flex', justifyContent: 'flex-end' }}>
+          <Grid item xs={12} sx={{ marginRight: 10, display: FLEX.FLEX, justifyContent: FLEX.FLEX_END }}>
             <img alt="logo" src={ASSET.BRAND_LOGO_FULL} width={700} />
           </Grid>
           <Grid item xs={12} sx={{ marginRight: 10 }}>
-            <Typography variant="h1" color={theme.palette.common.white} align="right" gutterBottom>
+            <Typography variant={TYPOGRAPHY.H1} color={theme.palette.common.white} align={KEY.RIGHT} gutterBottom>
               {GLOBAL.APP_TAGLINE.toUpperCase()}
             </Typography>
           </Grid>
         </Grid>
-
         <Grid
           container
-          justifyContent="flex-start"
+          justifyContent={FLEX.FLEX_START}
           spacing={2}
           sx={{
             marginTop: 5
           }}>
-          <Grid container flexDirection="row" justifyContent="center">
+          <Grid container flexDirection={FLEX_DIR.ROW} justifyContent={KEY.CENTER}>
             <Grid item lg={9}>
-              <Typography variant="overline" color="grey.500" sx={{ opacity: 0.5 }}>
+              <Typography variant={TYPOGRAPHY.OVERLINE} color="grey.500" sx={{ opacity: 0.5 }}>
                 {LABEL.OUR_PRODUCT}
               </Typography>
               <Grid container>
@@ -49,7 +48,7 @@ const Landing = () => {
               </Grid>
             </Grid>
             <Grid item lg={9} m={3}>
-              <Typography variant="overline" color="grey.500" sx={{ opacity: 0.5 }}>
+              <Typography variant={TYPOGRAPHY.OVERLINE} color="grey.500" sx={{ opacity: 0.5 }}>
                 {LABEL.OUR_COMPANY}
               </Typography>
               <Grid container>
@@ -62,7 +61,7 @@ const Landing = () => {
                 ))}
               </Grid>
             </Grid>
-            <Grid container flexDirection="row" justifyContent="flex-end">
+            <Grid container flexDirection={FLEX_DIR.ROW} justifyContent={FLEX.FLEX_END}>
               <Grid item lg={2} mr={10}>
                 <Button {...ButtonProps}>{isAuthenticated ? BUTTON.DASHBOARD : BUTTON.LOGIN}</Button>
               </Grid>
