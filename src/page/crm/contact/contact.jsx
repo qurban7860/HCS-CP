58a24b19d1e14a4b7bf0b56c20b935e695778e59
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-13 11:25:14 +1200

    HPS-1267 [Refax]: update Contact component to use CustomerLayout
    This code change updates the Contact component to use the CustomerLayout component instead of the ContactLayout component. This change improves the organization and structure of the CRM contact page.

--- src/page/crm/contact/contact.jsx
+++ src/page/crm/contact/contact.jsx
@@ -1,7 +1,12 @@
 import { ContactLayout } from 'section/crm'
+import { CustomerLayout } from 'section/crm'
 
 const Contact = () => {
-  return <ContactLayout />
+  return (
+    <CustomerLayout tab={1}>
+      <ContactLayout />
+    </CustomerLayout>
+  )
 }
 
 export default Contact
