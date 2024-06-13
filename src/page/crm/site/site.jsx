4c3fd3fc1c0229b2ec1675bbbca5323c966a4a43
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-13 13:32:02 +1200

    HPS-1312: Add Site component to CRM site page
    This code change adds a new component called Site to the CRM site page. The Site component is imported from the 'section/crm' module and is rendered within the CustomerLayout component. This addition enhances the functionality and user experience of the CRM site page.

---
+++ src/page/crm/site/site.jsx
@@ -0,0 +1,11 @@
+import { CustomerLayout, SiteTab } from 'section/crm'
+
+const Site = () => {
+  return (
+    <CustomerLayout tab={2}>
+      <SiteTab />
+    </CustomerLayout>
+  )
+}
+
+export default Site
