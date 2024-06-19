2388262ad44c81677320ec38ed1123901b8ff0c1
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-19 13:46:26 +1200

    Update useActiveLink hook to fix deepActive logic

--- src/hook/use-active-link.jsx
+++ src/hook/use-active-link.jsx
@@ -1,15 +1,15 @@
+import { useEffect } from 'react'
 import { useLocation, matchPath } from 'react-router-dom'
 
 function useActiveLink(path, deep = true) {
   const { pathname } = useLocation()
 
   const normalActive = path ? !!matchPath({ path, end: true }, pathname) : false
-
-  const deepActive = path ? !!matchPath({ path, end: false }, pathname) : false
+  const deepActive = path ? matchPath({ path, end: false }, pathname) : false
 
   return {
     active: deep ? deepActive : normalActive,
-    isExternalLink: path.includes('http'),
+    isExternalLink: path.includes('http')
   }
 }
 
