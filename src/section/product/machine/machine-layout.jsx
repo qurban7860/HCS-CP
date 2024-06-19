6fad7c72b8c44901264078a8d9b3d02b8cef417f
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-19 13:50:29 +1200

    HPS-1314: Add machine tabs and tab container components

--- src/section/product/machine/machine-layout.jsx
+++ src/section/product/machine/machine-layout.jsx
@@ -1,14 +1,13 @@
-import { memo, useEffect } from 'react'
+import { memo, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useSelector, dispatch } from 'store'
 import { useParams } from 'react-router-dom'
-import { ICON_NAME, Clock, useSettingContext } from 'hook'
+import { ICON_NAME, useSettingContext } from 'hook'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { PATH_CUSTOMER } from 'route/path'
-import { Box, Grid, Card, Divider, Link, Typography } from '@mui/material'
+import { PATH_MACHINE } from 'route/path'
+import { Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-import { GStyledTopBorderDivider, GStyledSpanBox, GStyledFlexEndBox, GCardOption } from 'theme/style'
 import {
   setDecoilerIcon,
   getMachineModels,
@@ -16,8 +15,6 @@ import {
   getCustomer,
   getMachine,
   setCustomerDialog,
-  getConnectedMachineDialog,
-  getMachineSiteDialogData,
   setMachineDialog,
   setMachineSiteDialog,
   resetCustomer,
@@ -25,28 +22,17 @@ import {
   resetMachineSiteDialogData
 } from 'store/slice'
 import { machineDefaultValues } from 'section/product'
-import { HowickResources } from 'section/common'
-import { IconTooltip, BackButton, AuditBox, GridViewField, GridViewTitle, CustomerDialog, MachineDialog, SiteDialog } from 'component'
-import { ViewFormField } from 'component/viewform'
-import { MotionLazyContainer } from 'component/animate'
-import { MARGIN } from 'config'
-import { KEY, TITLE, LABEL, VIEW_FORM, VARIANT, FLEX_DIR, FLEX, DECOILER, DECOILER_TYPE_ARR } from 'constant'
-import { MachineConnectionWidget, MachineSiteWidget, MachineHistoryWidget } from 'section/product/machine'
-import { truncate } from 'util/truncate'
+import { MotionLazyContainer, CustomerDialog, MachineDialog, SiteDialog } from 'component'
+import { VIEW_FORM, VARIANT, FLEX, DECOILER } from 'constant'
+import { MachineNav, MachineTab } from 'section/product/machine'
 
 const { ONE_HALF_T, THREE_T, FIVE_T, SIX_T } = DECOILER
 
-const MachineLayout = () => {
+const MachineLayout = ({ tab = 0 }) => {
+  const [renderedTab, setRenderedTab] = useState(tab)
   const { id } = useParams()
   const { machine, isLoading, connectedMachineDialog, machineSiteDialogData } = useSelector((state) => state.machine)
-  const { decoilerIcon } = useSelector((state) => state.machinemodel)
   const { customer, customerDialog } = useSelector((state) => state.customer)
-
-  const theme = useTheme()
-  const { themeMode } = useSettingContext()
-
-  const { MACHINE } = VIEW_FORM
-  const { TYPOGRAPHY } = VARIANT
 
   useEffect(() => {
     dispatch(getMachine(id))
@@ -68,11 +54,6 @@ const MachineLayout = () => {
   }, [dispatch])
 
   const defaultValues = machineDefaultValues(machine, customer)
-
-  const methods = useForm({
-    resolver: yupResolver(machineDefaultValues),
-    defaultValues
-  })
 
   useEffect(() => {
     dispatch(getMachineModels())
@@ -98,32 +79,21 @@ const MachineLayout = () => {
     }
   }, [dispatch, defaultValues?.machineModel])
 
-  const handleCustomerDialog = (event, customerId) => {
-    event.preventDefault()
-    dispatch(resetCustomer())
-    dispatch(getCustomer(customerId))
-    dispatch(setCustomerDialog(true))
-  }
-
-  const handleConnectedMachineDialog = (event, machineId) => {
-    event.preventDefault()
-    dispatch(resetConnectedMachineDialog())
-    dispatch(getConnectedMachineDialog(machineId))
-    dispatch(setMachineDialog(true))
-  }
-
-  const handleMachineSiteDialog = (event, machineId) => {
-    event.preventDefault()
-    dispatch(resetMachineSiteDialogData())
-    dispatch(getMachineSiteDialogData(machineId))
-    dispatch(setMachineSiteDialog(true))
-  }
-
-  const handleSiteWidgetDialog = (e, machineId) => {
-    e.preventDefault()
-    dispatch(resetMachineSiteDialogData())
-    dispatch(getMachineSiteDialogData(machineId))
-    dispatch(setMachineSiteDialog(true))
+  const navigatePage = (tab) => {
+    if (tab === 0 && id) {
+      navigate(PATH_MACHINE.machines.view(id))
+      // dispatch(setFromDialog(false))
+      // dispatch(setFromSiteDialog(false))
+    } else if (tab === 1 && id) {
+      // navigate(PATH_CUSTOMER.customers.contacts.view(id))
+      // dispatch(setFromDialog(false))
+      // dispatch(setFromSiteDialog(false))
+    } else if (tab === 2 && id) {
+      // navigate(PATH_CUSTOMER.customers.sites.view(id))
+      // dispatch(setFromDialog(false))
+      // dispatch(setFromSiteDialog(false))
+    } else if (tab === 3 && id) {
+    }
   }
 
   // TODO: #HPS-1062 when JIRA api integated, replace this mock data
@@ -131,150 +101,18 @@ const MachineLayout = () => {
   return (
     <MotionLazyContainer display={FLEX.FLEX}>
       {/*  TODO: HPS-1240 Make responsive */}
-      <Grid container spacing={2} flexDirection={FLEX_DIR.ROW} {...MARGIN.PAGE_PROP}>
-        <Grid container>
-          <Grid item sm={12}>
-            <BackButton />
-          </Grid>
-        </Grid>
-        <Grid item lg={3}>
-          <MachineHistoryWidget value={defaultValues} />
-          <MachineConnectionWidget
-            value={defaultValues}
-            handleConnectedMachineDialog={handleConnectedMachineDialog}
-            handleMachineSiteDialog={handleMachineSiteDialog}
-          />
-          <MachineSiteWidget value={defaultValues} handleSiteWidgetDialog={handleSiteWidgetDialog} />
-        </Grid>
-
-        <Grid item sm={12} lg={9}>
-          <Box mb={2}>
-            <Card {...GCardOption(themeMode)}>
-              <GStyledTopBorderDivider mode={themeMode} />
-              <Grid container px={1.5}>
-                <Grid item lg={8}>
-                  <GStyledSpanBox my={2}>
-                    <ViewFormField heading={' '} isLoading={isLoading} gridSize={8} isMachineView>
-                      <Typography variant={TYPOGRAPHY.H2}> {defaultValues?.serialNo} &nbsp;</Typography>
-                      <Typography variant={TYPOGRAPHY.H3} color={themeMode === KEY.LIGHT ? theme.palette.grey[500] : theme.palette.howick.bronze}>
-                        {defaultValues?.machineModel}
-                      </Typography>
-                    </ViewFormField>
-                  </GStyledSpanBox>
-                </Grid>
-                <Grid item lg={4}>
-                  <Grid container justifyContent={FLEX.FLEX_END} flexDirection={FLEX_DIR.COLUMN} alignContent={FLEX.FLEX_END}>
-                    <Grid item xs={12} justifyContent={FLEX.FLEX_END} mt={2}>
-                      <Grid container justifyContent={FLEX.FLEX_END} gap={3}>
-                        {defaultValues?.installationSiteCity && (
-                          <Clock city={defaultValues?.installationSiteCity} country={defaultValues?.installationSiteCountry} />
-                        )}
-                        {DECOILER_TYPE_ARR.some((type) => defaultValues?.machineModel?.includes(type)) && (
-                          <IconTooltip
-                            title={LABEL.DECOILER(defaultValues?.machineModel)}
-                            icon={decoilerIcon}
-                            color={themeMode === KEY.LIGHT ? theme.palette.grey[500] : theme.palette.grey[500]}
-                            iconOnly
-                          />
-                        )}
-                        {defaultValues?.isActive ? (
-                          <IconTooltip
-                            title={LABEL.ACTIVE}
-                            icon={ICON_NAME.ACTIVE}
-                            color={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
-                            iconOnly
-                          />
-                        ) : (
-                          <IconTooltip title={LABEL.INACTIVE} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} iconOnly />
-                        )}
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid container spacing={2} px={1.5} mb={5}>
-                <GridViewTitle title={TITLE.MACHINE_KEY_DETAILS} />
-                <Divider variant="middle" style={{ width: '100%', marginBottom: '20px' }} />
-                <Grid item lg={12} sm={12}>
-                  <Grid container spacing={2} p={2} pb={5}>
-                    <GridViewField heading={MACHINE.SERIAL_NO} isLoading={isLoading} children={defaultValues?.serialNo} gridSize={4} />
-                    <GridViewField heading={MACHINE.MODEL} isLoading={isLoading} children={defaultValues?.machineModel} gridSize={4} />
-                    <GridViewField
-                      heading={VIEW_FORM.ORGANIZATION}
-                      isLoading={isLoading}
-                      children={
-                        defaultValues.customer && (
-                          <Link
-                            onClick={(event) => handleCustomerDialog(event, defaultValues?.customerId)}
-                            href="#"
-                            underline="none"
-                            color={themeMode === KEY.LIGHT ? theme.palette.howick.midBlue : theme.palette.howick.orange}>
-                            {truncate(defaultValues?.customer, 21)}
-                          </Link>
-                        )
-                      }
-                      gridSize={4}
-                      country={defaultValues?.customerCountry}
-                      customerLink={PATH_CUSTOMER.customers.view(defaultValues?.customerId)}
-                      onClick={handleCustomerDialog}
-                    />
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Card>
-          </Box>
-          <Box mb={2}>
-            <Card {...GCardOption(themeMode)}>
-              <GStyledTopBorderDivider mode={themeMode} />
-              <Grid container spacing={2} px={1.5} mb={5}>
-                <GridViewTitle title={TITLE.MACHINE_INFO} />
-                <Divider variant="middle" style={{ width: '100%', marginBottom: '20px' }} />
-                <Grid item lg={12} sm={12}>
-                  <Grid container spacing={2} p={2} pb={5}>
-                    <GridViewField heading={VIEW_FORM.NAME} isLoading={isLoading} children={defaultValues?.name} gridSize={12} />
-                    <GridViewField
-                      heading={MACHINE.DEFAULT_PROFILE}
-                      isLoading={isLoading}
-                      children={
-                        Array.isArray(defaultValues?.profiles) && defaultValues?.profiles.length > 0
-                          ? defaultValues?.profiles[0]?.flange + 'X' + defaultValues?.profiles[0]?.web
-                          : TITLE.NOT_PROVIDED
-                      }
-                    />
-                    <GridViewField heading={VIEW_FORM.STATUS} isLoading={isLoading} children={defaultValues?.status} />
-                    <GridViewField heading={MACHINE.WORK_ORDER} isLoading={isLoading} children={defaultValues?.workOrderRef} />
-                    <GridViewField heading={MACHINE.FINANCING_COMPANY} isLoading={isLoading} children={defaultValues?.financialCompany} />
-                    <GridViewField heading={MACHINE.SUPPLIER} isLoading={isLoading} children={defaultValues?.supplier} />
-                    <GridViewField heading={MACHINE.SUPPORT_EXPIRATION} isLoading={isLoading} children={defaultValues?.supportExpireDate} />
-                    <GridViewField heading={MACHINE.PURCHASE_DATE} isLoading={isLoading} children={defaultValues?.purchaseDate} />
-                    <GridViewField heading={MACHINE.MANUFACTURE_DATE} isLoading={isLoading} children={defaultValues?.manufactureDate} />
-                    <GridViewField heading={MACHINE.SHIPPING_DATE} isLoading={isLoading} children={defaultValues?.shippingDate} />
-                    <GridViewField heading={MACHINE.INSTALLATION_DATE} isLoading={isLoading} children={defaultValues?.installationDate} />
-                    <GridViewField heading={VIEW_FORM.DESCRIPTION} isLoading={isLoading} children={defaultValues?.description} gridSize={12} />
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Card>
-          </Box>
-          <Box mb={4}>
-            <Card {...GCardOption(themeMode)}>
-              <GStyledTopBorderDivider mode={themeMode} />
-              <Grid container spacing={2} px={1.5} mb={5}>
-                <GridViewTitle title={TITLE.HOWICK_RESOURCES} />
-                <Divider variant="middle" style={{ width: '100%', marginBottom: '20px' }} />
-                <Grid item lg={12} sm={12}>
-                  <HowickResources value={defaultValues} isLoading={isLoading} />
-                </Grid>
-                <Grid item sm={12}>
-                  <GStyledFlexEndBox>
-                    <AuditBox value={defaultValues} />
-                  </GStyledFlexEndBox>
-                </Grid>
-              </Grid>
-            </Card>
-          </Box>
-        </Grid>
-      </Grid>
+      <MachineNav renderedTab={renderedTab} navigatePage={navigatePage} isLoading={isLoading} value={defaultValues} />
+      {renderedTab === 0 ? (
+        <MachineTab />
+      ) : renderedTab === 1 ? (
+        <ContactTab />
+      ) : renderedTab === 2 ? (
+        <SiteTab />
+      ) : renderedTab === 3 ? (
+        <Typography variant="h0">{'MACHINE PAGE'}</Typography>
+      ) : renderedTab === 4 ? (
+        <Typography variant="h0">{'SUPPORT PAGE'}</Typography>
+      ) : null}
       {customerDialog && <CustomerDialog />}
       {machineSiteDialogData && <SiteDialog />}
       {connectedMachineDialog && <MachineDialog />}
---
+++ src/section/product/machine/tabs.jsx
@@ -0,0 +1,67 @@
+import { ICON_NAME } from 'hook'
+import { KEY } from 'constant'
+
+/**
+ * Represents the tabs for the machine section.
+ * @typedef {Object} Tab
+ * @property {number} value - The value of the tab.
+ * @property {string} label - The label of the tab.
+ * @property {string} [icon] - The icon of the tab (optional).
+ *
+ * 0 - machine overview
+ * 1 - license
+ * 2 - tools
+ * 3 - profiles
+ * 4 - settings
+ * 5 - service records
+ * 6 - support
+ */
+
+/**
+ * An array of tabs for the customer section.
+ * @type {Tab[]}
+ */
+export const TABS = (value) => [
+  {
+    id: 0,
+    label: 'Machine Overview',
+    icon: ICON_NAME.CUSTOMER,
+    disabled: false
+  },
+  {
+    id: 1,
+    label: 'License',
+    icon: ICON_NAME.SITES,
+    disabled: true
+  },
+  {
+    id: 2,
+    label: 'Tools',
+    icon: ICON_NAME.CONTACTS,
+    disabled: true
+  },
+  {
+    id: 3,
+    label: 'Profiles',
+    icon: ICON_NAME.SUPPORT,
+    disabled: true
+  },
+  {
+    id: 4,
+    label: 'Settings',
+    icon: ICON_NAME.SUPPORT,
+    disabled: true
+  },
+  {
+    id: 5,
+    label: 'Service Records',
+    icon: ICON_NAME.SUPPORT,
+    disabled: true
+  },
+  {
+    id: 6,
+    label: 'Support',
+    icon: ICON_NAME.SUPPORT,
+    disabled: true
+  }
+]
