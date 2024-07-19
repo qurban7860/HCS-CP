11ff7e583ff97bf2128ab7ac4dfc71bc288dd4cb
ballyalley-o <bally.lomibao@howickltd.com>
2024-07-19 21:43:29 +1200

    HWKSC-1321: Add Customer Support Tickets page
    This commit adds a new file `tickets.jsx` under the `src/page/crm/support` directory. The file contains a functional component `CustomerSupportTickets` that renders the `CustomerLayout` component with the `TicketsTab` component inside. This addition introduces a new page for managing customer support tickets in the CRM section of the application.

---
+++ src/page/crm/support/tickets.jsx
@@ -0,0 +1,11 @@
+import { CustomerLayout, TicketsTab } from 'section/crm'
+
+const CustomerSupportTickets = () => {
+  return (
+    <CustomerLayout tab={4}>
+      <TicketsTab />
+    </CustomerLayout>
+  )
+}
+
+export default CustomerSupportTickets
