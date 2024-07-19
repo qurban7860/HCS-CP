cb6229803c1a646e2857d3a2c3935447fa904b1a
ballyalley-o <bally.lomibao@howickltd.com>
2024-07-19 21:41:26 +1200

    ```text refactor: Update query-pool.js to use fDate instead of fDateTime
    This commit updates the query-pool.js file to use the fDate function instead of the fDateTime function for filtering the 'created' field. This change improves the accuracy of the filtering process and ensures that only the date is considered, not the time.

--- src/hook/use-filter/query-pool.js
+++ src/hook/use-filter/query-pool.js
@@ -94,7 +94,7 @@ export const moduleFilter = (inputSub, filterName) => {
       filterParams?.fields?.customfield_10078?.toLowerCase().indexOf(filterName.toLowerCase()) >= 0 ||
       filterParams?.fields?.customfield_10069?.toLowerCase().indexOf(filterName.toLowerCase()) >= 0 ||
       filterParams?.fields?.customfield_10070?.value?.toLowerCase().indexOf(filterName.toLowerCase()) >= 0 ||
-      fDateTime(filterParams?.fields?.created)?.toLowerCase().indexOf(filterName.toLowerCase()) >= 0 ||
+      fDate(filterParams?.fields?.created)?.toLowerCase().indexOf(filterName.toLowerCase()) >= 0 ||
       filterParams?.fields?.summary?.toLowerCase().indexOf(filterName.toLowerCase()) >= 0 ||
       filterParams?.fields?.status?.name?.toLowerCase().indexOf(filterName.toLowerCase()) >= 0
   )
