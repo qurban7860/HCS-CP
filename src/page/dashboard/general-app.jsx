686fcfbfdb9381c2fd4365361719cee4e911b084
ballyalley-o <bally.lomibao@howickltd.com>
2024-07-23 14:38:48 +1200

    HWKSC-1285: Update GeneralAppPage component to fetch customer data and reset state
    This commit updates the GeneralAppPage component in the general-app.jsx file. It adds the necessary logic to fetch customer data, customer machines, and customer tickets using Redux actions. It also includes a cleanup function to reset the customer, customer machines, and customer ticket records when the component unmounts. These changes improve the functionality and maintainability of the GeneralAppPage component.
    
    Note: This commit message follows the established convention of using a prefix (`refactor`) to indicate code refactoring or improvements.

--- src/page/dashboard/general-app.jsx
+++ src/page/dashboard/general-app.jsx
@@ -1,17 +1,41 @@
 import { useSettingContext } from 'hook'
-import { Grid } from '@mui/material'
-import { Welcome } from 'component/widget'
+import { useSelector } from 'react-redux'
+import { dispatch } from 'store'
+import { getCustomerMachines, getCustomerTickets, getCustomer, resetCustomer, resetCustomerMachines, resetCustomerTicketRecords } from 'store/slice'
+import { Card, Grid, Typography } from '@mui/material'
+import { Welcome, DashboardWidget } from 'component/widget'
 import { GLOBAL } from 'config'
 import { toTitleCase } from 'util'
+import { useEffect, useLayoutEffect } from 'react'
 
 function GeneralAppPage() {
   const { themeMode } = useSettingContext()
+  const { customerMachines } = useSelector((state) => state.machine)
+  const { customer } = useSelector((state) => state.customer)
+  const { securityUser } = useSelector((state) => state.user)
+  const { customerTickets } = useSelector((state) => state.customerTicket)
+
+  useLayoutEffect(() => {
+    return () => {
+      dispatch(resetCustomer())
+      dispatch(resetCustomerMachines())
+      dispatch(resetCustomerTicketRecords())
+    }
+  }, [])
+
+  useEffect(() => {
+    dispatch(getCustomer(customer?._id))
+    dispatch(getCustomerMachines(customer?._id))
+    dispatch(getCustomerTickets(customer?.ref))
+  }, [customer?._id, customer?.ref, securityUser?.customer?._id])
 
   return (
     <Grid container>
       <Grid container spacing={3} mt={2}>
         <Grid item xs={12}>
           <Welcome title={toTitleCase(GLOBAL.APP_TAGLINE)} description={GLOBAL.APP_CUSTOMER_TAGLINE} />
+
+          <DashboardWidget machines={customerMachines} supportTickets={customerTickets?.issues} />
         </Grid>
       </Grid>
     </Grid>
