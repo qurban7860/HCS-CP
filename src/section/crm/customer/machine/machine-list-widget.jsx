30acb2a2c01ac58cabc5f196f42f16878f024811
ballyalley-o <bally.lomibao@howickltd.com>
2024-07-23 14:39:41 +1200

    refactor: Update GeneralAppPage component to fetch customer data and reset state

--- src/section/crm/customer/machine/machine-list-widget.jsx
+++ src/section/crm/customer/machine/machine-list-widget.jsx
@@ -7,12 +7,11 @@ import { useParams } from 'react-router-dom'
 import { dispatch } from 'store'
 import { getCustomerMachines } from 'store/slice'
 import { Grid, Typography, IconButton, Divider } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
-import { GStyledTooltip } from 'theme/style'
+import { useTheme, alpha } from '@mui/material/styles'
+import { GStyledListItemText, GStyledSpanBox, GStyledTooltip } from 'theme/style'
 import { FormHeader, SkeletonViewFormField, IconTooltip } from 'component'
 import { PATH_MACHINE } from 'route/path'
-import { GStyledListItemText, GStyledSpanBox } from 'theme/style'
-import { VARIANT, SIZE, LABEL, KEY, DECOILER, DECOILER_TYPE_ARR, FLEX } from 'constant'
+import { VARIANT, SIZE, LABEL, KEY, DECOILER_TYPE_ARR, FLEX } from 'constant'
 
 const { TYPOGRAPHY } = VARIANT
 
@@ -34,105 +33,137 @@ const MachineListWidget = ({ value, handleMachineDialog, handleMachineSiteDialog
 
   const { Icon: LocIcon, iconSrc } = useIcon(ICON_NAME.DECOILER_DEF)
 
+  // TODO: [HouseKeeping]
+  const sxProp =
+    value?.machines?.length < 5
+      ? {}
+      : {
+          position: 'relative',
+          '&::after': {
+            content: '""',
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            height: '30px',
+            backgroundImage: `linear-gradient(to bottom, rgba(255,255,255,0.1), ${alpha(theme.palette.grey[500], 0.5)})`
+          }
+        }
+
   return (
     <Grid container mb={2}>
-      <Grid item lg={12} sm={12} mb={2} bgcolor="background.paper">
+      <Grid
+        item
+        lg={12}
+        sm={12}
+        mb={2}
+        bgcolor="background.paper"
+        // TODO:if value?.machines?.length is more than 5, dont add the sx prop
+
+        sx={sxProp}>
         <GStyledSpanBox>
           <FormHeader label={LABEL.MACHINE(value?.machines?.length)} />
         </GStyledSpanBox>
-
-        <Grid container p={2}>
-          {value?.machines.length > 0 ? (
-            customerMachines.map((mach, index) => (
-              <Fragment key={index}>
-                <Grid item xs={8}>
-                  <GStyledListItemText
-                    primary={
-                      mach && (
-                        <GStyledSpanBox>
-                          <IconButton
-                            onClick={(e) => handleMachineDialog(e, mach._id)}
-                            size={SIZE.MEDIUM}
-                            color={themeMode === KEY.LIGHT ? 'grey.800' : 'common.white'}
-                            aria-label="view"
-                            target={KEY.BLANK}
-                            sx={{
-                              padding: 0,
-                              borderRadius: 2,
-                              m: 0
-                            }}>
-                            <Typography color={themeMode === KEY.LIGHT ? 'common.black' : 'grey.400'} variant={TYPOGRAPHY.H4}>
-                              {mach?.serialNo}
-                            </Typography>
-                          </IconButton>
-                          {DECOILER_TYPE_ARR.some((type) => mach?.machineModel?.name?.includes(type)) && (
-                            <GStyledTooltip
-                              title={LABEL.DECOILER_DEF}
-                              disableFocusListener
-                              placement={KEY.TOP}
-                              tooltipcolor={theme.palette.grey[500]}
-                              color={theme.palette.grey[500]}>
-                              <LocIcon icon={iconSrc} color={theme.palette.grey[500]} sx={{ height: 15 }} />
-                            </GStyledTooltip>
-                          )}
-                        </GStyledSpanBox>
-                      )
-                    }
-                    secondary={
-                      <Typography variant={TYPOGRAPHY.BODY2} color="text.secondary">
-                        {mach?.machineModel?.name}
-                      </Typography>
-                    }
-                  />
-                </Grid>
-                <Grid item xs={4} flex={1} justifyContent={KEY.FLEX_END} alignContent={KEY.RIGHT}>
-                  <GStyledSpanBox justifyContent={FLEX.FLEX_END} gap={1}>
-                    <IconTooltip
-                      title={LABEL.SITE_VIEW(mach?.serialNo)}
-                      icon={ICON_NAME.MAP_MARKER}
-                      dimension={18}
-                      onClick={(e) => handleMachineSiteDialog(e, mach._id)}
-                      color={themeMode === KEY.LIGHT ? theme.palette.howick.blue : theme.palette.howick.orange}
-                      iconOnly
-                      cursor
+        <Grid
+          container
+          sx={{
+            height: value?.machines.length > 5 ? '600px' : 'auto',
+            overflow: KEY.AUTO,
+            scrollBehavior: 'smooth'
+          }}>
+          <Grid container p={2}>
+            {value?.machines.length > 0 ? (
+              customerMachines.map((mach, index) => (
+                <Fragment key={index}>
+                  <Grid item xs={8}>
+                    <GStyledListItemText
+                      primary={
+                        mach && (
+                          <GStyledSpanBox>
+                            <IconButton
+                              onClick={(e) => handleMachineDialog(e, mach._id)}
+                              size={SIZE.MEDIUM}
+                              color={themeMode === KEY.LIGHT ? 'grey.800' : 'common.white'}
+                              aria-label="view"
+                              target={KEY.BLANK}
+                              sx={{
+                                padding: 0,
+                                borderRadius: 2,
+                                m: 0
+                              }}>
+                              <Typography color={themeMode === KEY.LIGHT ? 'common.black' : 'grey.400'} variant={TYPOGRAPHY.H4}>
+                                {mach?.serialNo}
+                              </Typography>
+                            </IconButton>
+                            {DECOILER_TYPE_ARR.some((type) => mach?.machineModel?.name?.includes(type)) && (
+                              <GStyledTooltip
+                                title={LABEL.DECOILER_DEF}
+                                disableFocusListener
+                                placement={KEY.TOP}
+                                tooltipcolor={theme.palette.grey[500]}
+                                color={theme.palette.grey[500]}>
+                                <LocIcon icon={iconSrc} color={theme.palette.grey[500]} sx={{ height: 15 }} />
+                              </GStyledTooltip>
+                            )}
+                          </GStyledSpanBox>
+                        )
+                      }
+                      secondary={
+                        <Typography variant={TYPOGRAPHY.BODY2} color="text.secondary">
+                          {mach?.machineModel?.name}
+                        </Typography>
+                      }
                     />
-                    <IconTooltip
-                      title={LABEL.NAVIGATE_TO(mach?.serialNo)}
-                      icon={ICON_NAME.OPEN_IN_NEW}
-                      dimension={18}
-                      onClick={() => window.open(PATH_MACHINE.machines.view(mach?._id), KEY.BLANK)}
-                      color={theme.palette.grey[500]}
-                      iconOnly
-                      cursor
-                    />
-                    {value?.isActive ? (
+                  </Grid>
+                  <Grid item xs={4} flex={1} justifyContent={KEY.FLEX_END} alignContent={KEY.RIGHT}>
+                    <GStyledSpanBox justifyContent={FLEX.FLEX_END} gap={1}>
                       <IconTooltip
-                        title={LABEL.ACTIVE}
-                        icon={ICON_NAME.ACTIVE}
-                        color={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
+                        title={LABEL.SITE_VIEW(mach?.serialNo)}
+                        icon={ICON_NAME.MAP_MARKER}
                         dimension={18}
-                        px={0}
-                        isActiveIcon
+                        onClick={(e) => handleMachineSiteDialog(e, mach._id)}
+                        color={themeMode === KEY.LIGHT ? theme.palette.howick.blue : theme.palette.howick.orange}
                         iconOnly
+                        cursor
                       />
-                    ) : (
-                      <IconTooltip title={LABEL.INACTIVE} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} dimension={18} iconOnly />
-                    )}
-                  </GStyledSpanBox>
-                </Grid>
-                {/* if in the last index dont add divider */}
-                {index !== customerMachines?.length - 1 && <Divider variant="fullWidth" style={{ width: '100%', marginBottom: '10px' }} />}
-              </Fragment>
-            ))
-          ) : loading ? (
-            <m.div>
-              <SkeletonViewFormField />
-            </m.div>
-          ) : (
-            <Typography variant={TYPOGRAPHY.OVERLINE1} color="text.no">
-              {LABEL.NO_MACHINE_FOUND}
-            </Typography>
-          )}
+                      <IconTooltip
+                        title={LABEL.NAVIGATE_TO(mach?.serialNo)}
+                        icon={ICON_NAME.OPEN_IN_NEW}
+                        dimension={18}
+                        onClick={() => window.open(PATH_MACHINE.machines.view(mach?._id), KEY.BLANK)}
+                        color={theme.palette.grey[500]}
+                        iconOnly
+                        cursor
+                      />
+                      {value?.isActive ? (
+                        <IconTooltip
+                          title={LABEL.ACTIVE}
+                          icon={ICON_NAME.ACTIVE}
+                          color={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
+                          dimension={18}
+                          px={0}
+                          isActiveIcon
+                          iconOnly
+                        />
+                      ) : (
+                        <IconTooltip title={LABEL.INACTIVE} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} dimension={18} iconOnly />
+                      )}
+                    </GStyledSpanBox>
+                  </Grid>
+                  {/* if in the last index dont add divider */}
+                  {index !== customerMachines?.length - 1 && <Divider variant="fullWidth" style={{ width: '100%', marginBottom: '10px' }} />}
+                </Fragment>
+              ))
+            ) : loading ? (
+              <m.div>
+                <SkeletonViewFormField />
+              </m.div>
+            ) : (
+              <Typography variant={TYPOGRAPHY.OVERLINE1} color="text.no">
+                {LABEL.NO_MACHINE_FOUND}
+              </Typography>
+            )}
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
