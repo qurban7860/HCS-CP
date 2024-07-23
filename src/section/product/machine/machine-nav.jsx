f1fd8ee2b342d7b674badbc8365765c832575997
ballyalley-o <bally.lomibao@howickltd.com>
2024-07-23 14:42:57 +1200

    refactor: Remove unused state and imports in MachineNav component

--- src/section/product/machine/machine-nav.jsx
+++ src/section/product/machine/machine-nav.jsx
@@ -1,5 +1,3 @@
-import PropTypes from 'prop-types'
-import { useSelector } from 'store'
 import { Clock, ICON_NAME, useSettingContext } from 'hook'
 import { Grid, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
