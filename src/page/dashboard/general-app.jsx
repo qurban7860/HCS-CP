e071c4ca91ac5269b83abc71ff68436e1f9c1aa3
ballyalley-o <bally.lomibao@howickltd.com>
2024-08-12 09:36:09 +1200

    disable-charts: Disable charts for the meantime, while waiting for useful data from server

--- src/page/dashboard/general-app.jsx
+++ src/page/dashboard/general-app.jsx
@@ -10,32 +10,30 @@ import { useEffect, useLayoutEffect } from 'react'
 
 function GeneralAppPage() {
   const { themeMode } = useSettingContext()
-  const { customerMachines } = useSelector((state) => state.machine)
-  const { customer } = useSelector((state) => state.customer)
-  const { securityUser } = useSelector((state) => state.user)
-  const { customerTickets } = useSelector((state) => state.customerTicket)
+  // const { customerMachines } = useSelector((state) => state.machine)
+  // const { customer } = useSelector((state) => state.customer)
+  // const { securityUser } = useSelector((state) => state.user)
+  // const { customerTickets } = useSelector((state) => state.customerTicket)
 
-  useLayoutEffect(() => {
-    return () => {
-      dispatch(resetCustomer())
-      dispatch(resetCustomerMachines())
-      dispatch(resetCustomerTicketRecords())
-    }
-  }, [])
+  // useLayoutEffect(() => {
+  //   return () => {
+  //     dispatch(resetCustomer())
+  //     dispatch(resetCustomerMachines())
+  //     dispatch(resetCustomerTicketRecords())
+  //   }
+  // }, [])
 
-  useEffect(() => {
-    dispatch(getCustomer(customer?._id))
-    dispatch(getCustomerMachines(customer?._id))
-    dispatch(getCustomerTickets(customer?.ref))
-  }, [customer?._id, customer?.ref, securityUser?.customer?._id])
+  // useEffect(() => {
+  //   dispatch(getCustomer(customer?._id))
+  //   dispatch(getCustomerMachines(customer?._id))
+  //   dispatch(getCustomerTickets(customer?.ref))
+  // }, [customer?._id, customer?.ref, securityUser?.customer?._id])
 
   return (
     <Grid container>
       <Grid container spacing={3} mt={2}>
         <Grid item xs={12}>
           <Welcome title={toTitleCase(GLOBAL.APP_TAGLINE)} description={GLOBAL.APP_CUSTOMER_TAGLINE} />
-
-          <DashboardWidget machines={customerMachines} supportTickets={customerTickets?.issues} />
         </Grid>
       </Grid>
     </Grid>
