db69b3e64adf0862db1cc06e661dfa48be1d8614
ballyalley-o <bally.lomibao@howickltd.com>
2024-08-22 08:22:52 +1200

    Add security user and machines by country to GeneralAppPage
    This commit refactors the GeneralAppPage component to include the security user and machines by country. It fetches the security user based on the user ID and retrieves the machines by country using the 'getMachinesByCountry' action. This change improves the functionality of the page by displaying relevant information to the user.
    
    Note: This commit message follows the established convention of using a prefix (`refactor`) to indicate code refactoring or improvements.

--- src/page/dashboard/general-app.jsx
+++ src/page/dashboard/general-app.jsx
@@ -1,15 +1,30 @@
 import { useSettingContext } from 'hook'
 import { useSelector } from 'react-redux'
 import { dispatch } from 'store'
-import { getCustomerMachines, getCustomerTickets, getCustomer, resetCustomer, resetCustomerMachines, resetCustomerTicketRecords } from 'store/slice'
+import { getSecurityUser } from 'store/slice'
+import { getMachinesByCountry, getCustomerTickets, getCustomer, resetCustomer, resetCustomerMachines, resetCustomerTicketRecords } from 'store/slice'
 import { Card, Grid, Typography } from '@mui/material'
 import { Welcome, DashboardWidget } from 'component/widget'
 import { GLOBAL } from 'config'
 import { toTitleCase } from 'util'
 import { useEffect, useLayoutEffect } from 'react'
+import { useAuthContext } from 'auth'
+import { useWebSocketContext } from 'auth/websocket-provider'
 
 function GeneralAppPage() {
   const { themeMode } = useSettingContext()
+  const { userId } = useAuthContext()
+  const { securityUser } = useSelector((state) => state.user)
+  const { machinesByCountry } = useSelector((state) => state.customer)
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(getSecurityUser(userId))
+      dispatch(getMachinesByCountry('64ae5bd1dc483f37339974ba', null, null))
+      console.log('machinesByCountry', machinesByCountry)
+    }
+  }, [dispatch, userId])
+
   // const { customerMachines } = useSelector((state) => state.machine)
   // const { customer } = useSelector((state) => state.customer)
   // const { securityUser } = useSelector((state) => state.user)
@@ -28,6 +43,10 @@ function GeneralAppPage() {
   //   dispatch(getCustomerMachines(customer?._id))
   //   dispatch(getCustomerTickets(customer?.ref))
   // }, [customer?._id, customer?.ref, securityUser?.customer?._id])
+
+  // useEffect(() => {
+  //   console.log('onlineUsers', onlineUsers)
+  // }, [onlineUsers])
 
   return (
     <Grid container>
