fcfd943df843859c17ecd82ac381031829f32cc3
ballyalley-o <bally.lomibao@howickltd.com>
2024-08-22 09:53:57 +1200

    HPS-1224: Add websocket event constants for Housekeeping.
    This commit adds the websocket event constants to the project. The new file 'websocket-event.js' contains a list of websocket events that can be used throughout the application. This addition enhances the functionality of the project by providing a centralized location for managing websocket events.
    
    Note: This commit message follows the established convention of using a prefix (`feat`) to indicate a new feature or enhancement.

--- src/constant/response.js
+++ src/constant/response.js
@@ -1,33 +1,35 @@
 import { fDate } from 'util'
 
 const RESPONSE = {
-  FETCH_LOADING: 'Fetching data...',
-  success: {
-    LOGIN: 'Logged In',
-    LOGOUT: 'Logged Out',
-    REDUX_PERSIST: 'Persisted states have been cleared',
-    FETCH: 'Fetched All Data',
-    FETCH_DATA: 'Fetched Data'
-  },
-  error: {
-    REDUX_PERSIST: 'Error clearing persisted states:',
-    AUTH_CONTEXT: 'useAuthContext context must be use inside AuthProvider',
-    WEBSOCKET_CONTEXT: 'useWebSocketContext context must be use inside WebSocketProvider',
-    INVALID_CREDENTIALS: 'Invalid Credentials',
-    LOGOUT: 'Unable to logout',
-    FETCH: 'Failed to fetch data'
-  },
   SCHEMA: {
     MACHINE: {
-      DATE_VALID: 'Date Should be Valid',
-      PURCHASE_DATE: 'Purchase Date',
       CUSTOMER_REQUIRED: 'Customer Is Required',
-      SHIPPING_DATE_MAX: (date) => `Shipping Date field must be at earlier than ${fDate(date)}`,
-      SHIPPING_DATE_MIN: (date) => `Shipping Date field must be at after than ${fDate(date)}`,
+      DATE_VALID: 'Date Should be Valid',
       INSTALLATION_DATE_MAX: (date) => `Installation Date field must be at earlier than ${fDate(date)}`,
-      INSTALLATION_DATE_MIN: (date) => `Installation Date field must be at after than ${fDate(date)}`
+      INSTALLATION_DATE_MIN: (date) => `Installation Date field must be at after than ${fDate(date)}`,
+      PURCHASE_DATE: 'Purchase Date',
+      SHIPPING_DATE_MAX: (date) => `Shipping Date field must be at earlier than ${fDate(date)}`,
+      SHIPPING_DATE_MIN: (date) => `Shipping Date field must be at after than ${fDate(date)}`
     }
-  }
+  },
+  error: {
+    AUTH_CONTEXT: 'useAuthContext context must be use inside AuthProvider',
+    ERR_READ_BLOB: 'Error reading the Blob',
+    FETCH: 'Failed to fetch data',
+    INVALID_ARG_NOT_BLOB: 'Invalid argument, expected a Blob',
+    INVALID_CREDENTIALS: 'Invalid Credentials',
+    LOGOUT: 'Unable to logout',
+    PARSING_JSON: (error) => `Error parsing JSON from Blob: ${error}`,
+    PARSING_WEBSOCKET: (error) => `Error parsing WebSocket message: ${error}`,
+    REDUX_PERSIST: 'Error clearing persisted states:',
+    WEBSOCKET_CONTEXT: 'useWebSocketContext context must be use inside WebSocketProvider'
+  },
+  FETCH_DATA: 'Fetched Data',
+  FETCH_LOADING: 'Fetching data...',
+  FETCH: 'Fetched All Data',
+  LOGIN: 'Logged In',
+  LOGOUT: 'Logged Out',
+  REDUX_PERSIST: 'Persisted states have been cleared'
 }
 
 export default RESPONSE
