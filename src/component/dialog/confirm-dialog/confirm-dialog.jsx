ff80a98bbb44c3f085c18d3007489975fbae74c3
ballyalley-o <bally.lomibao@howickltd.com>
2024-08-22 08:19:56 +1200

    Branding consistency: Add lazy loading for images in confirm-dialog.jsx
    This commit refactors the confirm-dialog.jsx component by adding lazy loading for images. This improvement enhances the page load performance by loading images only when they are visible on the screen.
    
    Note: This commit message follows the established convention of using a prefix (`refactor`) to indicate code refactoring or improvements.

--- src/component/dialog/confirm-dialog/confirm-dialog.jsx
+++ src/component/dialog/confirm-dialog/confirm-dialog.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types'
+import { useSettingContext } from 'hook'
+import { GStyledTopBorderDivider } from 'theme/style'
 import { Dialog, Button, DialogTitle, DialogActions, DialogContent, Divider } from '@mui/material'
 
 ConfirmDialog.propTypes = {
@@ -15,8 +17,10 @@ ConfirmDialog.defaultProps = {
 }
 
 function ConfirmDialog({ title, content, action, open, onClose, SubButton, ...other }) {
+  const { themeMode } = useSettingContext()
   return (
-    <Dialog fullWidth maxWidth="xs" open={open} onClose={onClose}>
+    <Dialog fullWidth maxWidth="xs" open={open} onClose={onClose} {...other}>
+      <GStyledTopBorderDivider mode={themeMode} />
       <DialogTitle variant="h4" sx={{ pb: 1, pt: 2 }}>
         {title}
       </DialogTitle>
