0351f038c8f466965a9b437cfba1424cefa0aaca
ballyalley-o <bally.lomibao@howickltd.com>
2024-08-22 11:29:21 +1200

    📃 docs(Documentation and Housekeeping for server route constants)

--- src/route/server.js
+++ src/route/server.js
@@ -8,87 +8,214 @@ function _url(...param) {
 }
 
 /**
- * @path {baseUrl}/api/{apiVersion}{PATH_SERVER}
+ * @path {baseUrl}/api/{apiVersion}/{...PATH_SERVER}
+ *
  */
 export const PATH_SERVER = {
-  // /configs
+  /**
+   * @module /configs
+   */
   CONFIG: _url('configs'),
-  //  /security
+  /**
+   * @module /dashboard
+   */
+  DASHBOARD: {
+    /**
+     * /dashboard
+     */
+    COUNT: _url('dashboard'),
+    /**
+     * /dashboard/machineCountries
+     */
+    MACHINE_COUNTRIES: _url('dashboard', 'machineCountries'),
+    /**
+     * /dashboard/machineModel
+     */
+    MACHINE_MODEL: _url('dashboard', 'machineModel'),
+    /**
+     * /dashboard/machineYear
+     */
+    MACHINE_YEAR: _url('dashboard', 'machineYear')
+  },
+  /**
+   * @module /security
+   */
   SECURITY: {
-    // /roles
+    /**
+     * @submodule /security/roles
+     */
     ROLES: {
-      // /
+      /**
+       * /security/roles
+       */
       list: _url('security', 'roles'),
-      // /roles/:id
+      /**
+       * /security/roles/:roleId
+       * @param {*} roleId - string
+       */
       detail: (roleId) => _url('security', 'roles', roleId)
     },
-    // /getToken
+    /**
+     * /security/getToken
+     */
     LOGIN: _url('security', 'getToken'),
-    // /multifactorverifyCode
+    /**
+     * /security/multifactorverifyCode
+     */
     MFA: _url('security', 'multifactorverifyCode'),
-    // /register
+    /**
+     * /security/register
+     */
     REGISTER: _url('security', 'register'),
-    // /logout/:userId
+    /**
+     * /security/logout/:userId
+     * @param {*} userId - string
+     */
     LOGOUT: (userId) => _url('security', 'logout', userId),
-    // /invites/sendUserInvite/:userId
+    /**
+     * /security/invites/sendUserInvite/:userId
+     * @param {*} userId - string
+     */
     SEND_USER_INVITE: (userId) => _url('security', 'invites', 'sendUserInvite', userId),
-    // /users
+    /**
+     * @submodule /security/users
+     */
     USER: {
-      // /
+      /**
+       *  /security/users
+       */
       list: _url('security', 'users'),
-      // /:id
+      /**
+       * /security/users/:userId
+       * @param {*} userId - string
+       */
       detail: (userId) => _url('security', 'users', userId),
-      // /updatePassword/:userId
+      /**
+       * /security/users/updatePassword/:userId
+       * @param {*} userId - string
+       */
       updatePassword: (userId) => _url('security', 'users', 'updatePassword', userId),
-      // /:userId/signinlogs/
+      /**
+       * /security/users/:userId/signinlogs
+       * @param {*} userId - string
+       */
       signInLogs: (userId) => _url('security', 'users', userId, 'signinlogs')
     }
   },
-
-  // /products
+  /**
+   * @module /products
+   */
   PRODUCT: {
+    /**
+     * @submodule /products/machines
+     */
     MACHINE: {
-      // /machines/:id
+      /**
+       * /products/machines/:machineId
+       * @param {*} machineId - string
+       */
       detail: (machineId) => _url('products', 'machines', machineId),
-      // /machines
+      /**
+       * /products/machines
+       */
       list: _url('products', 'machines'),
-      // /machines?customer=:customerId&isArchived=:isArchived
+      /**
+       * /products/machines?customer={:customerId}&isArchived={:isArchived}
+       * @param {*} customerId - string
+       * @param {*} isArchived - boolean
+       */
       viaCustomer: (customerId, isArchived) => _url('products', 'machines', `?customer=${customerId}&isArchived=${isArchived}`)
     },
+    /**
+     * @submodule - /products/models
+     */
     MODEL: {
-      // /models/:id
-      detail: (modelId) => _url('products', 'models', modelId),
-      // /models
-      list: _url('products', 'models')
+      /**
+       * /products/models
+       */
+      list: _url('products', 'models'),
+      /**
+       * /products/models/:modelId
+       * @param {*} modelId
+       */
+      detail: (modelId) => _url('products', 'models', modelId)
     }
   },
-
-  // /crm
+  /**
+   * @module /crm
+   */
   CRM: {
+    /**
+     * @submodule /crm/customers
+     */
     CUSTOMER: {
-      // /customers/:id
-      detail: (customerId) => _url('crm', 'customers', customerId),
-      // /customer
+      /**
+       * /crm/customers
+       */
       list: _url('crm', 'customers'),
-      // /customers/:id/contacts
+      /**
+       * /crm/customers/:customerId
+       * @param {*} customerId - string
+       */
+      detail: (customerId) => _url('crm', 'customers', customerId),
+      /**
+       * /crm/customers/:customerId/contacts
+       * @param {*} customerId - string
+       */
       listContact: (customerId) => _url('crm', 'customers', customerId, 'contacts'),
-      // /customers/:id/contacts/:contactId
+      /**
+       * /crm/customers/:customerId/contacts/:contactId
+       * @param {*} customerId - string
+       * @param {*} contactId - string
+       */
       contactDetail: (customerId, contactId) => _url('crm', 'customers', customerId, 'contacts', contactId),
-      // /customers/:id/sites
+      /**
+       * /crm/customers/:customerId/sites
+       * @param {*} customerId - string
+       */
       listSite: (customerId) => _url('crm', 'customers', customerId, 'sites'),
-      // /customers/:id/sites/:siteId
+      /**
+       * /crm/customers/:customerId/sites/:siteId
+       * @param {*} customerId - string
+       * @param {*} siteId - string
+       */
       siteDetail: (customerId, siteId) => _url('crm', 'customers', customerId, 'sites', siteId),
-      // /customers/:id/machine
+      /**
+       * /crm/customers/:customerId/machine
+       * @param {*} customerId - string
+       */
       listMachine: (customerId) => _url('crm', 'customers', customerId, 'machine')
     }
   },
-
-  // [NOTE] this is current under the name jira module
-  // /support (jira)
+  /**
+   * NOTE: this is current under the name jira module
+   *
+   * @module /jira
+   */
   SUPPORT: {
-    // /tickets
+    /**
+     * /jira/tickets
+     */
     TICKETS: _url('jira', 'tickets'),
-    // /tickets/:ticketId
+    /**
+     * /jira/tickets/:ticketId
+     * @param {*} ticketId
+     * @returns
+     */
     TICKET: (ticketId) => _url('jira', 'tickets', ticketId)
+  },
+  /**
+   * @module /logs
+   */
+  LOG: {
+    /**
+     * @submodule /logs/erp
+     */
+    ERP: {
+      /**
+       * /logs/erp/graph
+       */
+      GRAPH: _url('logs', 'erp', 'graph')
+    }
   }
 }
