b564e036976dccf2a7ffbc717707e71464ecbdfd
ballyalley-o <bally.lomibao@howickltd.com>
2024-08-28 08:08:49 +1200

    Add LOGOUT icon to ICON_WEB in icon-directory.js
    This commit adds the LOGOUT icon to the ICON_WEB object in the icon-directory.js file. The LOGOUT icon will be used for the logout functionality in the web application. This enhancement improves the user experience by providing a clear and recognizable icon for the logout feature.

--- src/config/nav-config.js
+++ src/config/nav-config.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
+import { useLocale } from 'locale'
 import { PATH_DASHBOARD, PATH_CUSTOMER, PATH_MACHINE, PATH_SUPPORT } from 'route/path'
 
 function NavConfiguration() {
+  const { t } = useLocale()
   const [navConfig, setNavConfig] = useState([
     {
       subheader: 'general',
       items: [
-        { title: 'Home', path: PATH_DASHBOARD.general.app },
+        { title: t('home'), path: PATH_DASHBOARD.general.app },
         { title: 'Machine', path: PATH_MACHINE.machines.list },
         { title: 'Organization', path: PATH_CUSTOMER.customers.list },
         { title: 'Document', path: '/documents', children: null, disabled: true },
