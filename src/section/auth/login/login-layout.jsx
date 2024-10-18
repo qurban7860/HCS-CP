306e86a3288501526fab58f9f8bbe072664c928f
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-18 15:53:56 +1300

    #1240: [UX] enhance LoginLayout with responsive design and improved structure

--- src/section/auth/login/login-layout.jsx
+++ src/section/auth/login/login-layout.jsx
@@ -1,42 +1,44 @@
 import PropTypes from 'prop-types'
 import { useSettingContext } from 'hook'
-import { Typography, Stack, Grid } from '@mui/material'
+import { Typography, Stack, Grid, Box } from '@mui/material'
 import { Logo } from 'component/logo'
 import { GLOBAL } from 'global'
 import { FLEX, KEY, TYPOGRAPHY } from 'constant'
 import { StyledRoot, StyledContent } from '../style'
 
 LoginLayout.propTypes = {
-  title: PropTypes.string,
-  children: PropTypes.node,
-  illustration: PropTypes.string
+ title: PropTypes.string,
+ children: PropTypes.node,
+ illustration: PropTypes.string
 }
 
 function LoginLayout({ children, illustration, title }) {
-  const { themeMode } = useSettingContext()
-  title = title || GLOBAL.APP_BRANDING
-  return (
-    <StyledRoot mode={themeMode}>
-      <StyledContent>
-        <Grid sx={{ display: FLEX.FLEX, justifyContent: KEY.CENTER, mb: -3 }} alignItems={KEY.CENTER} spacing={2} container>
-          <Grid item>
-            <Logo sx={{ width: '100%', p: 1, pointerEvents: KEY.NONE }} />
-            <Stack sx={{ alignItems: KEY.CENTER }}>
-              <Typography variant={TYPOGRAPHY.H} sx={{ mt: -2 }}>
-                {title.toUpperCase()}
-              </Typography>
-            </Stack>
-            <Stack sx={{ alignItems: KEY.END }}>
-              <Typography variant={TYPOGRAPHY.BODY2} sx={{ mb: 4, mt: -1 }}>
-                {GLOBAL.ENV} {GLOBAL.VERSION}
-              </Typography>
-            </Stack>
-          </Grid>
-        </Grid>
-        <Stack sx={{ width: '100%' }}> {children} </Stack>
-      </StyledContent>
-    </StyledRoot>
-  )
+ const { themeMode } = useSettingContext()
+ title = title || GLOBAL.APP_BRANDING
+ return (
+  <StyledRoot mode={themeMode}>
+   <StyledContent>
+    <Grid sx={{ display: FLEX.FLEX, justifyContent: KEY.CENTER, mt: 2, mb: -3 }} alignItems={KEY.CENTER} spacing={2} container>
+     <Grid item>
+      <Grid container flex={1} justifyContent="center">
+       <Logo sx={{ width: { xs: '80px', sm: '120px', md: '150px' }, pointerEvents: KEY.NONE }} />
+      </Grid>
+      <Stack sx={{ alignItems: KEY.CENTER }}>
+       <Typography className="portal-header" variant={TYPOGRAPHY.H} sx={{ mt: { xs: -3, lg: -5 } }}>
+        {title.toUpperCase()}
+       </Typography>
+      </Stack>
+      <Stack sx={{ alignItems: KEY.END }}>
+       <Typography variant={TYPOGRAPHY.BODY2} sx={{ mb: 5, mt: -1 }}>
+        {GLOBAL.ENV} {GLOBAL.VERSION}
+       </Typography>
+      </Stack>
+     </Grid>
+    </Grid>
+    <Box sx={{ width: { xs: '300px', sm: '400px' } }}> {children} </Box>
+   </StyledContent>
+  </StyledRoot>
+ )
 }
 
 export default LoginLayout
