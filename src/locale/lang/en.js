43ddfd2d2cabe9735a8039f36187684131be7fdf
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-18 15:53:04 +1300

    [localization] en lang update

--- src/locale/lang/en.js
+++ src/locale/lang/en.js
@@ -1,115 +1,116 @@
 const en = {
-  app: `app`,
-  user: `user`,
-  list: `list`,
-  edit: `edit`,
-  shop: `shop`,
-  blog: `blog`,
-  post: `post`,
-  mail: `mail`,
-  chat: `chat`,
-  cards: `cards`,
-  posts: `posts`,
-  create: `create`,
-  change_password: {
-    label: `Change Password`
-  },
-  customer: {
-    label: `Customer`
-  },
-  customer_group: {
-    label: `Customer Group`
-  },
-  home: {
-    local: `home`,
-    label: `Home`
-  },
-  machine: {
-    local: `machine`,
-    label: `Machine`
-  },
-  organization: {
-    local: `organization`,
-    label: `Organization`
-  },
-  organizations: {
-    local: `organizations`,
-    label: `Organizations`
-  },
-  document: {
-    local: `document`,
-    label: `Document`
-  },
-  support: {
-    local: `support`,
-    label: `Support`
-  },
-  portal: {
-    local: `portal`,
-    label: `Portal`
-  },
-  support_tickets:{
-    local: `support_tickets`,
-    label: `Support Tickets`
-  },
-  sites:{
-    local: `sites`,
-    label: `Sites`
-  },
-  contacts: {
-    local: `contacts`,
-    label: `Contacts`
-  },
-  customer_overview: {
-    local: `customer_overview`,
-    label: `Customer Overview`
-  },
-  kanban: `kanban`,
-  general: `general`,
-  banking: `banking`,
-  booking: `booking`,
-  profile: {
-    local: `profile`,
-    label: `Profile`
-  },
-  display_settings: {
-    label: `Display Settings`
-  },
-  logout: {
-    label: `Logout`
-  },
-  language: {
-    label: `Language`
-  },
-  notifications: {
-    label: `Notifications`,
-    no_notif: `You have no new notifications`
-  },
-  account: `account`,
-  product: `product`,
-  invoice: `invoice`,
-  details: `details`,
-  checkout: `checkout`,
-  calendar: `calendar`,
-  analytics: `analytics`,
-  ecommerce: `e-commerce`,
-  management: `management`,
-  menu_level: `menu level`,
-  menu_level_2a: `menu level 2a`,
-  menu_level_2b: `menu level 2b`,
-  menu_level_3a: `menu level 3a`,
-  menu_level_3b: `menu level 3b`,
-  menu_level_4a: `menu level 4a`,
-  menu_level_4b: `menu level 4b`,
-  item_disabled: `item disabled`,
-  item_label: `item label`,
-  item_caption: `item caption`,
-  item_external_link: `item external link`,
-  description: `description`,
-  other_cases: `other cases`,
-  item_by_roles: `item by roles`,
-  only_admin_can_see_this_item: `Only admin can see this item`,
-  app_customer_tagline: `Portal for convenient and efficient management of machines, their updates and support.`
+ register_agreement: 'By clicking "Register" you agree to our <1>Privacy Policy</1>.',
+ app: `app`,
+ user: `user`,
+ list: `list`,
+ edit: `edit`,
+ shop: `shop`,
+ blog: `blog`,
+ post: `post`,
+ mail: `mail`,
+ chat: `chat`,
+ cards: `cards`,
+ posts: `posts`,
+ create: `create`,
+ change_password: {
+  label: `Change Password`
+ },
+ customer: {
+  label: `Customer`
+ },
+ customer_group: {
+  label: `Customer Group`
+ },
+ home: {
+  local: `home`,
+  label: `Home`
+ },
+ machine: {
+  local: `machine`,
+  label: `Machine`
+ },
+ organization: {
+  local: `organization`,
+  label: `Organization`
+ },
+ organizations: {
+  local: `organizations`,
+  label: `Organizations`
+ },
+ document: {
+  local: `document`,
+  label: `Document`
+ },
+ support: {
+  local: `support`,
+  label: `Support`
+ },
+ portal: {
+  local: `portal`,
+  label: `Portal`
+ },
+ support_tickets: {
+  local: `support_tickets`,
+  label: `Support Tickets`
+ },
+ sites: {
+  local: `sites`,
+  label: `Sites`
+ },
+ contacts: {
+  local: `contacts`,
+  label: `Contacts`
+ },
+ customer_overview: {
+  local: `customer_overview`,
+  label: `Customer Overview`
+ },
+ kanban: `kanban`,
+ general: `general`,
+ banking: `banking`,
+ booking: `booking`,
+ profile: {
+  local: `profile`,
+  label: `Profile`
+ },
+ display_settings: {
+  label: `Display Settings`
+ },
+ logout: {
+  label: `Logout`
+ },
+ language: {
+  label: `Language`
+ },
+ notifications: {
+  label: `Notifications`,
+  no_notif: `You have no new notifications`
+ },
+ account: `account`,
+ product: `product`,
+ invoice: `invoice`,
+ details: `details`,
+ checkout: `checkout`,
+ calendar: `calendar`,
+ analytics: `analytics`,
+ ecommerce: `e-commerce`,
+ management: `management`,
+ menu_level: `menu level`,
+ menu_level_2a: `menu level 2a`,
+ menu_level_2b: `menu level 2b`,
+ menu_level_3a: `menu level 3a`,
+ menu_level_3b: `menu level 3b`,
+ menu_level_4a: `menu level 4a`,
+ menu_level_4b: `menu level 4b`,
+ item_disabled: `item disabled`,
+ item_label: `item label`,
+ item_caption: `item caption`,
+ item_external_link: `item external link`,
+ description: `description`,
+ other_cases: `other cases`,
+ item_by_roles: `item by roles`,
+ only_admin_can_see_this_item: `Only admin can see this item`,
+ app_customer_tagline: `Portal for convenient and efficient management of machines, their updates and support.`
 }
 
 export default en
