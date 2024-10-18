20d6de9af00da7d276338f0c959fd733f67315bf
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-18 15:52:12 +1300

    #1422: [validation] add SERIAL_NO regex for five-digit serial number validation

--- src/constant/regex.js
+++ src/constant/regex.js
@@ -1,6 +1,7 @@
 const REGEX = {
-  EMAIL: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-  ERROR_CODE: /^[4][0-9][0-9]$/,
+ EMAIL: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+ ERROR_CODE: /^[4][0-9][0-9]$/,
+ SERIAL_NO: /^\d{5}$/
 }
 
 export default REGEX
