14f762eb753abef13e4529a745bfbd5b93b69eda
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-21 10:26:28 +1300

    update login layout with gsap

--- src/section/auth/login/login-layout.jsx
+++ src/section/auth/login/login-layout.jsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 import { useSettingContext } from 'hook'
 import { Typography, Stack, Grid, Box } from '@mui/material'
-import { Logo } from 'component/logo'
+import { Logo, PortalLogo } from 'component'
 import { GLOBAL } from 'global'
 import { FLEX, KEY, TYPOGRAPHY } from 'constant'
 import { StyledRoot, StyledContent } from '../style'
@@ -12,24 +14,42 @@ LoginLayout.propTypes = {
  illustration: PropTypes.string
 }
 
+gsap.registerPlugin(useGSAP)
 function LoginLayout({ children, illustration, title }) {
  const { themeMode } = useSettingContext()
  title = title || GLOBAL.APP_BRANDING
+
+ useGSAP(() => {
+  const introTl = gsap.timeline()
+
+  introTl
+   .set('.howick-logo', { opacity: 1 })
+   .from('.portal-header', {
+    scale: 1,
+    opacity: 0,
+    ease: 'power4.in',
+    delay: 0.3,
+    stagger: 1
+   })
+   .from('.portal-version', {
+    opacity: 0,
+    y: 10
+   })
+ })
+
  return (
   <StyledRoot mode={themeMode}>
    <StyledContent>
     <Grid sx={{ display: FLEX.FLEX, justifyContent: KEY.CENTER, mt: 2, mb: -3 }} alignItems={KEY.CENTER} spacing={2} container>
      <Grid item>
-      <Grid container flex={1} justifyContent="center">
+      <Grid container flex={1} justifyContent='center'>
        <Logo sx={{ width: { xs: '80px', sm: '120px', md: '150px' }, pointerEvents: KEY.NONE }} />
       </Grid>
       <Stack sx={{ alignItems: KEY.CENTER }}>
-       <Typography className="portal-header" variant={TYPOGRAPHY.H} sx={{ mt: { xs: -3, lg: -5 } }}>
-        {title.toUpperCase()}
-       </Typography>
+       <PortalLogo className='portal-header' sx={{ width: { xs: '100px', sm: '140px', md: '250px' }, mt: { xs: -2, lg: -3 } }} />
       </Stack>
       <Stack sx={{ alignItems: KEY.END }}>
-       <Typography variant={TYPOGRAPHY.BODY2} sx={{ mb: 5, mt: -1 }}>
+       <Typography className='portal-version' variant={TYPOGRAPHY.BODY2} sx={{ mb: 5, mt: 1 }}>
         {GLOBAL.ENV} {GLOBAL.VERSION}
        </Typography>
       </Stack>
