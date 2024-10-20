260063ec0c7552377e4512ad7adc9d0f6be69ece
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-21 10:05:34 +1300

    #1452 [prettierrc]: propagation of the prettier config update of asset dir

--- src/component/viewform/style.jsx
+++ src/component/viewform/style.jsx
@@ -1,50 +1,48 @@
 import { styled } from '@mui/material/styles'
 import { Typography, Grid, Chip, Box } from '@mui/material'
 import { KEY } from 'constant'
-import { ASSET } from 'config'
+import { ASSET } from 'config/asset-directory'
 
 export const StyledDefaultTypography = styled(Typography)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  whiteSpace: 'pre-line',
-  wordBreak: 'break-word'
+ display: 'flex',
+ alignItems: 'center',
+ whiteSpace: 'pre-line',
+ wordBreak: 'break-word'
 }))
 
-export const StyledFieldGrid = styled(({ theme, mode, isMachineView, isNoBg, ...other }) => <Grid {...other} />)(
-  ({ theme, mode, isMachineView, isNoBg }) => ({
-    overflowWrap: 'break-word',
-    backgroundColor:
-      mode === KEY.LIGHT && !isMachineView && !isNoBg
-        ? theme.palette.grey[300]
-        : (isMachineView || isNoBg) && mode === KEY.LIGHT
-        ? 'transparent'
-        : (isMachineView || isNoBg) && mode === KEY.DARK
-        ? theme.palette.grey[900]
-        : theme.palette.grey[800],
-    padding: '0.5rem 1rem',
-    border: !isMachineView ? 'none' : `1px solid ${mode === KEY.LIGHT ? theme.palette.grey[300] : theme.palette.grey[700]}`,
-    borderRadius: isMachineView && theme.spacing(1)
-  })
-)
+export const StyledFieldGrid = styled(({ theme, mode, isMachineView, isNoBg, ...other }) => <Grid {...other} />)(({ theme, mode, isMachineView, isNoBg }) => ({
+ overflowWrap: 'break-word',
+ backgroundColor:
+  mode === KEY.LIGHT && !isMachineView && !isNoBg
+   ? theme.palette.grey[300]
+   : (isMachineView || isNoBg) && mode === KEY.LIGHT
+   ? 'transparent'
+   : (isMachineView || isNoBg) && mode === KEY.DARK
+   ? theme.palette.grey[900]
+   : theme.palette.grey[800],
+ padding: '0.5rem 1rem',
+ border: !isMachineView ? 'none' : `1px solid ${mode === KEY.LIGHT ? theme.palette.grey[300] : theme.palette.grey[700]}`,
+ borderRadius: isMachineView && theme.spacing(1)
+}))
 
 export const StyledChipGrid = styled(Grid)(({ theme }) => ({
-  marginTop: theme.spacing(-3),
-  marginBottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  whiteSpace: 'pre-line',
-  wordBreak: 'break-word'
+ marginTop: theme.spacing(-3),
+ marginBottom: 0,
+ display: 'flex',
+ alignItems: 'center',
+ whiteSpace: 'pre-line',
+ wordBreak: 'break-word'
 }))
 
 export const StyledFieldChip = styled(({ theme, ...other }) => <Chip {...other} />)(({ theme, mode }) => ({
-  margin: theme.spacing(0.2),
-  borderRadius: theme.spacing(0.4),
-  // border: `1px solid ${mode === KEY.LIGHT ? theme.palette.grey[100] : theme.palette.grey[700]}`,
-  backgroundColor: mode === KEY.LIGHT ? theme.palette.grey[400] : theme.palette.grey[700]
+ margin: theme.spacing(0.2),
+ borderRadius: theme.spacing(0.4),
+ // border: `1px solid ${mode === KEY.LIGHT ? theme.palette.grey[100] : theme.palette.grey[700]}`,
+ backgroundColor: mode === KEY.LIGHT ? theme.palette.grey[400] : theme.palette.grey[700]
 }))
 
 export const StyledFlagBox = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  marginLeft: 8
+ display: 'flex',
+ alignItems: 'center',
+ marginLeft: 8
 }))
