5d9eb583cb7bd544e1fe5fbde92d2b2fa98a0be5
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-24 17:06:49 +1300

    #1435: added apply-sort for updated table

---
+++ src/util/apply-sort.js
@@ -0,0 +1,11 @@
+export function applySort({ inputData, comparator }) {
+ const stabilizedThis = inputData && inputData.map((el, index) => [el, index])
+ stabilizedThis.sort((a, b) => {
+  const order = comparator(a[0], b[0])
+  if (order !== 0) return order
+  return a[1] - b[1]
+ })
+
+ inputData = stabilizedThis.map(el => el[0])
+ return inputData
+}
