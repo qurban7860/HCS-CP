e4b9f667154fa750c13b7b7af29b9e14b95d6343
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-24 17:11:50 +1300

    refactor FallbackButton component for improved formatting and readability

--- src/section/fallback/fallback-button.jsx
+++ src/section/fallback/fallback-button.jsx
@@ -4,20 +4,20 @@ import { BUTTON, COLOR, KEY } from 'constant'
 import { PATH_DASHBOARD } from 'route/path'
 
 const FallbackButton = () => {
-  const { themeMode } = useSettingContext()
-  return (
-    <Button
-      onClick={() => window.open(PATH_DASHBOARD.general.app, '_self')}
-      variant={themeMode === KEY.LIGHT ? 'contained' : 'contained'}
-      size="small"
-      sx={{
-        color: themeMode === KEY.LIGHT ? 'grey.300' : 'common.white',
-        backgroundColor: themeMode === KEY.LIGHT ? 'howick.bronze' : COLOR.PRIMARY,
-        borderColor: themeMode === KEY.LIGHT ? 'howick.darkBlue' : 'common.white'
-      }}>
-      {BUTTON.GO_BACK}
-    </Button>
-  )
+ const { themeMode } = useSettingContext()
+ return (
+  <Button
+   onClick={() => window.open(PATH_DASHBOARD.general.app, '_self')}
+   variant={themeMode === KEY.LIGHT ? 'contained' : 'contained'}
+   size='small'
+   sx={{
+    color: themeMode === KEY.LIGHT ? 'grey.300' : 'common.white',
+    backgroundColor: themeMode === KEY.LIGHT ? 'howick.bronze' : COLOR.PRIMARY,
+    borderColor: themeMode === KEY.LIGHT ? 'howick.darkBlue' : 'common.white'
+   }}>
+   {BUTTON.GO_BACK}
+  </Button>
+ )
 }
 
 export default FallbackButton
