3bda719c7cf73bf3dc7a61e9dc14e9f47097fbfb
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-24 17:13:33 +1300

    🐳 chore(#1435: [assets] add dark grey logo asset and improve icon formatting)

--- src/config/icon-directory.js
+++ src/config/icon-directory.js
@@ -11,186 +11,189 @@ import { normalizer } from 'util/format/normalizer'
  */
 
 function _getIcon(icon) {
-  return `/asset/icon/${icon}.svg`
+ return `/asset/icon/${icon}.svg`
 }
 
 function _getCountryIcon(icon) {
-  return `/asset/svg/flag/${icon}.svg`
+ return `/asset/svg/flag/${icon}.svg`
 }
 
 function _getBrandIcon(icon) {
-  return `/asset/brand/${icon}.svg`
+ return `/asset/brand/${icon}.svg`
 }
 
 function _getSvgPriorityIconByUrl(priority) {
-  const normalizerpriority = normalizer(priority)
-  return `${GLOBAL.JIRA_PRIORITY_ICON_URL}/${normalizerpriority}.svg`
+ const normalizerpriority = normalizer(priority)
+ return `${GLOBAL.JIRA_PRIORITY_ICON_URL}/${normalizerpriority}.svg`
 }
 
 function _getWebIcon(icon) {
-  return `mdi:${icon}`
+ return `mdi:${icon}`
 }
 
 function _getAnimatedIcon(icon) {
-  return `line-md:${icon}`
+ return `line-md:${icon}`
 }
 
 function _getMeteoIcon(icon) {
-  return `meteocons:${icon}`
+ return `meteocons:${icon}`
 }
 
 // add icons that are from MDI
 const ICON_WEB = {
-  ACTIVE: _getWebIcon('checkbox-marked-circle'),
-  ALERT_OUTLINE: _getWebIcon('alert-triangle-outline'),
-  ALERT_OCTAGON: _getWebIcon('alert-octagon'),
-  BLUR_ON: _getWebIcon('blur'),
-  CONTACT: _getWebIcon('contacts'),
-  CHEVRON_RIGHT: _getWebIcon('chevron-right'),
-  CLOSE: _getWebIcon('close'),
-  BACK: _getWebIcon('chevron-left'),
-  A_BACK: _getAnimatedIcon('chevron-small-left'),
-  CHECK_CIRCLE: _getWebIcon('checkbox-marked-circle'),
-  CHECK_CICLE_OUTLINE: _getWebIcon('check-circle-outline'),
-  CONTRAST_BOX: _getWebIcon('contrast-box'),
-  CONTRAST_ON: _getWebIcon('select-inverse'),
-  CONTRAST_OFF: _getWebIcon('select-off'),
-  CURRENTLY_EMPLOYED: _getWebIcon('person-badge'),
-  DANGER: _getWebIcon('dangerous'),
-  EYE: _getWebIcon('eye'),
-  EYE_OFF: _getWebIcon('eye-off'),
-  EDIT: _getWebIcon('pencil'),
-  ERROR: _getWebIcon('error'),
-  ERROR_OUTLINE: _getWebIcon('error-outline'),
-  FULLSCREEN: _getWebIcon('fullscreen'),
-  FULLSCREEN_EXIT: _getWebIcon('fullscreen-exit'),
-  INFO: _getWebIcon('info-outline'),
-  INACTIVE: _getWebIcon('minus-circle'),
-  INSTALL: _getWebIcon('hammer-wrench'),
-  JIRA: _getWebIcon('jira'),
-  LOGOUT: _getWebIcon('logout'),
-  MANUFACTURE: _getWebIcon('factory'),
-  MAGNIFY_IN: _getWebIcon('magnify-plus'),
-  MAGNIFY_OUT: _getWebIcon('magnify-minus'),
-  MAIN_SITE: _getWebIcon('office-building'),
-  MODE_LIGHT_A: _getMeteoIcon('clear-day-fill'),
-  MODE_DARK_A: _getMeteoIcon('clear-night-fill'),
-  PRIORITY: (priority) => _getSvgPriorityIconByUrl(priority),
-  READ_MORE: _getWebIcon('read-more'),
-  NOT_EMPLOYED: _getWebIcon('person-badge-alert'),
-  OPEN_IN_NEW: _getWebIcon('open-in-new'),
-  QUICK: _getWebIcon('lightning-bolt'),
-  PHONE: _getWebIcon('phone'),
-  REFRESH: _getWebIcon('refresh'),
-  SAVE: _getWebIcon('content-save'),
-  SEARCH: _getWebIcon('magnify'),
-  SETTING: _getWebIcon('cog'),
-  STATUS: _getWebIcon('arrow-up-bold-circle-outline'),
-  SHIPPING: _getWebIcon('airplane'),
-  SUPPORT: _getWebIcon('support'),
-  PURCHASED: _getWebIcon('store'),
-  TICKET: _getWebIcon('ticket-confirmation'),
-  TRAFFIC_CONE: _getWebIcon('traffic-cone'),
-  TRANSFER: _getWebIcon('transfer'),
-  TIX_STATUS: _getWebIcon('chevron-up'),
-  WARNING: _getWebIcon('warning')
+ ACTIVE: _getWebIcon('checkbox-marked-circle'),
+ ALERT_OUTLINE: _getWebIcon('alert-triangle-outline'),
+ ALERT_OCTAGON: _getWebIcon('alert-octagon'),
+ BLUR_ON: _getWebIcon('blur'),
+ CONTACT: _getWebIcon('contacts'),
+ CHEVRON_RIGHT: _getWebIcon('chevron-right'),
+ CLOSE: _getWebIcon('close'),
+ BACK: _getWebIcon('chevron-left'),
+ A_BACK: _getAnimatedIcon('chevron-small-left'),
+ CHECK_CIRCLE: _getWebIcon('checkbox-marked-circle'),
+ CHECK_CICLE_OUTLINE: _getWebIcon('check-circle-outline'),
+ CONTRAST_BOX: _getWebIcon('contrast-box'),
+ CONTRAST_ON: _getWebIcon('select-inverse'),
+ CONTRAST_OFF: _getWebIcon('select-off'),
+ CURRENTLY_EMPLOYED: _getWebIcon('person-badge'),
+ DANGER: _getWebIcon('dangerous'),
+ EYE: _getWebIcon('eye'),
+ EYE_OFF: _getWebIcon('eye-off'),
+ EDIT: _getWebIcon('pencil'),
+ ERROR: _getWebIcon('error'),
+ ERROR_OUTLINE: _getWebIcon('error-outline'),
+ COLUMN: _getWebIcon('view-column'),
+ FULLSCREEN: _getWebIcon('fullscreen'),
+ FULLSCREEN_EXIT: _getWebIcon('fullscreen-exit'),
+ INFO: _getWebIcon('info-outline'),
+ INACTIVE: _getWebIcon('minus-circle'),
+ INSTALL: _getWebIcon('hammer-wrench'),
+ JIRA: _getWebIcon('jira'),
+ LOGOUT: _getWebIcon('logout'),
+ MANUFACTURE: _getWebIcon('factory'),
+ MAGNIFY_IN: _getWebIcon('magnify-plus'),
+ MAGNIFY_OUT: _getWebIcon('magnify-minus'),
+ MAIN_SITE: _getWebIcon('office-building'),
+ MODE_LIGHT_A: _getMeteoIcon('clear-day-fill'),
+ MODE_DARK_A: _getMeteoIcon('clear-night-fill'),
+ PRIORITY: priority => _getSvgPriorityIconByUrl(priority),
+ READ_MORE: _getWebIcon('read-more'),
+ NOT_EMPLOYED: _getWebIcon('person-badge-alert'),
+ OPEN_IN_NEW: _getWebIcon('open-in-new'),
+ QUICK: _getWebIcon('lightning-bolt'),
+ PHONE: _getWebIcon('phone'),
+ REFRESH: _getWebIcon('refresh'),
+ SAVE: _getWebIcon('content-save'),
+ SEARCH: _getWebIcon('magnify'),
+ SETTING: _getWebIcon('cog'),
+ STATUS: _getWebIcon('arrow-up-bold-circle-outline'),
+ SHIPPING: _getWebIcon('airplane'),
+ SUPPORT: _getWebIcon('support'),
+ PURCHASED: _getWebIcon('store'),
+ TICKET: _getWebIcon('ticket-confirmation'),
+ TRAFFIC_CONE: _getWebIcon('traffic-cone'),
+ TRASH: _getWebIcon('trash-can'),
+ TRANSFER: _getWebIcon('transfer'),
+ TIX_STATUS: _getWebIcon('chevron-up'),
+ WARNING: _getWebIcon('warning')
 }
 
 // add icons that are saved locally
 const ICON_LOC = {
-  BILLING_CONTACT: _getIcon('contact-billing'),
-  DECOILER_DEF: _getIcon('decoiler-def'),
-  DECOILER_1_5T: _getIcon('decoiler_1-5t'),
-  DECOILER_3T: _getIcon('decoiler_3t'),
-  DECOILER_5T: _getIcon('decoiler_5t'),
-  DECOILER_6T: _getIcon('decoiler_6t'),
-  EXPAND: _getIcon('expand-outline'),
-  FRAMA: _getIcon('frama-def-1'),
-  FRAMA_3200: _getIcon('frama-3200'),
-  HOWICK_LOGO: _getIcon('howick-logo'),
-  HOWICK_LOGO_DARK: _getIcon('howick-logo-dark'),
-  MAP_MARKER: _getIcon('howick-marker-icon'),
-  MODE_LIGHT: _getIcon('sun'),
-  MODE_DARK: _getIcon('moon'),
-  PARENT: _getIcon('parent'),
-  SETTING: _getIcon('setting'),
-  US: _getCountryIcon('usa')
+ BILLING_CONTACT: _getIcon('contact-billing'),
+ DECOILER_DEF: _getIcon('decoiler-def'),
+ DECOILER_1_5T: _getIcon('decoiler_1-5t'),
+ DECOILER_3T: _getIcon('decoiler_3t'),
+ DECOILER_5T: _getIcon('decoiler_5t'),
+ DECOILER_6T: _getIcon('decoiler_6t'),
+ EXPAND: _getIcon('expand-outline'),
+ FRAMA: _getIcon('frama-def-1'),
+ FRAMA_3200: _getIcon('frama-3200'),
+ HOWICK_LOGO: _getIcon('howick-logo'),
+ HOWICK_LOGO_DARK: _getIcon('howick-logo-dark'),
+ MAP_MARKER: _getIcon('howick-marker-icon'),
+ MODE_LIGHT: _getIcon('sun'),
+ MODE_DARK: _getIcon('moon'),
+ PARENT: _getIcon('parent'),
+ SETTING: _getIcon('setting'),
+ US: _getCountryIcon('usa')
 }
 
 export const ICON_WEB_NAME = {
-  // @web
-  ALERT_OUTLINE: 'ALERT_OUTLINE',
-  ALERT_OCTAGON: 'ALERT_OCTAGON',
-  BACK: 'BACK',
-  A_BACK: 'A_BACK',
-  BLUR_ON: 'BLUR_ON',
-  CLOSE: 'CLOSE',
-  CHEVRON_RIGHT: 'CHEVRON_RIGHT',
-  CHECK_CIRCLE: 'CHECK_CIRCLE',
-  CHECK_CICLE_OUTLINE: 'CHECK_CICLE_OUTLINE',
-  CONTACT: 'CONTACT',
-  ACTIVE: 'ACTIVE',
-  CONTRAST_BOX: 'CONTRAST_BOX',
-  CONTRAST_ON: 'CONTRAST_ON',
-  CONTRAST_OFF: 'CONTRAST_OFF',
-  CURRENTLY_EMPLOYED: 'CURRENTLY_EMPLOYED',
-  DANGER: 'DANGER',
-  EDIT: 'EDIT',
-  EYE: 'EYE',
-  EYE_OFF: 'EYE_OFF',
-  ERROR_OUTLINE: 'ERROR_OUTLINE',
-  INFO: 'INFO',
-  INACTIVE: 'INACTIVE',
-  INSTALL: 'INSTALL',
-  JIRA: 'JIRA',
-  FULLSCREEN: 'FULLSCREEN',
-  FULLSCREEN_EXIT: 'FULLSCREEN_EXIT',
-  LOGOUT: 'LOGOUT',
-  MAIN_SITE: 'MAIN_SITE',
-  MANUFACTURE: 'MANUFACTURE',
-  MAGNIFY_IN: 'MAGNIFY_IN',
-  MAGNIFY_OUT: 'MAGNIFY_OUT',
-  MODE_LIGHT_A: 'MODE_LIGHT_A',
-  MODE_DARK_A: 'MODE_DARK_A',
-  NOT_EMPLOYED: 'NOT_EMPLOYED',
-  OPEN_IN_NEW: 'OPEN_IN_NEW',
-  QUICK: 'QUICK',
-  PHONE: 'PHONE',
-  READ_MORE: 'READ_MORE',
-  REFRESH: 'REFRESH',
-  SAVE: 'SAVE',
-  SEARCH: 'SEARCH',
-  STATUS: 'STATUS',
-  SETTING: 'SETTING',
-  SHIPPING: 'SHIPPING',
-  SUPPORT: 'SUPPORT',
-  PURCHASED: 'PURCHASED',
-  TICKET: 'TICKET',
-  TRAFFIC_CONE: 'TRAFFIC_CONE',
-  TRANSFER: 'TRANSFER',
-  TIX_STATUS: 'TIX_STATUS',
-  WARNING: 'WARNING'
+ // @web
+ ALERT_OUTLINE: 'ALERT_OUTLINE',
+ ALERT_OCTAGON: 'ALERT_OCTAGON',
+ BACK: 'BACK',
+ A_BACK: 'A_BACK',
+ BLUR_ON: 'BLUR_ON',
+ CLOSE: 'CLOSE',
+ CHEVRON_RIGHT: 'CHEVRON_RIGHT',
+ CHECK_CIRCLE: 'CHECK_CIRCLE',
+ CHECK_CICLE_OUTLINE: 'CHECK_CICLE_OUTLINE',
+ CONTACT: 'CONTACT',
+ ACTIVE: 'ACTIVE',
+ COLUMN: 'COLUMN',
+ CONTRAST_BOX: 'CONTRAST_BOX',
+ CONTRAST_ON: 'CONTRAST_ON',
+ CONTRAST_OFF: 'CONTRAST_OFF',
+ CURRENTLY_EMPLOYED: 'CURRENTLY_EMPLOYED',
+ DANGER: 'DANGER',
+ EDIT: 'EDIT',
+ EYE: 'EYE',
+ EYE_OFF: 'EYE_OFF',
+ ERROR_OUTLINE: 'ERROR_OUTLINE',
+ INFO: 'INFO',
+ INACTIVE: 'INACTIVE',
+ INSTALL: 'INSTALL',
+ JIRA: 'JIRA',
+ FULLSCREEN: 'FULLSCREEN',
+ FULLSCREEN_EXIT: 'FULLSCREEN_EXIT',
+ LOGOUT: 'LOGOUT',
+ MAIN_SITE: 'MAIN_SITE',
+ MANUFACTURE: 'MANUFACTURE',
+ MAGNIFY_IN: 'MAGNIFY_IN',
+ MAGNIFY_OUT: 'MAGNIFY_OUT',
+ MODE_LIGHT_A: 'MODE_LIGHT_A',
+ MODE_DARK_A: 'MODE_DARK_A',
+ NOT_EMPLOYED: 'NOT_EMPLOYED',
+ OPEN_IN_NEW: 'OPEN_IN_NEW',
+ QUICK: 'QUICK',
+ PHONE: 'PHONE',
+ READ_MORE: 'READ_MORE',
+ REFRESH: 'REFRESH',
+ SAVE: 'SAVE',
+ SEARCH: 'SEARCH',
+ STATUS: 'STATUS',
+ SETTING: 'SETTING',
+ SHIPPING: 'SHIPPING',
+ SUPPORT: 'SUPPORT',
+ PURCHASED: 'PURCHASED',
+ TICKET: 'TICKET',
+ TRAFFIC_CONE: 'TRAFFIC_CONE',
+ TRANSFER: 'TRANSFER',
+ TIX_STATUS: 'TIX_STATUS',
+ WARNING: 'WARNING'
 }
 
 export const ICON_LOC_NAME = {
-  // @local
-  BILLING_CONTACT: 'BILLING_CONTACT',
-  DECOILER_1_5T: 'DECOILER_1_5T',
-  DECOILER_3T: 'DECOILER_3T',
-  DECOILER_5T: 'DECOILER_5T',
-  DECOILER_6T: 'DECOILER_6T',
-  DECOILER_DEF: 'DECOILER_DEF',
-  EXPAND: 'EXPAND',
-  FRAMA: 'FRAMA',
-  FRAMA_3200: 'FRAMA_3200',
-  HOWICK_LOGO: 'HOWICK_LOGO',
-  HOWICK_LOGO_DARK: 'HOWICK_LOGO_DARK',
-  MAP_MARKER: 'MAP_MARKER',
-  MODE_LIGHT: 'MODE_LIGHT',
-  MODE_DARK: 'MODE_DARK',
-  US: 'US',
-  PARENT: 'PARENT',
-  SETTING: 'SETTING'
+ // @local
+ BILLING_CONTACT: 'BILLING_CONTACT',
+ DECOILER_1_5T: 'DECOILER_1_5T',
+ DECOILER_3T: 'DECOILER_3T',
+ DECOILER_5T: 'DECOILER_5T',
+ DECOILER_6T: 'DECOILER_6T',
+ DECOILER_DEF: 'DECOILER_DEF',
+ EXPAND: 'EXPAND',
+ FRAMA: 'FRAMA',
+ FRAMA_3200: 'FRAMA_3200',
+ HOWICK_LOGO: 'HOWICK_LOGO',
+ HOWICK_LOGO_DARK: 'HOWICK_LOGO_DARK',
+ MAP_MARKER: 'MAP_MARKER',
+ MODE_LIGHT: 'MODE_LIGHT',
+ MODE_DARK: 'MODE_DARK',
+ US: 'US',
+ PARENT: 'PARENT',
+ SETTING: 'SETTING'
 }
 
 export { ICON_LOC, ICON_WEB }
