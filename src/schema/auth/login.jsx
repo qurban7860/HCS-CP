74240e209611b82e8cbe65325cf65e52566c8560
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-27 19:56:25 +1300

    fix(auth): correct password required message in login schema

--- src/schema/auth/login.jsx
+++ src/schema/auth/login.jsx
@@ -1,12 +1,12 @@
 import * as Yup from 'yup'
 
 export const LoginSchema = Yup.object().shape({
-  email: Yup.string()
-    .transform((value, originalValue) => (originalValue ? originalValue.toLowerCase() : value))
-    .email()
-    .label('Login / Email Address')
-    .trim()
-    .required('Login / Email address is Required!')
-    .max(200),
-  password: Yup.string().label('Password').required('Password is Required!')
+ email: Yup.string()
+  .transform((value, originalValue) => (originalValue ? originalValue.toLowerCase() : value))
+  .email()
+  .label('Login / Email Address')
+  .trim()
+  .required('Login / Email address is Required!')
+  .max(200),
+ password: Yup.string().label('Password').required('Password is required')
 })
