a78b3db9d9c487e43a6f1a8b38fb24621ab84824
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-27 20:08:12 +1300

    feat(dialog): add LegendDialog component and update ConfirmDialog prop types

--- src/component/dialog/confirm-dialog/confirm-dialog.jsx
+++ src/component/dialog/confirm-dialog/confirm-dialog.jsx
@@ -3,33 +3,33 @@ import { useSettingContext } from 'hook'
 import { GStyledTopBorderDivider } from 'theme/style'
 import { Dialog, Button, DialogTitle, DialogActions, DialogContent, Divider } from '@mui/material'
 
-ConfirmDialog.propTypes = {
-  open: PropTypes.bool,
-  title: PropTypes.string,
-  action: PropTypes.node,
-  content: PropTypes.string,
-  onClose: PropTypes.func,
-  SubButton: PropTypes.string
+function ConfirmDialog({ title, content, action, open, onClose, SubButton = 'Cancel', ...other }) {
+ const { themeMode } = useSettingContext()
+ return (
+  <Dialog fullWidth maxWidth='xs' open={open} onClose={onClose} {...other}>
+   <GStyledTopBorderDivider mode={themeMode} />
+   <DialogTitle variant='h4' sx={{ pb: 1, pt: 2 }}>
+    {title}
+   </DialogTitle>
+   {/* <Divider orientation="horizontal" flexItem /> */}
+   {content && <DialogContent sx={{ typography: 'body2', pt: 2 }}> {content} </DialogContent>}
+   <DialogActions>
+    <Button variant='outlined' color='inherit' onClick={onClose}>
+     {SubButton}
+    </Button>
+    {action}
+   </DialogActions>
+  </Dialog>
+ )
 }
 
-function ConfirmDialog({ title, content, action, open, onClose, SubButton = 'Cancel', ...other }) {
-  const { themeMode } = useSettingContext()
-  return (
-    <Dialog fullWidth maxWidth="xs" open={open} onClose={onClose} {...other}>
-      <GStyledTopBorderDivider mode={themeMode} />
-      <DialogTitle variant="h4" sx={{ pb: 1, pt: 2 }}>
-        {title}
-      </DialogTitle>
-      {/* <Divider orientation="horizontal" flexItem /> */}
-      {content && <DialogContent sx={{ typography: 'body2', pt: 2 }}> {content} </DialogContent>}
-      <DialogActions>
-        <Button variant="outlined" color="inherit" onClick={onClose}>
-          {SubButton}
-        </Button>
-        {action}
-      </DialogActions>
-    </Dialog>
-  )
+ConfirmDialog.propTypes = {
+ open: PropTypes.bool,
+ title: PropTypes.string,
+ action: PropTypes.node,
+ content: PropTypes.string,
+ onClose: PropTypes.func,
+ SubButton: PropTypes.string
 }
 
 export default ConfirmDialog
