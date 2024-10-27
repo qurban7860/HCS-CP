d37792c627af7dab64d4adb525c0ae735c74e682
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-27 19:52:57 +1300

    feat(TablePaginationCustom): add TableHeader export and enhance pagination component with new props and structure

--- src/component/table/table-pagination-custom.jsx
+++ src/component/table/table-pagination-custom.jsx
@@ -1,22 +1,26 @@
-import { memo, useState } from 'react'
+import { Fragment, memo, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Icon, ICON_NAME, useSettingContext } from 'hook'
-import { Box, Switch, TablePagination, FormControlLabel, Button, MenuItem, Checkbox, Menu } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
-import { GStyledTablePaginationCustom } from 'theme/style'
 import { t } from 'i18next'
-import { KEY } from 'constant'
+import { Icon, ICON_NAME, useSettingContext } from 'hook'
+import { Box, Switch, Typography, FormControlLabel, Button, MenuItem, Checkbox, Menu } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import { GStyledTablePaginationCustom, GStyledMachineChip } from 'theme/style'
+import { normalizer, charAtText } from 'util'
+import { KEY, TYPOGRAPHY } from 'constant'
 
-TablePaginationCustom.propTypes = {
- sx: PropTypes.object,
- dense: PropTypes.bool,
- onChangeDense: PropTypes.func,
- rowsPerPageOptions: PropTypes.arrayOf(PropTypes.number),
- columnFilterButtonData: PropTypes.array,
- columnButtonClickHandler: PropTypes.func
-}
-
-function TablePaginationCustom({ dense, onChangeDense, rowsPerPageOptions = [10, 20, 50, 100], columnFilterButtonData = [], columnButtonClickHandler = () => {}, sx, ...other }) {
+function TablePaginationCustom({
+ count,
+ data,
+ dense,
+ component,
+ onChangeDense,
+ rowsPerPage,
+ rowsPerPageOptions = [10, 20, 50, 100],
+ columnFilterButtonData = [],
+ columnButtonClickHandler = () => {},
+ sx,
+ ...other
+}) {
  const [anchorEl, setAnchorEl] = useState(null)
  const { themeMode } = useSettingContext()
  const theme = useTheme()
@@ -44,44 +48,70 @@ function TablePaginationCustom({ dense, onChangeDense, rowsPerPageOptions = [10,
     ...sx
    }}>
    {columnFilterButtonData?.length > 0 && (
-    <Box sx={{ flexGrow: 1, pl: 2 }}>
-     <Button
-      startIcon={<Icon icon={ICON_NAME.COLUMN} />}
-      variant='outlined'
-      onClick={e => setAnchorEl(e.currentTarget)}
-      sx={{ color: themeMode === KEY.LIGHT ? theme.palette.grey[900] : theme.palette.grey[0], border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[900] : theme.palette.grey[0]}` }}>
-      {t('column.columns.label')}
-     </Button>
-     <Menu
-      id='long-menu'
-      MenuListProps={{
-       'aria-labelledby': 'long-button'
-      }}
-      anchorEl={anchorEl}
-      open={!!anchorEl}
-      onClose={handleClose}
-      PaperProps={{
-       style: {
-        width: '20ch',
-        maxHeight: 300,
-        overflowY: 'auto'
-       }
-      }}>
-      {columnFilterButtonData?.map(column => (
-       <MenuItem dense sx={{ p: 0 }} key={column.id} onClick={() => handleColumnClick(column)}>
-        <Checkbox checked={column.checked} disabled={column?.alwaysShow} />
-        {column.label}
-       </MenuItem>
-      ))}
-     </Menu>
-    </Box>
+    <Fragment>
+     <Box sx={{ flexGrow: 1, pl: 2 }}>
+      <Button
+       startIcon={<Icon icon={ICON_NAME.COLUMN} />}
+       variant='outlined'
+       onClick={e => setAnchorEl(e.currentTarget)}
+       sx={{
+        mr: 1,
+        color: themeMode === KEY.LIGHT ? theme.palette.grey[800] : theme.palette.grey[0],
+        border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[0]}`
+       }}>
+       {t('column.columns.label')}
+      </Button>
+      <Menu
+       id='long-menu'
+       MenuListProps={{
+        'aria-labelledby': 'long-button'
+       }}
+       anchorEl={anchorEl}
+       open={!!anchorEl}
+       onClose={handleClose}
+       PaperProps={{
+        style: {
+         width: '25ch',
+         maxHeight: 300,
+         overflowY: 'auto'
+        }
+       }}>
+       {columnFilterButtonData?.map(column => (
+        <MenuItem dense sx={{ p: 0 }} key={column.id} onClick={() => handleColumnClick(column)}>
+         <Checkbox checked={column.checked} disabled={column?.alwaysShow} />
+         {normalizer(column.label) === 'machine' ? (
+          <Icon title={'Machine'} icon={ICON_NAME.FRAMA} dimension={20} color={theme.palette.grey[400]} />
+         ) : normalizer(column.label) === 'date' ? (
+          <Icon icon={ICON_NAME.CALENDAR_CLOCK} dimension={20} c color={theme.palette.grey[400]} />
+         ) : (
+          <GStyledMachineChip
+           mode={themeMode}
+           size='small'
+           label={
+            <Typography variant={TYPOGRAPHY.OVERLINE0} p={0}>
+             {charAtText(column.label)}
+            </Typography>
+           }
+           disabled
+          />
+         )}
+         &nbsp;
+         {column.label}
+        </MenuItem>
+       ))}
+      </Menu>
+      {/* <IconTooltip title={'Legend'} icon={ICON_NAME.LEGEND} color={theme.palette.grey[600]} cursor tooltipColor={theme.palette.howick.darkBlue} /> */}
+     </Box>
+    </Fragment>
    )}
    <GStyledTablePaginationCustom
     labelRowsPerPage='Rows:'
-    colSpan={2}
+    data={data}
+    count={count}
     mode={themeMode}
+    rowsPerPage={rowsPerPage}
     rowsPerPageOptions={rowsPerPageOptions}
-    component='div'
+    component={component}
     showLastButton
     showFirstButton
     {...other}
@@ -110,4 +140,18 @@ function TablePaginationCustom({ dense, onChangeDense, rowsPerPageOptions = [10,
   </Box>
  )
 }
+
+TablePaginationCustom.propTypes = {
+ sx: PropTypes.object,
+ data: PropTypes.array,
+ count: PropTypes.number,
+ dense: PropTypes.bool,
+ component: PropTypes.elementType,
+ onChangeDense: PropTypes.func,
+ rowsPerPage: PropTypes.number,
+ rowsPerPageOptions: PropTypes.arrayOf(PropTypes.number),
+ columnFilterButtonData: PropTypes.array,
+ columnButtonClickHandler: PropTypes.func
+}
+
 export default memo(TablePaginationCustom)
