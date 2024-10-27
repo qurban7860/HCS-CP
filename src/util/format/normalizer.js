54669faa54603c7f45defe5557051f8bcccff441
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-27 23:25:53 +1300

    #1468: util(normalizer): add camelCaseNormalizer function and improve existing normalization functions

--- src/util/format/normalizer.js
+++ src/util/format/normalizer.js
@@ -1,7 +1,13 @@
 export function normalizer(str) {
-  return str?.trim()?.toLowerCase()
+ return str?.trim()?.toLowerCase()
 }
 
 export function normalizeLang(str) {
-  return str?.trim()?.replace(/[_ ]/g, '_')?.toLowerCase()
+ return str?.trim()?.replace(/[_ ]/g, '_')?.toLowerCase()
+}
+
+export function camelCaseNormalizer(camelCaseString) {
+ const spaced = camelCaseString.replace(/([a-z])([A-Z])|([A-Z])([A-Z][a-z])/g, '$1$3 $2$4')
+ const capitalized = spaced.charAt(0).toUpperCase() + spaced.slice(1)
+ return capitalized.replace(/\s+([A-Z])(?=[A-Z][a-z]|\d|\W|$)(?<!\s[A-Z])/g, '$1')
 }
