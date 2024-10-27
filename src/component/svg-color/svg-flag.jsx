8300745f934959efa8fa7299adcd4c6cc9b87e39
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-27 19:34:37 +1300

    [eslint-proptypes] - fix

--- src/component/svg-color/svg-flag.jsx
+++ src/component/svg-color/svg-flag.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import countries from 'i18n-iso-countries'
 import enLocale from 'i18n-iso-countries/langs/en.json'
 
@@ -6,29 +7,35 @@ import { GStyledTooltip } from 'theme/style'
 countries.registerLocale(enLocale)
 
 function _getCountryIcon(country) {
-  const countryCode = countries.getAlpha2Code(country, 'en')?.toLocaleLowerCase()
-  return `/asset/svg/flag/${countryCode}.svg`
+ const countryCode = countries.getAlpha2Code(country, 'en')?.toLocaleLowerCase()
+ return `/asset/svg/flag/${countryCode}.svg`
 }
 
 const SvgFlagIcon = ({ country, dimension = 24, color = '#FFF', ...other }) => {
-  return (
-    country !== undefined && (
-      // <Button
-      //   variant="filled"
-      //   sx={{
-      //     cursor: 'default',
-      //     borderColor: color,
-      //     padding: 0.4
-      //   }}
-      //   {...other}>
+ return (
+  country !== undefined && (
+   // <Button
+   //   variant="filled"
+   //   sx={{
+   //     cursor: 'default',
+   //     borderColor: color,
+   //     padding: 0.4
+   //   }}
+   //   {...other}>
 
-      <GStyledTooltip title={country} placement="top" disableFocusListener tooltipcolor={color} color={color}>
-        <img src={_getCountryIcon(country)} alt={country} style={{ borderRadius: 2, height: dimension }} />
-      </GStyledTooltip>
+   <GStyledTooltip title={country} placement='top' disableFocusListener tooltipcolor={color} color={color}>
+    <img src={_getCountryIcon(country)} alt={country} style={{ borderRadius: 2, height: dimension }} />
+   </GStyledTooltip>
 
-      // </Button>
-    )
+   // </Button>
   )
+ )
+}
+
+SvgFlagIcon.propTypes = {
+ country: PropTypes.string,
+ dimension: PropTypes.number,
+ color: PropTypes.string
 }
 
 export default SvgFlagIcon
