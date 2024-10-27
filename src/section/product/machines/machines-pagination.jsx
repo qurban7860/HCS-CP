5036815a00024f48a8871575bc38833a737fc041
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-27 20:03:31 +1300

    refactor(machines): streamline pagination component and enhance prop types and prettierrc propagation

--- src/section/product/machines/machines-pagination.jsx
+++ src/section/product/machines/machines-pagination.jsx
@@ -5,48 +5,40 @@ import { Box, TablePagination } from '@mui/material'
 import { StyledTablePagination } from './style'
 import { LABEL } from 'constant'
 
-const MachineListPagination = ({
-  rowsPerPageOptions = [5, 10, 15, 20, 30, 50],
-  data,
-  page,
-  mode,
-  rowsPerPage,
-  handleChangePage,
-  handleChangeRowsPerPage,
-  ...other
-}) => {
-  return (
-    <Box
-      sx={{
-        position: 'relative'
-      }}>
-      <StyledTablePagination
-        count={data?.length ?? 0}
-        component={m.div}
-        colSpan={2}
-        data={data}
-        mode={mode}
-        page={page}
-        labelRowsPerPage={LABEL.ROWS}
-        rowsPerPageOptions={rowsPerPageOptions}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-        rowsPerPage={rowsPerPage}
-        showLastButton
-        showFirstButton
-        {...other}
-      />
-    </Box>
-  )
+const MachineListPagination = ({ rowsPerPageOptions = [5, 10, 15, 20, 30, 50], data, page, mode, rowsPerPage, handleChangePage, handleChangeRowsPerPage, ...other }) => {
+ return (
+  <Box
+   sx={{
+    position: 'relative'
+   }}>
+   <StyledTablePagination
+    count={data?.length ?? 0}
+    component={m.div}
+    colSpan={2}
+    data={data}
+    mode={mode}
+    page={page}
+    labelRowsPerPage={LABEL.ROWS}
+    rowsPerPageOptions={rowsPerPageOptions}
+    onPageChange={handleChangePage}
+    onRowsPerPageChange={handleChangeRowsPerPage}
+    rowsPerPage={rowsPerPage}
+    showLastButton
+    showFirstButton
+    {...other}
+   />
+  </Box>
+ )
 }
 
 MachineListPagination.propTypes = {
-  data: PropTypes.array,
-  mode: PropTypes.string,
-  page: PropTypes.number,
-  rowsPerPage: PropTypes.number,
-  handleChangePage: PropTypes.func,
-  handleChangeRowsPerPage: PropTypes.func
+ data: PropTypes.array,
+ mode: PropTypes.string,
+ page: PropTypes.number,
+ rowsPerPage: PropTypes.number,
+ rowsPerPageOptions: PropTypes.array,
+ handleChangePage: PropTypes.func,
+ handleChangeRowsPerPage: PropTypes.func
 }
 
 export default memo(MachineListPagination)
--- src/section/product/machines/machines-section.jsx
+++ src/section/product/machines/machines-section.jsx
@@ -1,18 +1,11 @@
 import { useEffect, useState, memo, useLayoutEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
 import axios from 'axios'
+import _ from 'lodash'
 import { useSelector, dispatch } from 'store'
 import { useAuthContext } from 'auth'
 import { useTable, useFilter, getComparator, useSettingContext } from 'hook'
-import {
-  getMachines,
-  getSecurityUser,
-  setMachineFilterBy,
-  ChangeMachinePage,
-  ChangeMachineRowsPerPage,
-  resetMachines,
-  resetSecurityUser
-} from 'store/slice'
-import _ from 'lodash'
+import { getMachines, getSecurityUser, setMachineFilterBy, ChangeMachinePage, ChangeMachineRowsPerPage, resetMachines, resetSecurityUser } from 'store/slice'
 import { Table, Grid } from '@mui/material'
 import { GStyledTableHeaderBox } from 'theme/style'
 import { TableNoData, MotionLazyContainer, SkeletonTable, SearchBox, TableTitleBox } from 'component'
@@ -22,115 +15,107 @@ import { KEY, TITLE, FLEX, FLEX_DIR } from 'constant'
 import { StyledScrollTableContainer } from './style'
 
 const MachineListSection = ({ isArchived }) => {
-  const [tableData, setTableData] = useState([])
-  const { machines, initial, isLoading, machinePage, machineRowsPerPage } = useSelector((state) => state.machine)
-  const { userId } = useAuthContext()
-  const { securityUser } = useSelector((state) => state.user)
+ const [tableData, setTableData] = useState([])
+ const { machines, initial, isLoading, machinePage, machineRowsPerPage } = useSelector(state => state.machine)
+ const { userId } = useAuthContext()
+ const { securityUser } = useSelector(state => state.user)
 
-  const { themeMode } = useSettingContext()
-  const denseHeight = TABLE.DENSE_HEIGHT
+ const { themeMode } = useSettingContext()
+ const denseHeight = TABLE.DENSE_HEIGHT
 
-  const axiosToken = () => axios.CancelToken.source()
-  const cancelTokenSource = axiosToken()
+ const axiosToken = () => axios.CancelToken.source()
+ const cancelTokenSource = axiosToken()
 
-  const {
-    order,
-    orderBy,
-    setPage: setTablePage
-  } = useTable({
-    defaultOrderBy: KEY.CREATED_AT,
-    defaultOrder: KEY.DESC
-  })
+ const {
+  order,
+  orderBy,
+  setPage: setTablePage
+ } = useTable({
+  defaultOrderBy: KEY.CREATED_AT,
+  defaultOrder: KEY.DESC
+ })
 
-  useLayoutEffect(() => {
-    dispatch(resetMachines())
+ useLayoutEffect(() => {
+  dispatch(resetMachines())
 
-    if (userId !== securityUser?._id) {
-      dispatch(resetSecurityUser())
-    }
-  }, [userId])
-
-  useEffect(() => {
-    if (userId !== securityUser?._id) {
-      dispatch(getSecurityUser(userId))
-    }
-  }, [userId])
-
-  useEffect(() => {
-    const debouncedDispatch = _.debounce(() => {
-      dispatch(getMachines(null, null, isArchived, cancelTokenSource))
-    }, 300)
-
-    debouncedDispatch()
-
-    return () => debouncedDispatch.cancel()
-  }, [dispatch, machinePage, machineRowsPerPage, isArchived])
-
-  useEffect(() => {
-    if (initial) {
-      setTableData(machines || [])
-    }
-  }, [machines, initial])
-
-  const { filterName, handleFilterName, filteredData } = useFilter(
-    getComparator(order, orderBy),
-    tableData,
-    initial,
-    ChangeMachinePage,
-    setMachineFilterBy
-  )
-
-  const handleChangePage = (event, newPage) => {
-    if (newPage < Math.ceil(filteredData.length / machineRowsPerPage)) {
-      dispatch(ChangeMachinePage(newPage))
-    }
+  if (userId !== securityUser?._id) {
+   dispatch(resetSecurityUser())
   }
+ }, [userId])
 
-  const handleChangeRowsPerPage = (event) => {
-    dispatch(ChangeMachinePage(0))
-    dispatch(ChangeMachineRowsPerPage(parseInt(event.target.value, 10)))
+ useEffect(() => {
+  if (userId !== securityUser?._id) {
+   dispatch(getSecurityUser(userId))
   }
+ }, [userId])
 
-  const isNotFound = !isLoading && !filteredData.length
+ useEffect(() => {
+  const debouncedDispatch = _.debounce(() => {
+   dispatch(getMachines(null, null, isArchived, cancelTokenSource))
+  }, 300)
 
-  return (
-    <MotionLazyContainer display={FLEX.FLEX}>
-      <TableTitleBox title={TITLE.MACHINE_LIST} user={securityUser} />
-      <SearchBox term={filterName} mode={themeMode} handleSearch={handleFilterName} />
-      <Grid container flexDirection={FLEX_DIR.ROW} {...MARGIN.PAGE_PROP}>
-        <Grid item lg={12}>
-          <Grid container mb={2}>
-            <Grid item lg={12} sm={12} mb={2} bgcolor="background.paper">
-              <GStyledTableHeaderBox bgcolor={themeMode === KEY.LIGHT ? 'success.main' : 'grey.800'} flex={1} px={2} pt={2} />
-              <MachineListPagination
-                mode={themeMode}
-                data={filteredData}
-                page={machinePage}
-                rowsPerPage={machineRowsPerPage}
-                handleChangePage={handleChangePage}
-                handleChangeRowsPerPage={handleChangeRowsPerPage}
-              />
-              <StyledScrollTableContainer>
-                <Table>
-                  <MachineHeader mode={themeMode} />
-                  {(isLoading ? [...Array(machineRowsPerPage)] : filteredData)
-                    .slice(machinePage * machineRowsPerPage, machinePage * machineRowsPerPage + machineRowsPerPage)
-                    .map((row, index) =>
-                      row ? (
-                        <MachineTable key={row._id} machine={row} mode={themeMode} index={index} />
-                      ) : (
-                        !isNotFound && <SkeletonTable key={index} sx={{ height: denseHeight }} />
-                      )
-                    )}
-                  <TableNoData isNotFound={isNotFound} />
-                </Table>
-              </StyledScrollTableContainer>
-            </Grid>
-          </Grid>
-        </Grid>
+  debouncedDispatch()
+
+  return () => debouncedDispatch.cancel()
+ }, [dispatch, machinePage, machineRowsPerPage, isArchived])
+
+ useEffect(() => {
+  if (initial) {
+   setTableData(machines || [])
+  }
+ }, [machines, initial])
+
+ const { filterName, handleFilterName, filteredData } = useFilter(getComparator(order, orderBy), tableData, initial, ChangeMachinePage, setMachineFilterBy)
+
+ const handleChangePage = (event, newPage) => {
+  if (newPage < Math.ceil(filteredData.length / machineRowsPerPage)) {
+   dispatch(ChangeMachinePage(newPage))
+  }
+ }
+
+ const handleChangeRowsPerPage = event => {
+  dispatch(ChangeMachinePage(0))
+  dispatch(ChangeMachineRowsPerPage(parseInt(event.target.value, 10)))
+ }
+
+ const isNotFound = !isLoading && !filteredData.length
+
+ return (
+  <MotionLazyContainer display={FLEX.FLEX}>
+   <TableTitleBox title={TITLE.MACHINE_LIST} user={securityUser} />
+   <SearchBox term={filterName} mode={themeMode} handleSearch={handleFilterName} />
+   <Grid container flexDirection={FLEX_DIR.ROW} {...MARGIN.PAGE_PROP}>
+    <Grid item lg={12}>
+     <Grid container mb={2}>
+      <Grid item lg={12} sm={12} mb={2} bgcolor='background.paper'>
+       <GStyledTableHeaderBox bgcolor={themeMode === KEY.LIGHT ? 'success.main' : 'grey.800'} flex={1} px={2} pt={2} />
+       <MachineListPagination
+        mode={themeMode}
+        data={filteredData}
+        page={machinePage}
+        rowsPerPage={machineRowsPerPage}
+        handleChangePage={handleChangePage}
+        handleChangeRowsPerPage={handleChangeRowsPerPage}
+       />
+       <StyledScrollTableContainer>
+        <Table>
+         <MachineHeader mode={themeMode} />
+         {(isLoading ? [...Array(machineRowsPerPage)] : filteredData)
+          .slice(machinePage * machineRowsPerPage, machinePage * machineRowsPerPage + machineRowsPerPage)
+          .map((row, index) => (row ? <MachineTable key={row._id} machine={row} mode={themeMode} index={index} /> : !isNotFound && <SkeletonTable key={index} sx={{ height: denseHeight }} />))}
+         <TableNoData isNotFound={isNotFound} />
+        </Table>
+       </StyledScrollTableContainer>
       </Grid>
-    </MotionLazyContainer>
-  )
+     </Grid>
+    </Grid>
+   </Grid>
+  </MotionLazyContainer>
+ )
+}
+
+MachineListSection.propTypes = {
+ isArchived: PropTypes.bool
 }
 
 export default memo(MachineListSection)
