253e679e7ee20555a1fc087c38f43edfa72c69b3
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-29 09:23:59 +1300

    #1468: feat(machine-logs): add MachineLogsTable component for enhanced log display

--- src/section/product/machine-logs/machine-logs-tab.jsx
+++ src/section/product/machine-logs/machine-logs-tab.jsx
@@ -1,26 +1,54 @@
-import { Fragment, useEffect, useReducer, useState } from 'react'
+import { Fragment, useEffect, useReducer, useState, useCallback, useLayoutEffect } from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 import { useSelector } from 'react-redux'
 import { dispatch } from 'store'
+import { useSearchParams } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { useAuthContext } from 'auth'
 import { useTable, getComparator, useSettingContext } from 'hook'
-import { ChangeLogPage, ChangeLogRowsPerPage, getLogs } from 'store/slice'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { getCustomers, getSecurityUser, getLogs, ChangeLogPage, ChangeLogRowsPerPage, resetLogs, resetSecurityUser } from 'store/slice'
+import { addLogSchema } from 'schema'
 import { Grid, Table } from '@mui/material'
-import { TableNoData, SkeletonTable, LogDetailsDialog } from 'component'
-import { StyledScrollTableContainer, LogsHeader, LogsTable, LogsPagination, tableColumnsReducer } from 'section/log/logs'
+import { MotionLazyContainer, TableTitleBox, TableNoData, SkeletonTable, LogDetailsDialog } from 'component'
+import FormProvider from 'component/hook-form'
+import { LogsTableController, useLogDefaultValues, LogsTable, LogsPagination, tableColumnsReducer } from 'section/log/logs'
+import { MachineLogsTable } from 'section/product'
 import { GStyledTableHeaderBox } from 'theme/style'
 import { MARGIN } from 'config'
 import { getLogTypeConfigForGenerationAndType, logGraphTypes } from 'config/log-types'
-import { FLEX_DIR, KEY } from 'constant'
+import { FLEX, FLEX_DIR, KEY } from 'constant'
 import { applySort, fDateTime } from 'util'
 
-const MachineLogsTab = ({ logType, isLogsPage, payload }) => {
+const MachineLogsTab = () => {
+ const [selectedSearchFilter, setSelectedSearchFilter] = useState('')
  const [openLogDetailsDialog, setOpenLogDetailsDialog] = useState(false)
  const [selectedLog, setSelectedLog] = useState(null)
  const [logComponentTitle, setLogComponentTitle] = useState({ date: '', componentLabel: '' })
  const [tableData, setTableData] = useState([])
  const { logs, logPage, logsTotalCount, isLoading, logRowsPerPage } = useSelector(state => state.log)
+ const { securityUser } = useSelector(state => state.user)
+ const { customers } = useSelector(state => state.customer)
+ const { customerMachines } = useSelector(state => state.machine)
  const [tableColumns, dispatchTableColumns] = useReducer(tableColumnsReducer, getLogTypeConfigForGenerationAndType(5, 'ERP').tableColumns)
+
+ const { userId, user } = useAuthContext()
  const { themeMode } = useSettingContext()
+ const [searchParams] = useSearchParams()
+
+ const axiosToken = () => axios.CancelToken.source()
+ const cancelTokenSource = axiosToken()
+ const isGraphPage = () => searchParams.get('type') === 'erpGraph'
+
+ const defaultValues = useLogDefaultValues(customers[0], customerMachines[0])
+ const methods = useForm({
+  resolver: yupResolver(addLogSchema),
+  defaultValues
+ })
+
+ const { watch, setValue, handleSubmit, trigger } = methods
+ const { customer, machine, dateFrom, dateTo, logType, filteredSearchKey, logPeriod, logGraphType } = watch()
 
  useEffect(() => {
   dispatch(
@@ -36,16 +64,16 @@ const MachineLogsTab = ({ logType, isLogsPage, payload }) => {
   setTableData(logs?.data || [])
  }, [logs])
 
- useEffect(() => {
-  const newColumns = logType?.tableColumns
-  if (newColumns) {
-   dispatchTableColumns({
-    type: 'handleLogTypeChange',
-    newColumns,
-    allMachineLogsPage: isLogsPage
-   })
-  }
- }, [logType])
+ //  useEffect(() => {
+ //   const newColumns = logType?.tableColumns
+ //   if (newColumns) {
+ //    dispatchTableColumns({
+ //     type: 'handleLogTypeChange',
+ //     newColumns,
+ //     allMachineLogsPage: isLogsPage
+ //    })
+ //   }
+ //  }, [logType])
 
  const { order, orderBy, selected, onSort } = useTable({
   defaultOrderBy: KEY.CREATED_AT,
@@ -93,66 +121,90 @@ const MachineLogsTab = ({ logType, isLogsPage, payload }) => {
   )
  }
 
- const isNotFound = !isLoading && !dataFiltered.length
+ const onGetLogs = data => {
+  const customerId = customer._id
+  const machineId = machine?._id || undefined
+  dispatch(ChangeLogPage(0))
+  dispatch(
+   getLogs({
+    customerId,
+    machineId,
+    page: 0,
+    pageSize: logRowsPerPage,
+    fromDate: dateFrom,
+    toDate: dateTo,
+    isArchived: false,
+    isMachineArchived: machine?.isArchived,
+    selectedLogType: logType.type,
+    searchKey: filteredSearchKey,
+    searchColumn: selectedSearchFilter
+   })
+  )
+ }
+
+ const handleCustomerChange = useCallback(
+  newCustomer => {
+   setValue('customer', newCustomer)
+   setValue('machine', null)
+   trigger(['customer', 'machine'])
+   dispatch(resetLogs())
+  },
+  [dispatch, setValue, trigger]
+ )
+
+ const handleMachineChange = useCallback(
+  newMachine => {
+   setValue('machine', newMachine)
+   trigger('machine')
+   dispatch(resetLogs())
+  },
+  [dispatch, setValue, trigger]
+ )
+
+ const handleLogTypeChange = useCallback(
+  newLogType => {
+   setValue('logType', newLogType)
+   trigger('logType')
+  },
+  [setValue, trigger]
+ )
+
+ const payload = {
+  customerId: machine?.customer?._id,
+  machineId: machine?._id || undefined,
+  page: logPage,
+  pageSize: logRowsPerPage,
+  fromDate: dateFrom,
+  toDate: dateTo,
+  isArchived: false,
+  isMachineArchived: false,
+  selectedLogType: logType?.type,
+  searchKey: filteredSearchKey,
+  searchColumn: selectedSearchFilter
+ }
 
  return (
   <Fragment>
-   <Grid container flexDirection={FLEX_DIR.ROW} {...MARGIN.PAGE_PROP}>
-    <Grid item lg={12}>
-     <Grid container mb={2}>
-      <Grid item lg={12} sm={12} mb={2} bgcolor='background.paper'>
-       <GStyledTableHeaderBox bgcolor={themeMode === KEY.LIGHT ? 'success.main' : 'grey.800'} flex={1} px={2} pt={2} />
-       <LogsPagination
-        count={logsTotalCount || 0}
-        data={logsTotalCount}
-        page={logPage}
-        rowsPerPage={logRowsPerPage}
-        handleChangePage={handleChangePage}
-        handleChangeRowsPerPage={handleChangeRowsPerPage}
-        columnFilterButtonData={tableColumns}
-        handleColumnButtonClick={handleColumnButtonClick}
+   <MotionLazyContainer display={FLEX.FLEX}>
+    <FormProvider methods={methods} onSubmit={handleSubmit(onGetLogs)}>
+     <Grid container spacing={2} mt={3}>
+      <Grid item xs={12} sm={12}>
+       <LogsTableController
+        customers={customers}
+        handleCustomerChange={handleCustomerChange}
+        customerMachines={customerMachines}
+        handleMachineChange={handleMachineChange}
+        handleLogTypeChange={handleLogTypeChange}
+        isGraphPage={isGraphPage}
+        methods={methods}
+        onGetLogs={onGetLogs}
        />
-       <StyledScrollTableContainer>
-        <Table>
-         <LogsHeader columns={tableColumns} dataFiltered={dataFiltered} orderBy={orderBy} order={order} onSort={onSort} />
-         {(isLoading ? [...Array(logRowsPerPage)] : dataFiltered).map((row, index) =>
-          row ? (
-           <LogsTable
-            key={row._id}
-            row={row}
-            columns={tableColumns}
-            onViewRow={() => handleViewRow(row._id, row)}
-            mode={themeMode}
-            index={index}
-            dataFiltered={dataFiltered}
-            onSort={onSort}
-            order={order}
-            orderBy={orderBy}
-            selected={selected.includes(row._id)}
-           />
-          ) : (
-           !isNotFound && <SkeletonTable key={index} sx={{ height: 60 }} />
-          )
-         )}
-         <TableNoData isNotFound={isNotFound} />
-        </Table>
-       </StyledScrollTableContainer>
       </Grid>
      </Grid>
-    </Grid>
-   </Grid>
-   {openLogDetailsDialog && (
-    <LogDetailsDialog
-     open={openLogDetailsDialog}
-     componentTitle={logComponentTitle}
-     logDetails={selectedLog}
-     isLogsPage={isLogsPage}
-     openLogDetailsDialog={openLogDetailsDialog}
-     setOpenLogDetailsDialog={setOpenLogDetailsDialog}
-     logType={logType?.type}
-     refreshLogsList={refreshLogsList}
-    />
-   )}
+    </FormProvider>
+
+    <MachineLogsTable isLogsPage={false} logType={logType} payload={payload} />
+   </MotionLazyContainer>
   </Fragment>
  )
 }
