7d4a96853832bbabd6d751bac9473a72960f6f15
ballyalley-o <bally.lomibao@howickltd.com>
2024-10-30 08:17:55 +1300

    feat(router): add new route for graph page in the machine

--- src/route/index.jsx
+++ src/route/index.jsx
@@ -155,6 +155,10 @@ export default function Router() {
         {
          element: <MachinesLogsPage />,
          index: true
+        },
+        {
+         path: 'graph'
+         // element:<MachinesLogsPage/>
         }
        ]
       },
