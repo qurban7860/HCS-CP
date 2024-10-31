222bab0c37f3077a609f56890e1df7718e8cea38
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-01 01:02:19 +1300

    feat(chart): export LogLineChart and enhance log exports

--- src/component/chart/index.js
+++ src/component/chart/index.js
@@ -2,4 +2,6 @@ import Chart from 'react-apexcharts'
 export default Chart
 export { default as ChartStyleOverlay } from './chart-style-overlay'
 export { default as useChart } from './use-chart'
+
 export * from './log/log-stacked-chart'
+export * from './log/log-line-chart'
