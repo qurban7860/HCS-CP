e94067df3989ce448957980b6664ce2b565e6322
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-12 07:59:02 +1300

    #1499: refactor(welcome): improve code formatting and readability in Welcome component

--- src/component/widget/welcome.jsx
+++ src/component/widget/welcome.jsx
@@ -7,41 +7,36 @@ import { KEY, TYPOGRAPHY } from 'constant'
 import { ASSET } from 'config'
 
 Welcome.propTypes = {
-  img: PropTypes.node,
-  action: PropTypes.node,
-  title: PropTypes.string,
-  description: PropTypes.string
+ img: PropTypes.node,
+ action: PropTypes.node,
+ title: PropTypes.string,
+ description: PropTypes.string
 }
 
 function Welcome({ title, description, action, img, ...other }) {
-  const { themeMode } = useSettingContext()
-  const theme = useTheme()
-  return (
-    <GStyledWelcomeContainerDiv {...other}>
-      <Stack
-        flexGrow={1}
-        sx={{
-          textAlign: { xs: KEY.CENTER, md: KEY.LEFT },
-          mb: { xs: 5, md: 10 },
-          mt: { xs: 0, md: 5 }
-        }}>
-        {/* keep for branding  */}
-        <GStyledSpanBox gap={2} my={2}>
-          <img
-            alt="logo"
-            src={themeMode === KEY.DARK ? ASSET.HOWICK_PORTAL_DARK : ASSET.HOWICK_PORTAL}
-            width={900}
-            style={{ pointerEvents: KEY.NONE }}
-          />
-        </GStyledSpanBox>
-        <GStyledWelcomeDescription variant={TYPOGRAPHY.SUBTITLE0} themeMode={themeMode}>
-          {description}
-        </GStyledWelcomeDescription>
-        {action && action}
-      </Stack>
-      {img && img}
-    </GStyledWelcomeContainerDiv>
-  )
+ const { themeMode } = useSettingContext()
+ const theme = useTheme()
+ return (
+  <GStyledWelcomeContainerDiv {...other}>
+   <Stack
+    flexGrow={1}
+    sx={{
+     textAlign: { xs: KEY.CENTER, md: KEY.LEFT },
+     mb: { xs: 5, md: 10 },
+     mt: { xs: 0, md: 5 }
+    }}>
+    {/* keep for branding  */}
+    <GStyledSpanBox gap={2} my={2}>
+     <img alt='logo' src={themeMode === KEY.DARK ? ASSET.HOWICK_PORTAL_DARK : ASSET.HOWICK_PORTAL} width={900} style={{ pointerEvents: KEY.NONE }} />
+    </GStyledSpanBox>
+    <GStyledWelcomeDescription variant={TYPOGRAPHY.SUBTITLE0} themeMode={themeMode}>
+     {description}
+    </GStyledWelcomeDescription>
+    {action && action}
+   </Stack>
+   {img && img}
+  </GStyledWelcomeContainerDiv>
+ )
 }
 
 export default Welcome
