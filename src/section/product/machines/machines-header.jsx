37fad29022ea93231c7a329ffd71e4d057037cab
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-12 09:02:23 +1300

    #1499 : refactor(machines): improve styling consistency and enhance prop type validation

--- src/section/product/machines/machines-header.jsx
+++ src/section/product/machines/machines-header.jsx
@@ -1,19 +1,24 @@
+import PropTypes from 'prop-types'
 import { TableHead, TableRow } from '@mui/material'
 import { StyledHeaderTableCell } from './style'
 import { HEADER } from './constant'
 
 const MachineHeader = ({ mode }) => {
-  return (
-    <TableHead>
-      <TableRow>
-        {HEADER.map((item) => (
-          <StyledHeaderTableCell key={item.key} item={item} mode={mode}>
-            {item.label}
-          </StyledHeaderTableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  )
+ return (
+  <TableHead>
+   <TableRow>
+    {HEADER.map(item => (
+     <StyledHeaderTableCell key={item.key} item={item} mode={mode}>
+      {item.label}
+     </StyledHeaderTableCell>
+    ))}
+   </TableRow>
+  </TableHead>
+ )
+}
+
+MachineHeader.propTypes = {
+ mode: PropTypes.string
 }
 
 export default MachineHeader
