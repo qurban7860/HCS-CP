10babf871d7433bcabd24482dcd7354a4a4ce66c
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-12 16:40:50 +1300

    #1512: feat(regex): add PASSWORD regex for enhanced validation

--- src/constant/regex.js
+++ src/constant/regex.js
@@ -1,7 +1,8 @@
 const REGEX = {
  EMAIL: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
  ERROR_CODE: /^[4][0-9][0-9]$/,
- SERIAL_NO: /^\d{5}$/
+ SERIAL_NO: /^\d{5}$/,
+ PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/
 }
 
 export default REGEX
