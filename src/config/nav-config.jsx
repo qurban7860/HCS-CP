6a998c97ab40cef5fdcdb0c958703e36e6ba202d
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-13 14:44:10 +1300

    #1499 #1495:refactor(nav-config): rename file to .jsx and update navigation items with icons and captions

--- src/config/nav-config.jsx
+++ src/config/nav-config.jsx
@@ -1,17 +1,16 @@
 import { useState, useEffect } from 'react'
-import { useLocale } from 'locale'
+import { ICON_NAME } from 'hook'
 import { PATH_DASHBOARD, PATH_CUSTOMER, PATH_MACHINE, PATH_SUPPORT, PATH_LOGS } from 'route/path'
 
 function NavConfiguration() {
- const { t } = useLocale()
+ const customerId = localStorage.getItem('customer')
  const [navConfig, setNavConfig] = useState([
   {
    subheader: 'general',
    items: [
-    { title: 'home.label', path: PATH_DASHBOARD.general.app },
+    { title: '|', path: PATH_DASHBOARD.general.app, icon: ICON_NAME.DASHBOARD, caption: 'dashboard.label' },
+    { title: 'home.label', path: PATH_CUSTOMER.customers.view(customerId) },
     { title: 'machine.label', path: PATH_MACHINE.machines.list },
-    // { title: 'organization.label', path: PATH_CUSTOMER.customers.list },
-    // { title: 'document.label', path: '/documents', children: null, disabled: true },
     { title: 'log.label', path: PATH_LOGS.machines.list },
     { title: 'support.label', path: PATH_SUPPORT.tickets.list }
    ]
