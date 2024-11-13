e43fb1da0658a8b63ce95d2c13f73534c479b68e
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-13 16:37:04 +1300

    #1520 [initialized] feat(auth): implement new password section with layout and form components

---
+++ src/section/auth/new-password/index.js
@@ -0,0 +1,3 @@
+export { default as ResetPasswordLayout } from './new-password-layout'
+export { default as ResetPasswordForm } from './new-password-form'
+export { default as ResetPassword } from './new-password'
