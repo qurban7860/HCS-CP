802fa511222a5b7f59772aa022a818a539cc371b
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-15 11:51:17 +1300

    [eslint-resolve] refactor(default-values): rename function to useSiteDefaultValues for clarity

--- src/section/crm/site/default-values/default-values.jsx
+++ src/section/crm/site/default-values/default-values.jsx
@@ -7,47 +7,43 @@ import { fDate, parseAddress } from 'util'
  * @param {Object} customer - The customer object.
  * @returns {Object} - The default values for the site object.
  */
-export default function siteDefaultValues(site, customer) {
-  return useMemo(() => {
-    return {
-      id: site?._id || '',
-      name: site?.name || '',
-      customer: customer || null,
-      customerName: site?.customer?.name || '',
-      customerId: site?.customer?._id || '',
-      customerMainSiteId: customer?.mainSite?._id || '',
-      customerCountry: customer?.mainSite?.address?.country || '',
-      address: parseAddress(site?.address) || '',
-      street: site?.address?.street || '',
-      suburb: site?.address?.suburb || '',
-      postCode: site?.address?.postCode || '',
-      region: site?.address?.region || '',
-      city: site?.address?.city || '',
-      state: site?.address?.state || '',
-      country: site?.address?.country || '',
-      phone: site?.phoneNumbers || '',
-      website: site?.website || '',
-      primaryTechnicalContact: site?.primaryTechnicalContact || '',
-      primaryTechnicalContactId: site?.primaryTechnicalContact?._id || '',
-      primaryTechnicalContactFullName: site?.primaryTechnicalContact
-        ? `${site.primaryTechnicalContact.firstName} ${site.primaryTechnicalContact.lastName}`
-        : '',
-      primaryBillingContact: site?.primaryBillingContact || '',
-      primaryBillingContactId: site?.primaryBillingContact?._id || '',
-      primaryBillingContactFullName: site?.primaryBillingContact
-        ? `${site.primaryBillingContact.firstName} ${site.primaryBillingContact.lastName}`
-        : '',
-      lat: site?.lat || '',
-      long: site?.long || '',
-      isActive: site?.isActive || false,
-      isArchived: site?.isArchived || false,
-      contactTypes: site?.contactTypes,
-      createdAt: fDate(site?.createdAt) || '',
-      updatedAt: fDate(site?.updatedAt) || '',
-      createdBy: site?.createdBy?.name || '',
-      updatedBy: site?.updatedBy?.name || '',
-      createdIP: site?.createdIP || '',
-      updatedIP: site?.updatedIP || ''
-    }
-  }, [site])
+export default function useSiteDefaultValues(site, customer) {
+ return useMemo(() => {
+  return {
+   id: site?._id || '',
+   name: site?.name || '',
+   customer: customer || null,
+   customerName: site?.customer?.name || '',
+   customerId: site?.customer?._id || '',
+   customerMainSiteId: customer?.mainSite?._id || '',
+   customerCountry: customer?.mainSite?.address?.country || '',
+   address: parseAddress(site?.address) || '',
+   street: site?.address?.street || '',
+   suburb: site?.address?.suburb || '',
+   postCode: site?.address?.postCode || '',
+   region: site?.address?.region || '',
+   city: site?.address?.city || '',
+   state: site?.address?.state || '',
+   country: site?.address?.country || '',
+   phone: site?.phoneNumbers || '',
+   website: site?.website || '',
+   primaryTechnicalContact: site?.primaryTechnicalContact || '',
+   primaryTechnicalContactId: site?.primaryTechnicalContact?._id || '',
+   primaryTechnicalContactFullName: site?.primaryTechnicalContact ? `${site.primaryTechnicalContact.firstName} ${site.primaryTechnicalContact.lastName}` : '',
+   primaryBillingContact: site?.primaryBillingContact || '',
+   primaryBillingContactId: site?.primaryBillingContact?._id || '',
+   primaryBillingContactFullName: site?.primaryBillingContact ? `${site.primaryBillingContact.firstName} ${site.primaryBillingContact.lastName}` : '',
+   lat: site?.lat || '',
+   long: site?.long || '',
+   isActive: site?.isActive || false,
+   isArchived: site?.isArchived || false,
+   contactTypes: site?.contactTypes,
+   createdAt: fDate(site?.createdAt) || '',
+   updatedAt: fDate(site?.updatedAt) || '',
+   createdBy: site?.createdBy?.name || '',
+   updatedBy: site?.updatedBy?.name || '',
+   createdIP: site?.createdIP || '',
+   updatedIP: site?.updatedIP || ''
+  }
+ }, [site])
 }
