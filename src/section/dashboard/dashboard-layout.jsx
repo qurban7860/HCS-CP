be9157eb0d0d36c97a4b2898e93288d68911c89c
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-15 14:11:53 +1300

    [prettierrc] refactor(dashboard): improve code formatting and readability in DashboardLayout component

--- src/section/dashboard/dashboard-layout.jsx
+++ src/section/dashboard/dashboard-layout.jsx
@@ -9,46 +9,45 @@ import Header, { Navbar } from './header'
 import { SZ } from 'constant'
 
 function DashboardLayout() {
-  const { themeMode } = useSettingContext()
-  const theme = useTheme()
-  // KEEP: for future use
-  // const isDesktop = useResponsive('up', 'lg')
-  const { scrollYProgress } = useScroll()
+ const { themeMode } = useSettingContext()
+ const theme = useTheme()
+ // const isDesktop = useResponsive('up', 'lg')
+ const { scrollYProgress } = useScroll()
 
-  // [HouseKeeping] Progress Bar
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001
-  })
-  const progress = (
-    <m.div
-      style={{
-        top: 0,
-        left: 0,
-        right: 0,
-        height: 3,
-        zIndex: 1999,
-        position: 'fixed',
-        transformOrigin: '0%',
-        backgroundColor: theme.palette.primary.main,
-        scaleX
-      }}
-    />
-  )
+ // [HouseKeeping] Progress Bar
+ const scaleX = useSpring(scrollYProgress, {
+  stiffness: 100,
+  damping: 30,
+  restDelta: 0.001
+ })
+ const progress = (
+  <m.div
+   style={{
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 3,
+    zIndex: 1999,
+    position: 'fixed',
+    transformOrigin: '0%',
+    backgroundColor: theme.palette.primary.main,
+    scaleX
+   }}
+  />
+ )
 
-  return (
-    <Fragment>
-      {progress}
-      <Header />
-      <GStyledContainer maxWidth={SZ.XL} themeMode={themeMode}>
-        <Navbar />
-        <Main>
-          <Outlet />
-        </Main>
-      </GStyledContainer>
-    </Fragment>
-  )
+ return (
+  <Fragment>
+   {progress}
+   <Header />
+   <GStyledContainer maxWidth={SZ.XL} themeMode={themeMode}>
+    <Navbar />
+    <Main>
+     <Outlet />
+    </Main>
+   </GStyledContainer>
+  </Fragment>
+ )
 }
 
 export default DashboardLayout
