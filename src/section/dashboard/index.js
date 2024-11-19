41de13f74f1ffb7d797016a32f8f792e1c708ff0
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-19 16:25:24 +1300

    [exports]: export ProductionRateGraphWidget for improved dashboard functionality

--- src/section/dashboard/index.js
+++ src/section/dashboard/index.js
@@ -1,2 +1,3 @@
 export { default } from './dashboard-layout'
 export { default as ProductionTotalGraphWidget } from './production-total-graph-widget/production-total-graph-widget'
+export { default as ProductionRateGraphWidget } from './production-rate-graph-widget/production-rate-graph-widget'
