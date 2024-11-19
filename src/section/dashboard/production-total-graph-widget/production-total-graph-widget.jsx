59cd4df7fe0b3b6d7621a124b1c7ea8468e3fdf4
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-19 14:02:57 +1300

    #1535 #1536 🎈: [perf](production-total-graph-widget): simplify component structure and improve machine selection handling

--- src/section/dashboard/production-total-graph-widget/production-total-graph-widget.jsx
+++ src/section/dashboard/production-total-graph-widget/production-total-graph-widget.jsx
@@ -1,130 +1,101 @@
-import { memo, useLayoutEffect, useEffect, useCallback, useState } from 'react'
-import { m } from 'framer-motion'
+import { memo, useCallback, useState, useMemo } from 'react'
 import { t } from 'i18next'
 import PropTypes from 'prop-types'
 import { Icon, useSettingContext, useResponsive, ICON_NAME } from 'hook'
 import { useSelector } from 'react-redux'
 import { dispatch } from 'store'
-import { getLogs, getLogGraphData, resetLogs, resetLogsGraphData } from 'store/slice'
+import { getLogGraphData } from 'store/slice'
 import { ERPProductionTotal } from 'section/log'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import { addLogSchema } from 'schema'
-import { useLogDefaultValues } from 'section/log/logs'
-import { Grid, Alert, IconButton, Divider } from '@mui/material'
-import { FormHeader, RHFAutocomplete, SkeletonGraphLoader, FormProvider } from 'component'
-import { useTheme, alpha } from '@mui/material/styles'
+import { Grid, Autocomplete, TextField } from '@mui/material'
+import { FormHeader, SkeletonGraphLoader } from 'component'
 import { GStyledSpanBox, GStyledLoadingButton } from 'theme/style'
-import { GLOBAL } from 'config'
-import { VARIANT, KEY, SNACK, FLEX, FLEX_DIR } from 'constant'
+import { KEY, FLEX, FLEX_DIR } from 'constant'
 
-const LogType = 'erp'
+const LOG_TYPE = 'erp'
+const LOG_PERIOD = 'Quarterly'
+const GRAPH_TYPE = 'length_and_waste'
 
 const ProductionTotalGraphWidget = ({ value, handleMachineDialog, handleMachineSiteDialog }) => {
  const { isLoading, logsGraphData } = useSelector(state => state.log)
  const { customerMachines } = useSelector(state => state.machine)
  const { customer } = useSelector(state => state.customer)
 
+ const [selectedMachine, setSelectedMachine] = useState(() => (customerMachines?.length > 0 ? { _id: null, name: 'All' } : { _id: null, name: 'All' }))
  const { themeMode } = useSettingContext()
  const isMobile = useResponsive('down', 'sm')
 
- const defaultValues = useLogDefaultValues(true, customerMachines[0])
+ const optionsMachines = useMemo(() => {
+  return customerMachines?.length > 0 ? [{ _id: null, name: 'All' }, ...customerMachines] : []
+ }, [customerMachines])
+ const graphLabels = useMemo(() => ({ yaxis: 'Cumulative Total Value', xaxis: LOG_PERIOD }), [])
 
- const methods = useForm({
-  resolver: yupResolver(addLogSchema),
-  defaultValues
- })
+ const handleMachineChange = useCallback((event, newMachine) => {
+  const machineId = newMachine?._id || null
+  dispatch(getLogGraphData(customer._id, machineId, LOG_TYPE, LOG_PERIOD, GRAPH_TYPE))
+ }, [])
 
- const logPeriod = 'Quarterly'
- const {
-  watch,
-  setValue,
-  handleSubmit,
-  trigger,
-  formState: { errors, isSubmitting }
- } = methods
-
- const { machine } = watch()
- const graphLabels = { yaxis: 'Cumulative Total Value', xaxis: logPeriod }
-
- const handleMachineChange = useCallback(
-  newMachine => {
-   setValue('machine', newMachine)
-   trigger('machine')
-  },
-  [setValue, trigger]
- )
-
- useEffect(() => {
-  if (customer?._id && machine?._id) {
-   dispatch(getLogGraphData(customer._id, machine._id, LogType, logPeriod, 'length_and_waste'))
+ const handleRefresh = useCallback(() => {
+  if (customer?._id && selectedMachine?._id) {
+   const machineId = selectedMachine?._id || null
+   dispatch(getLogGraphData(customer._id, machineId, LOG_TYPE, LOG_PERIOD, GRAPH_TYPE))
   }
- }, [customer?._id, machine?._id, dispatch])
+ }, [customer?._id, selectedMachine?._id, dispatch])
 
- const onGetGraphData = async data => {
-  const customerId = customer?._id
-  if (machine?._id) {
-   await dispatch(getLogGraphData(customerId, machine._id, LogType, logPeriod, 'length_and_waste'))
-  }
- }
+ const getOptionLabel = useCallback(option => `${option.serialNo || ''} ${option?.name && option?.name !== 'All' ? '-' : ''} ${option?.name || ''}`, [])
+ const isOptionEqualToValue = useCallback((option, value) => option?._id === value?._id, [])
 
  return (
-  <FormProvider methods={methods}>
-   <form onSubmit={handleSubmit(onGetGraphData)}>
-    {/* {!!errors.afterSubmit || (errors.afterSubmit && <Alert severity='error'>{errors?.afterSubmit?.message || SNACK.GENERIC_ERROR}</Alert>)} */}
-    <Grid container mb={2}>
-     <Grid item mb={2} bgcolor='background.paper' borderRadius={2} xs={12}>
-      <GStyledSpanBox>
-       <FormHeader label={'Daily Production Total'} />
-      </GStyledSpanBox>
-      <Grid
-       container
-       flexDirection={FLEX_DIR.ROW}
-       p={2}
-       sx={{
-        height: 'auto',
-        width: '100%',
-        maxWidth: '90vw',
-        overflow: KEY.AUTO,
-        scrollBehavior: 'smooth'
-       }}>
-       <Grid item xs={12}>
-        <Grid container justifyContent={FLEX.FLEX_END}>
-         <Grid item xs={12} sm={6}>
-          <RHFAutocomplete
-           name='machine'
-           label={t('machine.label')}
-           options={customerMachines || []}
-           isOptionEqualToValue={(option, value) => option._id === value._id}
-           getOptionLabel={option => `${option.serialNo || ''} ${option?.name ? '-' : ''} ${option?.name || ''}`}
-           renderOption={(props, option) => <li {...props} key={option?._id}>{`${option.serialNo || ''} ${option?.name ? '-' : ''} ${option?.name || ''}`}</li>}
-           onChange={(e, newValue) => handleMachineChange(newValue)}
-           size='small'
-          />
-         </Grid>
-         <Grid item xs={12} sm={6}>
-          <Grid container justifyContent={FLEX.FLEX_END}>
-           <GStyledLoadingButton mode={themeMode} fullWidth={isMobile} isLoading={isLoading} type='submit' onClick={() => onGetGraphData()} variant='contained' size='large'>
-            <Icon icon={ICON_NAME.REFRESH} />
-           </GStyledLoadingButton>
-          </Grid>
-         </Grid>
-        </Grid>
+  <Grid container mb={2}>
+   <Grid item mb={2} bgcolor='background.paper' borderRadius={2} xs={12}>
+    <GStyledSpanBox>
+     <FormHeader label={`${LOG_PERIOD} Production Total`} />
+    </GStyledSpanBox>
+    <Grid
+     container
+     flexDirection={FLEX_DIR.ROW}
+     p={2}
+     sx={{
+      height: 'auto',
+      width: '100%',
+      maxWidth: '90vw',
+      overflow: KEY.AUTO,
+      scrollBehavior: 'smooth'
+     }}>
+     <Grid item xs={12}>
+      <Grid container justifyContent={FLEX.FLEX_END}>
+       <Grid item xs={12} sm={6}>
+        <Autocomplete
+         fullWidth
+         options={optionsMachines}
+         value={selectedMachine}
+         isOptionEqualToValue={isOptionEqualToValue}
+         getOptionLabel={getOptionLabel}
+         renderInput={params => (
+          <TextField {...params} label={t('machine.machines.label')} size='small'>
+           {optionsMachines?.length === 0 && <SkeletonGraphLoader />}
+          </TextField>
+         )}
+         onChange={(event, newMachine) => {
+          setSelectedMachine(newMachine)
+          handleMachineChange(event, newMachine)
+         }}
+        />
        </Grid>
-       <Grid item xs={12}>
-        {!isLoading ? (
-         <ERPProductionTotal timePeriod={logPeriod} customer={customer} graphLabels={graphLabels} logsGraphData={logsGraphData} isDashboard graphHeight={300} />
-        ) : (
-         <m.div>
-          <SkeletonGraphLoader />
-         </m.div>
-        )}
+       <Grid item xs={12} sm={6}>
+        <Grid container justifyContent={FLEX.FLEX_END}>
+         <GStyledLoadingButton mode={themeMode} fullWidth={isMobile} isLoading={isLoading} onClick={handleRefresh} variant='contained' size='large'>
+          <Icon icon={ICON_NAME.REFRESH} />
+         </GStyledLoadingButton>
+        </Grid>
        </Grid>
       </Grid>
      </Grid>
+     <Grid item xs={12}>
+      <ERPProductionTotal timePeriod={LOG_PERIOD} customer={customer} graphLabels={graphLabels} logsGraphData={logsGraphData} isDashboard graphHeight={300} />
+     </Grid>
     </Grid>
-   </form>
-  </FormProvider>
+   </Grid>
+  </Grid>
  )
 }
 
