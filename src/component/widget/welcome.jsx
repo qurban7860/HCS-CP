871ac6eab79ebeeb2c5baf1f184066e59132e0f3
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-22 14:30:47 +1300

    #1496: added statistics to home page

--- src/component/widget/welcome.jsx
+++ src/component/widget/welcome.jsx
@@ -1,14 +1,21 @@
+import { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
 import { useSettingContext, Icon, ICON_NAME } from 'hook'
-import { Stack, Box, Grid, Card, Typography } from '@mui/material'
-import { GStyledWelcomeTitle, GStyledWelcomeContainerDiv, GStyledWelcomeDescription, GStyledSpanBox, GStyledTopBorderDivider, GCardOption } from 'theme/style'
-import { useTheme } from '@mui/material/styles'
+import { useTheme, Stack, Box, Grid, Card, Typography } from '@mui/material'
+import { SkeletonViewFormField } from 'component'
+import { GStyledWelcomeContainerDiv, GStyledWelcomeDescription, GStyledSpanBox, GStyledTopBorderDivider, GCardOption } from 'theme/style'
 import { KEY, TYPOGRAPHY } from 'constant'
 import { ASSET } from 'config'
+import { t } from 'i18next'
 
 function Welcome({ title, description, action, img, customer, ...other }) {
+ const { customerMachines, isLoading } = useSelector(state => state.machine)
  const { themeMode } = useSettingContext()
  const theme = useTheme()
+
+ const portalSyncedMachines = customerMachines?.filter(machine => machine?.portalKey)
+
  return (
   <GStyledWelcomeContainerDiv {...other}>
    <Stack
@@ -31,11 +38,55 @@ function Welcome({ title, description, action, img, customer, ...other }) {
        <Card {...GCardOption(themeMode)}>
         <GStyledTopBorderDivider mode={themeMode} />
         <GStyledSpanBox px={2}>
-         <Icon icon={ICON_NAME.COMPANY} />
+         <Icon icon={ICON_NAME.COMPANY} color={themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[400]} />
          <Typography variant={TYPOGRAPHY.H3} m={2}>
           {customer?.name}
          </Typography>
         </GStyledSpanBox>
+        <Box mb={2}>
+         <GStyledSpanBox px={2}>
+          {isLoading ? (
+           () => <SkeletonViewFormField />
+          ) : (
+           <Fragment>
+            <Icon
+             icon={ICON_NAME.FRAMA}
+             color={themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[400]}
+             sx={{
+              width: 15
+             }}
+            />
+            <Typography variant={TYPOGRAPHY.OVERLINE0} color={themeMode === KEY.LIGHT ? theme.palette.grey[500] : theme.palette.grey[500]} mx={1}>
+             {(customerMachines?.length > 1 ? t('machine.machines.label') : t('machine.label')) + ':'}
+            </Typography>
+            <Typography variant={TYPOGRAPHY.BODY1} color={themeMode === KEY.LIGHT ? theme.palette.grey[500] : theme.palette.grey[500]} mx={1}>
+             {customerMachines?.length} {KEY.MACHINES}
+            </Typography>
+           </Fragment>
+          )}
+         </GStyledSpanBox>
+         <GStyledSpanBox px={2}>
+          {isLoading ? (
+           () => <SkeletonViewFormField />
+          ) : (
+           <Fragment>
+            <Icon
+             icon={ICON_NAME.PORTAL_SYNC}
+             color={themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[400]}
+             sx={{
+              width: 15
+             }}
+            />
+            <Typography variant={TYPOGRAPHY.OVERLINE0} color={themeMode === KEY.LIGHT ? theme.palette.grey[500] : theme.palette.grey[500]} mx={1}>
+             {t('portal_synced.label') + ':'}
+            </Typography>
+            <Typography variant={TYPOGRAPHY.BODY1} color={themeMode === KEY.LIGHT ? theme.palette.grey[500] : theme.palette.grey[500]} mx={1}>
+             {portalSyncedMachines?.length} / {customerMachines?.length} {customerMachines?.length > 1 ? t('machine.machines.label') : t('machine.label')}
+            </Typography>
+           </Fragment>
+          )}
+         </GStyledSpanBox>
+        </Box>
        </Card>
       </Box>
      </Grid>
