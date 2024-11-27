aadf779989218a0d1af7aafe31947b154724da44
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-28 10:44:03 +1300

    housekeeping: simplify code formatting and improve readability

--- src/component/animate/motion-lazy-container.jsx
+++ src/component/animate/motion-lazy-container.jsx
@@ -1,20 +1,18 @@
 import PropTypes from 'prop-types'
 import { LazyMotion, m } from 'framer-motion'
 import { Box } from '@mui/material'
-
-// eslint-disable-next-line import/extensions
-const loadFeatures = () => import('./feature.js').then((res) => res.default)
+const loadFeatures = () => import('./feature.js').then(res => res.default)
 
 MotionLazyContainer.propTypes = {
-  children: PropTypes.node
+ children: PropTypes.node
 }
 
 export default function MotionLazyContainer({ children }) {
-  return (
-    <LazyMotion strict features={loadFeatures}>
-      <Box component={m.div} style={{ height: '100vh' }}>
-        {children}
-      </Box>
-    </LazyMotion>
-  )
+ return (
+  <LazyMotion strict features={loadFeatures}>
+   <Box component={m.div} style={{ height: '100vh' }}>
+    {children}
+   </Box>
+  </LazyMotion>
+ )
 }
