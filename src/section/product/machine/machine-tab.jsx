8b568a30f3fab61c2212702bd0288f6151e6f516
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-28 13:16:40 +1300

    🦄 [refax-s1]: streamline rendering of machine information fields and update titles for localization

--- src/section/product/machine/machine-tab.jsx
+++ src/section/product/machine/machine-tab.jsx
@@ -1,9 +1,8 @@
-import { useEffect, memo } from 'react'
+import { Fragment, useEffect, memo } from 'react'
 import { t } from 'i18next'
 import { useSelector, dispatch } from 'store'
 import { useParams } from 'react-router-dom'
 import { useSettingContext, useResponsive } from 'hook'
-import { PATH_CUSTOMER } from 'route/path'
 import {
  getCustomer,
  getMachine,
@@ -24,7 +23,7 @@ import { GStyledTopBorderDivider, GStyledFlexEndBox, GCardOption } from 'theme/s
 import { AuditBox, GridViewField, GridViewTitle } from 'component'
 import { MachineConnectionWidget, MachineConnectionListCard } from 'section/product/machine'
 import { MARGIN } from 'config'
-import { KEY, TITLE, VIEW_FORM, VARIANT, FLEX_DIR } from 'constant'
+import { TITLE, FLEX_DIR } from 'constant'
 import { truncate } from 'util/truncate'
 
 const MachineTab = () => {
@@ -36,8 +35,6 @@ const MachineTab = () => {
  const isDesktop = useMediaQuery(theme => theme.breakpoints.up('lg'))
  const isMobile = useResponsive('down', 'sm')
  const theme = useTheme()
-
- const { MACHINE } = VIEW_FORM
 
  useEffect(() => {
   if (id !== machine?._id) {
@@ -83,32 +80,32 @@ const MachineTab = () => {
   dispatch(setMachineSiteDialog(true))
  }
 
- const renderMachineInformation = () => {
+ const renderFields = config => {
   return (
-   <>
-    {fieldsMachineInformationConfig?.map(field => {
-     const additionalProps = {}
+   <Fragment>
+    {config?.map(field => {
+     const additionalProps = field.additionalProps ? field.additionalProps(defaultValues, handleCustomerDialog, themeMode) : {}
      let content = field.value(defaultValues)
-
-     if (field.type === 'link' && defaultValues[field.key]) {
-      content = (
-       <Link
-        onClick={event => field.linkProps.onClick(event, defaultValues?.customerId, handleCustomerDialog)}
-        href={field.linkProps.href}
-        underline={field.linkProps.underline}
-        color={field.linkProps.color(themeMode)}>
-        {truncate(content, field.truncate)}
-       </Link>
-      )
+     {
+      if (field.type === 'link' && defaultValues[field.key]) {
+       content = (
+        <Link
+         onClick={event => field.linkProps.onClick(event, defaultValues?.customerId, handleCustomerDialog)}
+         href={field.linkProps.href}
+         underline={field.linkProps.underline}
+         color={field.linkProps.color(themeMode)}>
+         {truncate(content, field.truncate)}
+        </Link>
+       )
+      }
      }
-
      return (
       <GridViewField key={field.key} heading={t(field.heading)} isLoading={isLoading} gridSize={field.gridSize || 6} {...additionalProps}>
        {content}
       </GridViewField>
      )
     })}
-   </>
+   </Fragment>
   )
  }
 
@@ -125,18 +122,10 @@ const MachineTab = () => {
      <Card {...GCardOption(themeMode)}>
       <GStyledTopBorderDivider mode={themeMode} />
       <Grid container spacing={2} px={1.5}>
-       <GridViewTitle title={TITLE.MACHINE_KEY_DETAILS} />
+       <GridViewTitle title={t('key_detail.key_details.label')} />
        <Grid item lg={12} sm={12}>
         <Grid container spacing={2} p={2} pb={5}>
-         <GridViewField heading={MACHINE.SERIAL_NO} isLoading={isLoading} gridSize={4}>
-          {defaultValues?.serialNo}
-         </GridViewField>
-         <GridViewField heading={MACHINE.MODEL} isLoading={isLoading} gridSize={4}>
-          {defaultValues?.machineModel}
-         </GridViewField>
-         <GridViewField heading={MACHINE.DEFAULT_PROFILE} isLoading={isLoading} gridSize={4}>
-          {Array.isArray(defaultValues?.profiles) && defaultValues?.profiles.length > 0 ? defaultValues?.profiles[0]?.flange + 'X' + defaultValues?.profiles[0]?.web : t('not_provided.label')}
-         </GridViewField>
+         {renderFields(fieldsKeyConfig)}
         </Grid>
        </Grid>
       </Grid>
@@ -147,64 +136,10 @@ const MachineTab = () => {
      <Card {...GCardOption(themeMode)}>
       <GStyledTopBorderDivider mode={themeMode} />
       <Grid container spacing={2} px={1.5}>
-       <GridViewTitle title={TITLE.MACHINE_INFO} />
+       <GridViewTitle title={t('machine_information.label')} />
        <Grid item lg={12} sm={12}>
         <Grid container spacing={2} p={2} pb={5}>
-         <GridViewField heading={t('name.label')} isLoading={isLoading} gridSize={12}>
-          {defaultValues?.name}
-         </GridViewField>
-         <GridViewField
-          heading={VIEW_FORM.ORGANIZATION}
-          isLoading={isLoading}
-          country={defaultValues?.customerCountry}
-          customerLink={PATH_CUSTOMER.customers.view(defaultValues?.customerId)}
-          onClick={handleCustomerDialog}>
-          {defaultValues.customer && (
-           <Link
-            onClick={event => handleCustomerDialog(event, defaultValues?.customerId)}
-            href='#'
-            underline='none'
-            color={themeMode === KEY.LIGHT ? theme.palette.howick.midBlue : theme.palette.howick.orange}>
-            {truncate(defaultValues?.customer, 21)}
-           </Link>
-          )}
-         </GridViewField>
-         <GridViewField heading={VIEW_FORM.STATUS} isLoading={isLoading}>
-          {defaultValues?.status}
-         </GridViewField>
-         <GridViewField heading={MACHINE.WORK_ORDER} isLoading={isLoading}>
-          {defaultValues?.workOrderRef}
-         </GridViewField>
-         <GridViewField heading={MACHINE.FINANCING_COMPANY} isLoading={isLoading}>
-          {defaultValues?.financialCompany}
-         </GridViewField>
-         <GridViewField heading={MACHINE.SUPPLIER} isLoading={isLoading}>
-          {defaultValues?.supplier}
-         </GridViewField>
-         <GridViewField heading={MACHINE.SUPPORT_EXPIRATION} isLoading={isLoading}>
-          {defaultValues?.supportExpireDate}
-         </GridViewField>
-         <GridViewField heading={MACHINE.PURCHASE_DATE} isLoading={isLoading}>
-          {defaultValues?.purchaseDate}
-         </GridViewField>
-         <GridViewField heading={MACHINE.MANUFACTURE_DATE} isLoading={isLoading}>
-          {defaultValues?.manufactureDate}
-         </GridViewField>
-         <GridViewField heading={MACHINE.SHIPPING_DATE} isLoading={isLoading}>
-          {defaultValues?.shippingDate}
-         </GridViewField>
-         <GridViewField heading={MACHINE.INSTALLATION_DATE} isLoading={isLoading}>
-          {defaultValues?.installationDate}
-         </GridViewField>
-         <GridViewField heading={t('installation_site.label')} isLoading={isLoading}>
-          {defaultValues?.installationSiteName}
-         </GridViewField>
-         <GridViewField heading={t('billing_site.label')} isLoading={isLoading}>
-          {defaultValues?.billingSiteName}
-         </GridViewField>
-         <GridViewField heading={VIEW_FORM.DESCRIPTION} isLoading={isLoading} gridSize={12}>
-          {defaultValues?.description}
-         </GridViewField>
+         {renderFields(fieldsMachineInformationConfig)}
         </Grid>
        </Grid>
       </Grid>
@@ -221,7 +156,7 @@ const MachineTab = () => {
      <Card {...GCardOption(themeMode)}>
       <GStyledTopBorderDivider mode={themeMode} />
       <Grid container spacing={2} px={1.5}>
-       <GridViewTitle title={TITLE.HOWICK_RESOURCES} />
+       <GridViewTitle title={t('howick_resources.label')} />
        <Divider orientation='horizontal' variant='fullWidth' />
        <Grid item lg={12} sm={12}>
         <HowickResources value={defaultValues} isLoading={isLoading} />
