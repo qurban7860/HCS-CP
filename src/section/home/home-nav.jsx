b771a84688c055b9da2d04490dcd8f52820fe63b
ballyalley-o <bally.lomibao@howickltd.com>
2024-11-29 23:36:43 +1300

    #1240 #1563: 🦄 [refax]: enhance layout responsiveness and improve component structure; replace Card with CommonFieldsCard

--- src/section/home/home-nav.jsx
+++ src/section/home/home-nav.jsx
@@ -1,49 +1,97 @@
+import { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { t } from 'i18next'
-import { useSettingContext, ICON_NAME } from 'hook'
-import { Grid } from '@mui/material'
+import { useSettingContext, ICON_NAME, useResponsive } from 'hook'
+import { Tag, Flex } from 'antd'
+import { useMediaQuery, Grid, Typography, ListItemText } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-import { GStyledHeaderCardContainer, GStyledTopBorderDivider, GStyledSpanBox } from 'theme/style'
+import { GStyledHeaderCardContainer, GStyledTopBorderDivider, GStyledSpanBox, GStyledListItemText, GStyledMiniChip } from 'theme/style'
 import { SvgFlagIcon, IconTooltip, ViewFormField, BadgeCardMedia, GridViewField } from 'component'
-import { KEY, VARIANT, LABEL, FLEX, FLEX_DIR, VIEW_FORM } from 'constant'
+import { KEY, TYPOGRAPHY, LABEL, FLEX, FLEX_DIR, SIZE } from 'constant'
 import { truncate } from 'util'
 
 const HomeNav = ({ value, isLoading }) => {
- const theme = useTheme()
  const { themeMode } = useSettingContext()
+ const theme = useTheme()
+ const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+ const isDesktop = useMediaQuery(theme.breakpoints.up('md'))
+
+ const renderStatusIcons = () => (
+  <Grid
+   container
+   justifyContent={FLEX.FLEX_END}
+   gap={isMobile ? 0.5 : 2}
+   sx={{
+    flexWrap: 'wrap',
+    alignItems: KEY.CENTER
+   }}>
+   <SvgFlagIcon country={value?.country} color={themeMode === KEY.LIGHT ? theme.palette.howick.midBlue : theme.palette.howick.bronze} dimension={isMobile ? 15 : 20} />
+   {value?.isActive ? (
+    <IconTooltip
+     title={t('active.label')}
+     icon={ICON_NAME.ACTIVE}
+     color={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
+     tooltipColor={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
+     isActiveIcon
+     iconOnly
+     dimension={isMobile ? 15 : 20}
+    />
+   ) : (
+    <IconTooltip title={LABEL.INACTIVE} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} dimension={isMobile ? 15 : 20} />
+   )}
+  </Grid>
+ )
 
  return (
-  <GStyledHeaderCardContainer>
-   <GStyledTopBorderDivider mode={themeMode} />
-   <Grid container px={1.5}>
-    <Grid item xs={12} lg={8}>
-     <GStyledSpanBox my={2}>
-      <BadgeCardMedia />
-      <ViewFormField variant={VARIANT.TYPOGRAPHY.H4} heading={VIEW_FORM.ORGANIZATION} isLoading={isLoading} isMachineView>
-       {truncate(value?.name, 50)}
-      </ViewFormField>
-     </GStyledSpanBox>
-    </Grid>
-    <Grid item xs={12} lg={4}>
-     <Grid container justifyContent={FLEX.FLEX_END} flexDirection={FLEX_DIR.COLUMN} alignContent={FLEX.FLEX_END}>
-      <Grid item xs={12} justifyContent={FLEX.FLEX_END} mt={2}>
-       <Grid container justifyContent={FLEX.FLEX_END} gap={2} alignItems={KEY.CENTER}>
-        <SvgFlagIcon country={value?.country} color={themeMode === KEY.LIGHT ? theme.palette.howick.midBlue : theme.palette.howick.bronze} dimension={24} />
-        {value?.isActive ? (
-         <IconTooltip title={LABEL.ACTIVE} icon={ICON_NAME.ACTIVE} color={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main} isActiveIcon iconOnly />
-        ) : (
-         <IconTooltip title={LABEL.INACTIVE} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} />
-        )}
-       </Grid>
+  <Fragment>
+   <GStyledHeaderCardContainer height={120}>
+    <GStyledTopBorderDivider mode={themeMode} />
+    <Grid container spacing={2} flexDirection={FLEX_DIR.COLUMN} px={isMobile ? 1 : 1.5}>
+     <Grid container flexDirection={FLEX_DIR.ROW} p={2}>
+      <Grid item xs={8} md={8} display={FLEX.FLEX} alignItems={KEY.CENTER} mt={2}>
+       <GStyledSpanBox gap={1} mb={1}>
+        <BadgeCardMedia />
+        <ListItemText
+         primary={
+          <Typography color={themeMode === KEY.LIGHT ? 'common.black' : 'common.white'} variant={isMobile ? TYPOGRAPHY.H5 : TYPOGRAPHY.H4}>
+           {truncate(value?.name, 50)}
+          </Typography>
+         }
+         secondary={
+          <Grid
+           container
+           sx={{
+            height: 20,
+            flexWrap: 'wrap',
+            overflow: 'auto'
+           }}>
+           {value?.tradingName &&
+            value?.tradingName?.slice(0, 3)?.map((item, index) => (
+             <Typography key={index} color={theme.palette.grey[500]} variant={isDesktop ? TYPOGRAPHY.OVERLINE1 : TYPOGRAPHY.OVERLINE_MINI} mx={0.5}>
+              {item}
+             </Typography>
+            ))}
+          </Grid>
+         }
+        />
+       </GStyledSpanBox>
+      </Grid>
+      <Grid item xs={4} sm={4} display={FLEX.FLEX} justifyContent={FLEX.FLEX_END}>
+       {renderStatusIcons()}
+      </Grid>
+      <Grid container p={2} display={FLEX.FLEX} justifyContent={FLEX_DIR.ROW}>
+       {/* <GridViewField heading={t('trading_name.label')} isLoading={isLoading} chip={value?.tradingName} gridSize={7} isNoBg /> */}
+       {!isDesktop && (
+        <Grid item xs={5} sm={12}>
+         {renderStatusIcons()}
+        </Grid>
+       )}
+       {/* {isDesktop && <GridViewField heading={t('website.label')} isLoading={isLoading} link={value?.website} gridSize={4} isNoBg />} */}
       </Grid>
      </Grid>
     </Grid>
-    <Grid container columnSpacing={2} px={2} pb={2}>
-     <GridViewField heading={t('trading_name.label')} isLoading={isLoading} chip={value?.tradingName} gridSize={8} isNoBg />
-     <GridViewField heading={t('website.label')} isLoading={isLoading} link={value?.website} gridSize={4} isNoBg />
-    </Grid>
-   </Grid>
-  </GStyledHeaderCardContainer>
+   </GStyledHeaderCardContainer>
+  </Fragment>
  )
 }
 
