492cdbec33f747eeccc496d0db2cfec02dd83b78
ballyalley-o <bally.lomibao@howickltd.com>
2024-12-09 17:40:05 +1000

    #1522: feat(role-types): add 'Customer' role to the role types configuration

--- src/config/role-types.js
+++ src/config/role-types.js
@@ -1,13 +1,11 @@
 export const roleTypes = [
-  { name: 'Super Admin', value: 'SuperAdmin' },
-  { name: 'Developer', value: 'Developer' },
-  { name: 'Sales Manager', value: 'SalesManager' },
-  { name: 'Sales Engineer', value: 'SalesEngineer' },
-  { name: 'Technical Manager', value: 'TechnicalManager' },
-  { name: 'Technician', value: 'Technician' },
-  // { name: 'Global Manager', value: 'GlobalManager'},
-  // { name: 'Regional Manager', value: 'RegionalManager'},
-  // { name: 'Support Manager', value: 'SupportManager'},
-  { name: 'API Access', value: 'APIAccess' },
-  { name: 'Email Access', value: 'EmailAccess' }
+ { name: 'Super Admin', value: 'SuperAdmin' },
+ { name: 'Developer', value: 'Developer' },
+ { name: 'Sales Manager', value: 'SalesManager' },
+ { name: 'Sales Engineer', value: 'SalesEngineer' },
+ { name: 'Technical Manager', value: 'TechnicalManager' },
+ { name: 'Technician', value: 'Technician' },
+ { name: 'Customer', value: 'Customer' },
+ { name: 'API Access', value: 'APIAccess' },
+ { name: 'Email Access', value: 'EmailAccess' }
 ]
