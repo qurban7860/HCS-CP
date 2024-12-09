e9b71c7410d4cb0e3e703e7d6c112008e891ea48
ballyalley-o <bally.lomibao@howickltd.com>
2024-12-09 17:59:48 +1000

    #1522 #1572 [schema]: update validation schema to include customer and contact details, and enhance email handling

--- src/schema/auth/user-invite.jsx
+++ src/schema/auth/user-invite.jsx
@@ -1,15 +1,27 @@
 import * as yup from 'yup'
-import { REGEX } from 'constant'
-
-const serialNoRegEx = new RegExp(REGEX.SERIAL_NO)
 
 export const UserInviteSchema = yup.object().shape({
- customerName: yup.string().required('Organization name is required'),
- contactPersonName: yup.string().required('Contact name is required'),
- address: yup.string(),
- email: yup.string().email('Invalid email format').required('Email is required'),
- machineSerialNos: yup.array().of(yup.string().matches(serialNoRegEx, 'Invalid serial number').length(5, 'Serial number must be 5 characters long')).min(1, 'At least one machine is required'),
- phoneNumber: yup.string(),
- country: yup.string().label('Country').nullable(),
- customerNote: yup.string().max(500, 'Customer note must be less than 500 characters')
+ customer: yup.object().required().label('Customer').nullable(),
+ contact: yup.object().required().nullable().label('Contact'),
+ name: yup.string().required().max(200).label('Full Name'),
+ phone: yup.string().label('Phone Number'),
+ email: yup
+  .string()
+  .transform(value => value?.toLowerCase())
+  .email()
+  .label('Email Address')
+  .required()
+  .trim()
+  .max(200),
+ loginEmail: yup
+  .string()
+  .transform(value => value?.toLowerCase())
+  .email()
+  .label('Log in /  Email Address')
+  .trim()
+  .max(200),
+ roles: yup.array().min(1, 'Please select at least one role').nullable().label('Roles').required(),
+ machines: yup.array().nullable(),
+ multiFactorAuthentication: yup.boolean(),
+ currentEmployee: yup.boolean()
 })
