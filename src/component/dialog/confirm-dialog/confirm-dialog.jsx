0e42cf6b45dc04e91c38a444c618904dc4658fd2
ballyalley-o <bally.lomibao@howickltd.com>
2024-12-11 10:29:30 +1000

    #1581 #1582 [perf & refax]:  update typography and prop naming; enhance user invite form with confirmation dialog

--- src/component/dialog/confirm-dialog/confirm-dialog.jsx
+++ src/component/dialog/confirm-dialog/confirm-dialog.jsx
@@ -2,19 +2,20 @@ import PropTypes from 'prop-types'
 import { useSettingContext } from 'hook'
 import { Dialog, Button, DialogTitle, DialogActions, DialogContent } from '@mui/material'
 import { GStyledTopBorderDivider, GBackdropPropsOption } from 'theme/style'
+import { TYPOGRAPHY } from 'constant'
 
-function ConfirmDialog({ title, content, action, open, onClose, SubButton = 'Cancel', ...other }) {
+function ConfirmDialog({ title, content, action, open, onClose, subButton = 'Cancel', ...other }) {
  const { themeMode } = useSettingContext()
  return (
   <Dialog fullWidth maxWidth='xs' open={open} onClose={onClose} {...other} BackdropProps={GBackdropPropsOption(themeMode)}>
    <GStyledTopBorderDivider mode={themeMode} />
-   <DialogTitle variant='h4' sx={{ pb: 1, pt: 2 }}>
+   <DialogTitle variant={TYPOGRAPHY.H4} sx={{ pb: 1, pt: 2 }}>
     {title}
    </DialogTitle>
    {content && <DialogContent sx={{ typography: 'body2', pt: 2 }}> {content} </DialogContent>}
    <DialogActions>
     <Button variant='outlined' color='inherit' onClick={onClose}>
-     {SubButton}
+     {subButton}
     </Button>
     {action}
    </DialogActions>
@@ -26,9 +27,9 @@ ConfirmDialog.propTypes = {
  open: PropTypes.bool,
  title: PropTypes.string,
  action: PropTypes.node,
- content: PropTypes.string,
+ content: PropTypes.any,
  onClose: PropTypes.func,
- SubButton: PropTypes.string
+ subButton: PropTypes.string
 }
 
 export default ConfirmDialog
--- src/section/auth/user-invite/user-invite-form.jsx
+++ src/section/auth/user-invite/user-invite-form.jsx
@@ -15,7 +15,8 @@ import {
  getActiveContacts,
  getCustomerRoles,
  setUserInviteDialog,
- resetUserInviteResponse,
+ setUserInviteConfirmDetails,
+ resetUserInviteConfirmDetails,
  resetCustomer,
  resetActiveContacts,
  resetSecurityUsers
@@ -23,12 +24,19 @@ import {
 import { UserInviteSchema } from 'schema'
 import { useUserInviteDefaultValues } from 'section/auth'
 import { Grid, Box, Checkbox, Typography } from '@mui/material'
-import { RHFRequiredTextFieldWrapper, UserInviteSuccessDialog } from 'component'
+import { RHFRequiredTextFieldWrapper, UserInviteSuccessDialog, ConfirmDialog } from 'component'
 import FormProvider, { RHFTextField, RHFAutocomplete, RHFPhoneInput } from 'component/hook-form'
 import { GStyledLoadingButton, GStyledSpanBox, GStyledFieldChip } from 'theme/style'
-import { RADIUS } from 'config'
 import { REGEX, LOCAL_STORAGE_KEY, KEY, LABEL, SIZE, COLOR, TYPOGRAPHY, FLEX_DIR, COUNTRY } from 'constant'
-import { delay, getCountryCode } from 'util'
+import { delay, getCountryCode, roleCoverUp } from 'util'
+
+const FORM_EL = {
+ contact: 'contact',
+ name: 'name',
+ phone: 'phone',
+ email: 'email',
+ roles: 'roles'
+}
 
 /**
  * Used by the customer admin to invite their own users to the Howick Portal
@@ -37,59 +45,42 @@ import { delay, getCountryCode } from 'util'
 function UserInviteForm() {
  const [isFormComplete, setIsFormComplete] = useState(false)
  const [isSuccessState, setIsSuccessState] = useState(false)
+ const [isConfirming, setIsConfirming] = useState(false)
  const { customer } = useSelector(state => state.customer)
  const { activeContacts } = useSelector(state => state.contact)
- const { userInviteDialog, securityUserTotalCount, securityUsers } = useSelector(state => state.user)
+ const { securityUserTotalCount, securityUsers, userInviteDialog } = useSelector(state => state.user)
  const { customerRoles } = useSelector(state => state.role)
  const { user } = useAuthContext()
  const { themeMode } = useSettingContext()
 
  const regEx = new RegExp(REGEX.ERROR_CODE)
  const isMobile = useResponsive('down', 'sm')
-
- const roleName = role => (role?.name === KEY.CUSTOMER_ADMIN ? 'Admin' : 'User' || '')
  const countryCode = getCountryCode(customer?.mainSite?.address?.country) || KEY.DEFAULT_COUNTRY_CODE
- const axiosToken = () => axios.CancelToken.source()
- const cancelTokenSource = axiosToken()
 
  useEffect(() => {
   dispatch(resetSecurityUsers())
   dispatch(resetCustomer())
   dispatch(resetActiveContacts())
-  dispatch(resetUserInviteResponse())
+  dispatch(resetUserInviteConfirmDetails())
  }, [dispatch])
 
- useEffect(() => {
-  const debouncedDispatch = _.debounce(() => {
-   if (user?.customer && !customer) dispatch(getCustomer(user.customer))
-  }, 300)
-  debouncedDispatch()
-  return () => debouncedDispatch.cancel()
+ const fetchCustomer = useCallback(() => {
+  if (user?.customer && !customer) {
+   dispatch(getCustomer(user.customer))
+  }
  }, [user?.customer, customer, dispatch])
 
  useEffect(() => {
-  const debouncedDispatch = _.debounce(() => {
-   if (user?.customer && !securityUsers) dispatch(getSecurityUsers(user.customer))
-  }, 300)
-  debouncedDispatch()
-  return () => debouncedDispatch.cancel()
- }, [user?.customer, securityUsers, dispatch])
+  const timer = setTimeout(fetchCustomer, 300)
+  return () => clearTimeout(timer)
+ }, [fetchCustomer])
 
  useEffect(() => {
-  const debouncedDispatch = _.debounce(() => {
-   if (user?.customer && !activeContacts) dispatch(getActiveContacts(user.customer))
-  }, 300)
-  debouncedDispatch()
-  return () => debouncedDispatch.cancel()
- }, [user?.customer, activeContacts, dispatch])
-
- useEffect(() => {
-  const debouncedDispatch = _.debounce(() => {
-   if (!customerRoles) dispatch(getCustomerRoles())
-  }, 300)
-  debouncedDispatch()
-  return () => debouncedDispatch.cancel()
- }, [user?.customer, customerRoles, dispatch])
+  const fetchData = async () => {
+   await Promise.all([dispatch(getSecurityUsers(user.customer)), dispatch(getActiveContacts(user.customer)), dispatch(getCustomerRoles())])
+  }
+  fetchData()
+ }, [user?.customer, dispatch])
 
  const defaultValues = useUserInviteDefaultValues(customer)
  const methods = useForm({
@@ -106,7 +97,7 @@ function UserInviteForm() {
   formState: { errors, isSubmitting, isSubmitSuccessful }
  } = methods
 
- const { email, contact, name, roles } = watch()
+ const { email, contact, name, roles, phone } = watch()
 
  const checkFormCompletion = useCallback(() => {
   setIsFormComplete(!!name && !!REGEX.EMAIL.test(email) && !!contact && roles.length > 0)
@@ -117,6 +108,11 @@ function UserInviteForm() {
    checkFormCompletion()
   }
  }, [checkFormCompletion])
+
+ const handleConfirmation = async () => {
+  setIsConfirming(true)
+  await Promise.all([dispatch(setUserInviteConfirmDetails({ customer, name, email, phone, roles })), dispatch(setUserInviteDialog(true))])
+ }
 
  const handleSubmissionError = error => {
   if (error?.errors) {
@@ -148,6 +144,7 @@ function UserInviteForm() {
  const onSubmit = async data => {
   try {
    setIsSuccessState(true)
+   setIsConfirming(false)
    const Data = {
     ...data,
     customer
@@ -157,13 +154,13 @@ function UserInviteForm() {
    if (REGEX.SUCCESS_CODE.test(response.status)) {
     snack(t('responses.success.user_invite_request_submitted'), { variant: COLOR.SUCCESS })
     reset()
-    dispatch(setUserInviteDialog(true))
     setIsFormComplete(false)
     setIsSuccessState(false)
    } else {
     console.error('Submission failed:', response)
     snack('Submission failed', { variant: COLOR.ERROR })
     setIsSuccessState(false)
+    setIsConfirming(false)
    }
   } catch (error) {
    handleSubmissionError(error)
@@ -241,45 +238,59 @@ function UserInviteForm() {
        name='roles'
        label={t('role.roles.label')}
        options={customerRoles}
-       getOptionLabel={option => roleName(option)}
+       getOptionLabel={option => roleCoverUp(option)}
        isOptionEqualToValue={(option, value) => option?._id === value?._id}
-       renderOption={(props, option, { selected }) => (
-        <li {...props}>
-         <Checkbox checked={selected} />
-         {roleName(option)}
-        </li>
-       )}
+       renderOption={(props, option, { selected }) => {
+        // eslint-disable-next-line react/prop-types
+        const { key, ...liProps } = props
+        return (
+         <li key={option?._id} {...liProps}>
+          <Checkbox checked={selected} />
+          {roleCoverUp(option)}
+         </li>
+        )
+       }}
        renderTags={(value, getTagProps) =>
         value.map((option, index) => {
          const { key, ...tagProps } = getTagProps({ index })
-         return <GStyledFieldChip key={option?._id} label={roleName(option)} mode={themeMode} {...tagProps} />
+         return <GStyledFieldChip key={option?._id} label={roleCoverUp(option)} mode={themeMode} {...tagProps} />
         })
        }
       />
      </Grid>
     </Grid>
-
     <Grid container direction={{ xs: 'column', md: 'row' }} justifyContent={KEY.CENTER}>
      <Grid item xs={12} sm={4} md={4}>
       <GStyledLoadingButton
        fullWidth
-       isLoading={isSubmitting}
        color={KEY.INHERIT}
        size={SIZE.LARGE}
-       type={KEY.SUBMIT}
+       type={'button'}
        variant={KEY.CONTAINED}
-       loading={isSubmitting}
+       //    isLoading={isConfirming}
+       loading={isConfirming}
        mode={themeMode}
-       disabled={!isFormComplete}
-       sx={RADIUS.BORDER}>
+       onClick={handleConfirmation}
+       disabled={!isFormComplete}>
        {t('send_invitation.label').toUpperCase()}
       </GStyledLoadingButton>
      </Grid>
-     {/* Spacer */}
      <Box height={{ xs: 50, md: 100 }} />
     </Grid>
+
+    {userInviteDialog && (
+     <UserInviteSuccessDialog
+      setIsConfirming={setIsConfirming}
+      isSubmitSuccessful={isSubmitSuccessful}
+      onConfirm={handleSubmit(onSubmit)}
+      action={
+       <GStyledLoadingButton isLoading={isSubmitting} color={KEY.INHERIT} type={KEY.SUBMIT} loading={isSubmitting} variant={KEY.CONTAINED} mode={themeMode}>
+        {t('confirm.label').toUpperCase()}
+       </GStyledLoadingButton>
+      }
+     />
+    )}
    </FormProvider>
-   {userInviteDialog && <UserInviteSuccessDialog />}
   </Fragment>
  )
 }
