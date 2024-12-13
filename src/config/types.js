3c54abf99ce937e64ba18c947d5b19817a95633f
ballyalley-o <bally.lomibao@howickltd.com>
2024-12-13 16:15:02 +1000

    HPS-1609 ✨ [feat]: add BADGE icon to icon directory and rename status-types to types, including role types

--- src/config/types.js
+++ src/config/types.js
@@ -3,3 +3,9 @@ export const STATUS_TYPES = [
  { id: 'active', label: 'Active', value: 'active' },
  { id: 'inActive', label: 'In Active', value: 'inActive' }
 ]
+
+export const ROLE_TYPES = [
+ { id: 'all', label: 'All', value: 'all' },
+ { id: 'admin', label: 'Admin', value: 'CustomerAdmin' },
+ { id: 'user', label: 'User', value: 'CustomerUser' }
+]
