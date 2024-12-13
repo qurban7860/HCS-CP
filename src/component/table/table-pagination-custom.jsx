da0d511690d0f2c163abc1154e87c9efff749fb2
ballyalley-o <bally.lomibao@howickltd.com>
2024-12-14 07:06:46 +1000

    HPS-1610: enhance layout and add noPaginationToolbar prop

--- src/component/table/table-pagination-custom.jsx
+++ src/component/table/table-pagination-custom.jsx
@@ -27,6 +27,7 @@ function TablePaginationCustom({
  rowsPerPageOptions = [10, 20, 50, 100],
  columnFilterButtonData = [],
  columnButtonClickHandler = () => {},
+ noPaginationToolbar = false,
  sx,
  ...other
 }) {
@@ -65,145 +66,167 @@ function TablePaginationCustom({
    sx={{
     position: 'relative',
     display: 'flex',
+    flexDirection: { xs: 'column', md: 'row' },
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: { xs: 'stretch', md: 'center' },
     color: themeMode === KEY.LIGHT ? theme.palette.grey[900] : theme.palette.grey[0],
     background: themeMode === KEY.LIGHT ? theme.palette.background.default : theme.palette.grey[800],
     ...sx
    }}>
-   <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+   <Box
+    sx={{
+     display: 'flex',
+     flexDirection: { xs: 'column', md: 'row' },
+     width: { xs: '100%', md: 'auto' },
+     gap: { xs: 1, md: 0 },
+     mb: { xs: 1, md: 0 }
+    }}>
     {columnFilterButtonData?.length > 0 && (
-     <Fragment>
-      <Box sx={{ flexGrow: 1, pl: 2 }}>
-       <Button
-        startIcon={<Icon icon={ICON_NAME.COLUMN} />}
-        variant='outlined'
-        disabled={disabled}
-        onClick={e => setAnchorEl(e.currentTarget)}
-        sx={{
-         mr: 1,
-         color: themeMode === KEY.LIGHT ? theme.palette.grey[800] : theme.palette.grey[0],
-         border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[0]}`
-        }}>
-        <Typography variant={isDesktop ? TYPOGRAPHY.BODY2 : TYPOGRAPHY.CAPTION}>{t('column.columns.label')}</Typography>
-       </Button>
-       <Menu
-        id='long-menu'
-        MenuListProps={{ 'aria-labelledby': 'long-button' }}
-        anchorEl={anchorEl}
-        open={!!anchorEl}
-        onClose={handleClose}
-        PaperProps={{ style: { width: '25ch', maxHeight: 300, overflowY: 'auto' } }}>
-        {columnFilterButtonData?.map(column => (
-         <MenuItem dense sx={{ p: 0 }} key={column.id} onClick={() => handleColumnClick(column)}>
-          <Checkbox checked={column.checked} disabled={column?.alwaysShow} />
-          {normalizer(column.label) === 'machine' ? (
-           <Icon title={t('machine.label')} icon={ICON_NAME.FRAMA} dimension={20} color={theme.palette.grey[400]} />
-          ) : normalizer(column.label) === 'date' ? (
-           <Icon icon={ICON_NAME.CALENDAR_CLOCK} dimension={20} c color={theme.palette.grey[400]} />
-          ) : (
-           <GStyledMachineChip
-            mode={themeMode}
-            size='small'
-            label={
-             <Typography variant={TYPOGRAPHY.OVERLINE0} p={0}>
-              {charAtText(column.label)}
-             </Typography>
-            }
-            disabled
-           />
-          )}
-          &nbsp;
-          {column.label}
-         </MenuItem>
-        ))}
-       </Menu>
-      </Box>
-     </Fragment>
+     <Box
+      sx={{
+       pl: { xs: 0, md: 2 },
+       width: '100%',
+       mb: { xs: 1, md: 0 },
+       mr: { md: 1 }
+      }}>
+      <Button
+       fullWidth
+       startIcon={<Icon icon={ICON_NAME.COLUMN} />}
+       variant='outlined'
+       disabled={disabled}
+       onClick={e => setAnchorEl(e.currentTarget)}
+       sx={{
+        color: themeMode === KEY.LIGHT ? theme.palette.grey[800] : theme.palette.grey[0],
+        border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[0]}`
+       }}>
+       <Typography variant={TYPOGRAPHY.BODY2}>{t('column.columns.label')}</Typography>
+      </Button>
+      <Menu id='column-menu' anchorEl={anchorEl} open={!!anchorEl} onClose={handleClose} PaperProps={{ style: { width: isDesktop ? '25ch' : '100%', maxHeight: 300, overflowY: 'auto' } }}>
+       {columnFilterButtonData?.map(column => (
+        <MenuItem dense sx={{ p: 0 }} key={column.id} onClick={() => handleColumnClick(column)}>
+         <Checkbox checked={column.checked} disabled={column?.alwaysShow} />
+         {normalizer(column.label) === 'machine' ? (
+          <Icon title={t('machine.label')} icon={ICON_NAME.FRAMA} dimension={20} color={theme.palette.grey[400]} />
+         ) : normalizer(column.label) === 'date' ? (
+          <Icon icon={ICON_NAME.CALENDAR_CLOCK} dimension={20} c color={theme.palette.grey[400]} />
+         ) : (
+          <GStyledMachineChip
+           mode={themeMode}
+           size={KEY.SMALL}
+           label={
+            <Typography variant={TYPOGRAPHY.OVERLINE0} p={0}>
+             {charAtText(column.label)}
+            </Typography>
+           }
+           disabled
+          />
+         )}
+         &nbsp;
+         {column.label}
+        </MenuItem>
+       ))}
+      </Menu>
+     </Box>
     )}
+
     {showFilterStatus && (
-     <Fragment>
-      <Box sx={{ flexGrow: 1, pl: 2 }}>
-       <Button
-        startIcon={<Icon icon={ICON_NAME.STATUS_FILTER} />}
-        variant='outlined'
-        onClick={e => setFilterStatusAnchorEl(e.currentTarget)}
-        sx={{
-         mr: 1,
-         color: themeMode === KEY.LIGHT ? theme.palette.grey[800] : theme.palette.grey[0],
-         border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[0]}`
-        }}>
-        <GStyledSpanBox>
-         <Typography variant={isDesktop ? TYPOGRAPHY.BODY2 : TYPOGRAPHY.CAPTION}>{t('status.label') + ':'} </Typography>
-         <ColorizedStatusTextBox status={currentFilterStatus} />
-         {!filterStatusAnchorEl ? <Icon icon={ICON_NAME.CHEVRON_DOWN} /> : <Icon icon={ICON_NAME.CHEVRON_UP} />}
-        </GStyledSpanBox>
-       </Button>
-       <Menu
-        id='long-menu'
-        MenuListProps={{ 'aria-labelledby': 'long-button' }}
-        anchorEl={filterStatusAnchorEl}
-        open={!!filterStatusAnchorEl}
-        onClose={handleFilterStatusClose}
-        PaperProps={{ style: { width: '25ch', maxHeight: 300, overflowY: 'auto' } }}>
-        {STATUS_TYPES?.map(column => (
-         <MenuItem dense sx={{ p: 1 }} key={column.id} onClick={e => handleFilterStatus(e, column.value)} value={column.value}>
-          <Typography variant={TYPOGRAPHY.BODY2}>{column.label}</Typography>
-         </MenuItem>
-        ))}
-       </Menu>
-      </Box>
-     </Fragment>
+     <Box
+      sx={{
+       pl: { xs: 0, md: 2 },
+       width: '100%',
+       mb: { xs: 1, md: 0 },
+       mr: { md: 1 }
+      }}>
+      <Button
+       fullWidth
+       startIcon={<Icon icon={ICON_NAME.STATUS_FILTER} />}
+       variant='outlined'
+       onClick={e => setFilterStatusAnchorEl(e.currentTarget)}
+       sx={{
+        color: themeMode === KEY.LIGHT ? theme.palette.grey[800] : theme.palette.grey[0],
+        border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[0]}`
+       }}>
+       <GStyledSpanBox>
+        <Typography variant={TYPOGRAPHY.BODY2}>{t('status.label') + ': '}</Typography>
+        <ColorizedStatusTextBox status={currentFilterStatus} />
+        {!filterStatusAnchorEl ? <Icon icon={ICON_NAME.CHEVRON_DOWN} /> : <Icon icon={ICON_NAME.CHEVRON_UP} />}
+       </GStyledSpanBox>
+      </Button>
+      <Menu
+       id='status-menu'
+       anchorEl={filterStatusAnchorEl}
+       open={!!filterStatusAnchorEl}
+       onClose={handleFilterStatusClose}
+       PaperProps={{ style: { width: isDesktop ? '25ch' : '90%', maxHeight: 300, overflowY: 'auto' } }}>
+       {STATUS_TYPES?.map(column => (
+        <MenuItem dense sx={{ p: 1 }} key={column.id} onClick={e => handleFilterStatus(e, column.value)} value={column.value}>
+         <Typography variant={TYPOGRAPHY.BODY2}>{column.label}</Typography>
+        </MenuItem>
+       ))}
+      </Menu>
+     </Box>
     )}
+
     {showFilterRole && (
-     <Fragment>
-      <Box sx={{ flexGrow: 1, pl: 2 }}>
-       <Button
-        startIcon={<Icon icon={ICON_NAME.BADGE} />}
-        variant='outlined'
-        onClick={e => setFilterRoleAnchorEl(e.currentTarget)}
-        sx={{
-         mr: 1,
-         color: themeMode === KEY.LIGHT ? theme.palette.grey[800] : theme.palette.grey[0],
-         border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[0]}`
-        }}>
-        <GStyledSpanBox>
-         <Typography variant={isDesktop ? TYPOGRAPHY.BODY2 : TYPOGRAPHY.CAPTION}>{t('role.label') + ':'} </Typography>
-         <ColorizedStatusTextBox role={currentFilterRole} />
-         {!filterRoleAnchorEl ? <Icon icon={ICON_NAME.CHEVRON_DOWN} /> : <Icon icon={ICON_NAME.CHEVRON_UP} />}
-        </GStyledSpanBox>
-       </Button>
-       <Menu
-        id='long-menu'
-        MenuListProps={{ 'aria-labelledby': 'long-button' }}
-        anchorEl={filterRoleAnchorEl}
-        open={!!filterRoleAnchorEl}
-        onClose={handleFilterRoleClose}
-        PaperProps={{ style: { width: '25ch', maxHeight: 300, overflowY: 'auto' } }}>
-        {ROLE_TYPES?.map(column => (
-         <MenuItem dense sx={{ p: 1 }} key={column.id} onClick={e => handleFilterRole(e, column.value)} value={column.value}>
-          <Typography variant={TYPOGRAPHY.BODY2}>{column.label}</Typography>
-         </MenuItem>
-        ))}
-       </Menu>
-      </Box>
-     </Fragment>
+     <Box
+      sx={{
+       pl: { xs: 0, md: 2 },
+       width: '100%',
+       mb: { xs: 1, md: 0 }
+      }}>
+      <Button
+       fullWidth
+       startIcon={<Icon icon={ICON_NAME.BADGE} />}
+       variant='outlined'
+       onClick={e => setFilterRoleAnchorEl(e.currentTarget)}
+       sx={{
+        color: themeMode === KEY.LIGHT ? theme.palette.grey[800] : theme.palette.grey[0],
+        border: `1px solid ${themeMode === KEY.LIGHT ? theme.palette.grey[600] : theme.palette.grey[0]}`
+       }}>
+       <GStyledSpanBox>
+        <Typography variant={TYPOGRAPHY.BODY2}>{t('role.label') + ': '}</Typography>
+        <ColorizedStatusTextBox role={currentFilterRole} />
+        {!filterRoleAnchorEl ? <Icon icon={ICON_NAME.CHEVRON_DOWN} /> : <Icon icon={ICON_NAME.CHEVRON_UP} />}
+       </GStyledSpanBox>
+      </Button>
+      <Menu
+       id='role-menu'
+       anchorEl={filterRoleAnchorEl}
+       open={!!filterRoleAnchorEl}
+       onClose={handleFilterRoleClose}
+       PaperProps={{ style: { width: isDesktop ? '25ch' : '90%', maxHeight: 300, overflowY: 'auto' } }}>
+       {ROLE_TYPES?.map(column => (
+        <MenuItem dense sx={{ p: 1 }} key={column.id} onClick={e => handleFilterRole(e, column.value)} value={column.value}>
+         <Typography variant={TYPOGRAPHY.BODY2}>{column.label}</Typography>
+        </MenuItem>
+       ))}
+      </Menu>
+     </Box>
     )}
    </Box>
-   <GStyledTablePaginationCustom
-    labelRowsPerPage={t('row.label') + ':'}
-    data={data}
-    count={count}
-    mode={themeMode}
-    rowsPerPage={rowsPerPage}
-    rowsPerPageOptions={isPaginationDisabled ? false : filteredRowsPerPageOptions}
-    component={component}
-    showLastButton
-    showFirstButton
-    {...other}
-    sx={{ '.MuiTablePagination-toolbar': { height: '20px', width: '!important 200px' }, borderTop: 'none' }}
-    {...other}
-   />
+   {!noPaginationToolbar && (
+    <GStyledTablePaginationCustom
+     labelRowsPerPage={t('row.label') + ':'}
+     data={data}
+     count={count}
+     mode={themeMode}
+     rowsPerPage={rowsPerPage}
+     rowsPerPageOptions={isPaginationDisabled ? false : filteredRowsPerPageOptions}
+     component={component}
+     showLastButton
+     showFirstButton
+     {...other}
+     sx={{
+      '.MuiTablePagination-toolbar': {
+       height: '20px',
+       width: '!important 200px'
+      },
+      borderTop: 'none',
+      width: { xs: '100%', md: 'auto' }
+     }}
+    />
+   )}
+
    {onChangeDense && (
     <FormControlLabel
      label='Dense'
@@ -239,7 +262,8 @@ TablePaginationCustom.propTypes = {
  rowsPerPage: PropTypes.number,
  rowsPerPageOptions: PropTypes.arrayOf(PropTypes.number),
  columnFilterButtonData: PropTypes.array,
- columnButtonClickHandler: PropTypes.func
+ columnButtonClickHandler: PropTypes.func,
+ noPaginationToolbar: PropTypes.bool
 }
 
 export default memo(TablePaginationCustom)
