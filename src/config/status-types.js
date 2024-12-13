2a7c3eba7ddef319f51de8c21181edf5de90f0c5
ballyalley-o <bally.lomibao@howickltd.com>
2024-12-13 14:39:55 +1000

    HPS-1577 feat: add CHEVRON_UP icon and STATUS_TYPES configuration

---
+++ src/config/status-types.js
@@ -0,0 +1,5 @@
+export const STATUS_TYPES = [
+ { id: 'all', label: 'All', value: 'all' },
+ { id: 'active', label: 'Active', value: 'active' },
+ { id: 'inActive', label: 'In Active', value: 'inActive' }
+]
