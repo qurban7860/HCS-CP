47f2830ba46a38c4886829472cbdd00c56d40bbc
ballyalley-o <bally.lomibao@howickltd.com>
2024-12-16 21:02:50 +1000

    enhance Icon component to enforce icon prop type validation with PropTypes

--- src/hook/use-icon/icon.jsx
+++ src/hook/use-icon/icon.jsx
@@ -1,9 +1,13 @@
+import PropTypes from 'prop-types'
 import { useIcon } from 'hook'
-import { ICON_WEB } from 'config'
 
-const Icon = ({ icon, priorityIcon, ...other }) => {
-  const { Icon: IconInstance, iconSrc } = useIcon(icon)
-  return <IconInstance icon={iconSrc} {...other} />
+const Icon = ({ icon, ...other }) => {
+ const { Icon: IconInstance, iconSrc } = useIcon(icon)
+ return <IconInstance icon={iconSrc} {...other} />
+}
+
+Icon.propTypes = {
+ icon: PropTypes.string.isRequired
 }
 
 export default Icon
