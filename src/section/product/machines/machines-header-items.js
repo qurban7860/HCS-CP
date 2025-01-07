353200ef0f3f6f1d0f8cd28018f1dad41c2d6a1c
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-08 12:10:41 +1300

    [refax]: HEADER_ITEMS to use dot notation for nested properties in machines header

--- src/section/product/machines/machines-header-items.js
+++ src/section/product/machines/machines-header-items.js
@@ -4,9 +4,9 @@ import { fDate } from 'util'
 export const HEADER_ITEMS = [
  { id: 'serialNo', label: t('serial_number.label'), align: 'left', checked: true, value: mach => mach?.serialNo },
  //  { id: 'name', label: t('name.label'), align: 'left', checked: false },
- { id: 'model', label: t('model.label'), align: 'left', checked: true, value: mach => mach?.machineModel?.name },
+ { id: 'machineModel.name', label: t('model.label'), align: 'left', checked: true, value: mach => mach?.machineModel?.name },
  { id: 'installationDate', label: t('installation_date.label'), align: 'left', checked: true, value: mach => fDate(mach?.installationDate) },
  { id: 'shippingDate', label: t('shipping_date.label'), align: 'left', checked: true, value: mach => fDate(mach?.shippingDate) },
- { id: 'status', label: t('status.label'), align: 'left', checked: true, value: mach => mach?.status?.name },
+ { id: 'status.name', label: t('status.label'), align: 'left', checked: true, value: mach => mach?.status?.name },
  { id: 'isActive', label: t('active.label'), align: 'center', checked: true, width: '5%', value: mach => mach?.isActive }
 ]
