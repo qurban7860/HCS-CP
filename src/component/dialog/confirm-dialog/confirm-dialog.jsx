908a93e8900319b46821f37246ab44d90c3ca1c8
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-09 13:03:13 +1300

    🦄 [refax]: enhance ConfirmDialog with internationalization and loading state support

--- src/component/dialog/confirm-dialog/confirm-dialog.jsx
+++ src/component/dialog/confirm-dialog/confirm-dialog.jsx
@@ -1,23 +1,28 @@
 import PropTypes from 'prop-types'
+import { t } from 'i18next'
 import { useSettingContext } from 'hook'
-import { Dialog, Button, DialogTitle, DialogActions, DialogContent } from '@mui/material'
-import { GStyledTopBorderDivider, GBackdropPropsOption } from 'theme/style'
-import { TYPOGRAPHY } from 'constant'
+import { Dialog, DialogTitle, DialogActions, DialogContent } from '@mui/material'
+import { GStyledTopBorderDivider, GBackdropPropsOption, GStyledCloseButton, GStyledDefLoadingButton } from 'theme/style'
+import { SZ, TYPOGRAPHY } from 'constant'
 
-function ConfirmDialog({ title, content, action, open, onClose, subButton = 'Cancel', ...other }) {
+function ConfirmDialog({ title, content, action, open, onClose, onClick, actionButtonTextColor, actionButtonBgColor, i18SubButtonLabel = 'cancel.label', i18ActionButtonLabel, isLoading, ...other }) {
  const { themeMode } = useSettingContext()
  return (
-  <Dialog fullWidth maxWidth='xs' open={open} onClose={onClose} {...other} BackdropProps={GBackdropPropsOption(themeMode)}>
+  <Dialog fullWidth maxWidth={SZ.XS} open={open} onClose={onClose} {...other} BackdropProps={GBackdropPropsOption(themeMode)}>
    <GStyledTopBorderDivider mode={themeMode} />
    <DialogTitle variant={TYPOGRAPHY.H4} sx={{ pb: 1, pt: 2 }}>
     {title}
    </DialogTitle>
-   {content && <DialogContent sx={{ typography: 'body2', pt: 2 }}> {content} </DialogContent>}
+   {content && <DialogContent sx={{ typography: TYPOGRAPHY.BODY2, pt: 2 }}> {content} </DialogContent>}
    <DialogActions>
-    <Button variant='outlined' color='inherit' onClick={onClose}>
-     {subButton}
-    </Button>
-    {action}
+    <GStyledCloseButton onClick={onClose}>{t(i18SubButtonLabel).toUpperCase()}</GStyledCloseButton>
+    {action ? (
+     action
+    ) : (
+     <GStyledDefLoadingButton isLoading={isLoading} type={'button'} textColor={actionButtonTextColor} bgColor={actionButtonBgColor} loading={isLoading} onClick={onClick}>
+      {t(i18ActionButtonLabel).toUpperCase()}
+     </GStyledDefLoadingButton>
+    )}
    </DialogActions>
   </Dialog>
  )
@@ -29,7 +34,12 @@ ConfirmDialog.propTypes = {
  action: PropTypes.node,
  content: PropTypes.any,
  onClose: PropTypes.func,
- subButton: PropTypes.string
+ onClick: PropTypes.func,
+ isLoading: PropTypes.bool,
+ actionButtonTextColor: PropTypes.string,
+ actionButtonBgColor: PropTypes.string,
+ i18SubButtonLabel: PropTypes.string,
+ i18ActionButtonLabel: PropTypes.string
 }
 
 export default ConfirmDialog
