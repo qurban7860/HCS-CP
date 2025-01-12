250f0a87799b3844b1fcf88c56053fceb622a838
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-13 12:03:45 +1300

    HPS-1652 [resolve-warning]: convert Icon component to forwardRef for improved ref handling

--- src/hook/use-icon/icon.jsx
+++ src/hook/use-icon/icon.jsx
@@ -1,11 +1,13 @@
+import { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { useIcon } from 'hook'
 
-const Icon = ({ icon, ...other }) => {
+const Icon = forwardRef(({ icon, ...other }, ref) => {
  const { Icon: IconInstance, iconSrc } = useIcon(icon)
- return <IconInstance icon={iconSrc} {...other} />
-}
+ return <IconInstance icon={iconSrc} ref={ref} {...other} />
+})
 
+Icon.displayName = 'Icon'
 Icon.propTypes = {
  icon: PropTypes.string.isRequired
 }
