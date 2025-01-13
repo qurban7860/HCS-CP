51b1fdfdae0b61951f3f35734e35482d9901d864
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-13 15:33:46 +1300

    HPS-1652 [resolve-error]:change borderRadius prop type

--- src/component/image/image.jsx
+++ src/component/image/image.jsx
@@ -70,7 +70,7 @@ Image.propTypes = {
  sx: PropTypes.object,
  effect: PropTypes.string,
  disabledEffect: PropTypes.bool,
- borderRadius: PropTypes.string,
+ borderRadius: PropTypes.number,
  ratio: PropTypes.oneOf(['4/3', '3/4', '6/4', '4/6', '16/9', '9/16', '21/9', '9/21', '1/1'])
 }
 
