b497eb78c801aec47aa51b2a84ba67ca5fb5ef33
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-13 15:42:24 +1300

    🦄 [refax]: extend propTypes and improve SkeletonViewFormField component for customizable skeleton bars

--- src/component/skeleton/view-form-field.jsx
+++ src/component/skeleton/view-form-field.jsx
@@ -2,16 +2,20 @@ import PropTypes from 'prop-types'
 import { Stack, Skeleton } from '@mui/material'
 
 SkeletonViewFormField.propTypes = {
-  variant: PropTypes.string
+  variant: PropTypes.string,
+  isDouble: PropTypes.bool,
+  firstBarHeight: PropTypes.number,
+  secondBarHeight: PropTypes.number,
+  bgColor: PropTypes.string
 }
 
-export default function SkeletonViewFormField({ variant }) {
-  return (
-    <Stack spacing={1} direction="row" alignItems="center" sx={{ py: 0.5, pl: 0, pr: 5 }}>
-      <Stack spacing={variant ? 1.6 : 0.5} sx={{ flexGrow: 1 }}>
-        <Skeleton variant="text" sx={{ height: 9 }} />
-        <Skeleton variant="text" sx={{ height: 6 }} />
-      </Stack>
-    </Stack>
-  )
+export default function SkeletonViewFormField({ isDouble, firstBarHeight = 20, secondBarHeight = 6, bgColor = 'background.paper' }) {
+ return (
+  <Stack spacing={1} direction='row' alignItems='center' sx={{ py: 0.5, pl: 0, pr: 5 }}>
+   <Stack spacing={0.6} sx={{ flexGrow: 1 }}>
+    <Skeleton variant={'text'} sx={{ height: firstBarHeight, width: '100%', borderRadius: 1, backgroundColor: bgColor }} />
+    {isDouble && <Skeleton variant={'text'} sx={{ height: secondBarHeight, width: '100%', borderRadius: 1, backgroundColor: bgColor }} />}
+   </Stack>
+  </Stack>
+ )
 }
