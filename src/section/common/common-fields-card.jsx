50bc33c0e24477c4284583e383e6b428e05cbc6c
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-21 15:46:02 +1300

    HPS-1679 [refax]: enhance CommonFieldsCard with user invite functionality and loading state handling

--- src/section/common/common-fields-card.jsx
+++ src/section/common/common-fields-card.jsx
@@ -1,14 +1,14 @@
 import { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { t } from 'i18next'
-import { useSettingContext, ICON_NAME } from 'hook'
+import { useSettingContext, Icon, ICON_NAME } from 'hook'
 import { useTheme, Box, Card, Grid, Link } from '@mui/material'
 import { GridViewField, GridViewTitle, IconTooltip } from 'component'
-import { GCardOption, GStyledTopBorderDivider, GStyledSpanBox } from 'theme/style'
+import { GCardOption, GStyledTopBorderDivider, GStyledSpanBox, GStyledIconLoadingButton } from 'theme/style'
 import { KEY, FLEX } from 'constant'
 import { truncate } from 'util'
 
-const CommonFieldsCard = ({ defaultValues, fieldsConfig, i18nKey, isLoading, handleDialog, isChildren, children, withStatusIcon }) => {
+const CommonFieldsCard = ({ defaultValues, fieldsConfig, i18nKey, isLoading, handleDialog, isChildren, children, withStatusIcon, isContactsPage, contactHasActiveUser, handleUserInvite }) => {
  const { themeMode } = useSettingContext()
  const theme = useTheme()
 
@@ -60,18 +60,34 @@ const CommonFieldsCard = ({ defaultValues, fieldsConfig, i18nKey, isLoading, han
     <Grid container spacing={1} px={1.5}>
      {withStatusIcon && (
       <Grid item xs={12} sm={12} mt={1.5}>
-       <GStyledSpanBox justifyContent={FLEX.FLEX_END}>
+       <GStyledSpanBox justifyContent={FLEX.FLEX_END} gap={2}>
+       {isContactsPage && (
+          contactHasActiveUser ? (
+            <IconTooltip
+              title={t('has_active_user_account.label')}
+              icon={ICON_NAME.USER_ACTIVE}
+              color={themeMode === KEY.LIGHT ? theme.palette.howick.blue : theme.palette.howick.blue}
+              tooltipColor={themeMode === KEY.LIGHT ? theme.palette.howick.blue : theme.palette.howick.blue}
+              tooltipTextColor={theme.palette.common.white}
+              iconOnly
+            />
+          ) : (
+            <GStyledIconLoadingButton textColor={theme.palette.common.white} bgColor={theme.palette.howick.midBlue} onClick={handleUserInvite} gap={2}>
+              <Icon icon={ICON_NAME.MAIL_USER} sx={{ width: 15, height: 15, p: 0 }}/> &nbsp; {t('send_invite.label').toUpperCase()}
+            </GStyledIconLoadingButton>
+          )
+        )}
         {defaultValues?.isActive ? (
          <IconTooltip
           title={t('active.label')}
           icon={ICON_NAME.ACTIVE}
           color={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
           tooltipColor={themeMode === KEY.LIGHT ? theme.palette.burnIn.altDark : theme.palette.burnIn.main}
-          isActiveIcon
+          tooltipTextColor={themeMode === KEY.LIGHT ? theme.palette.common.white : theme.palette.common.black}
           iconOnly
          />
         ) : (
-         <IconTooltip title={t('nactive.label')} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} iconOnly />
+         <IconTooltip title={t('inactive.label')} icon={ICON_NAME.INACTIVE} color={theme.palette.error.dark} iconOnly />
         )}
        </GStyledSpanBox>
       </Grid>
@@ -97,7 +113,10 @@ CommonFieldsCard.propTypes = {
  handleDialog: PropTypes.func,
  isLoading: PropTypes.bool,
  isChildren: PropTypes.bool,
- withStatusIcon: PropTypes.bool
+ withStatusIcon: PropTypes.bool,
+ isContactsPage: PropTypes.bool,
+ contactHasActiveUser: PropTypes.bool,
+ handleUserInvite: PropTypes.func
 }
 
 export default CommonFieldsCard
