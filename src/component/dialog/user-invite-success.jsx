81575f004f1264a8f5270f16fabe1e838a92d581
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-21 15:43:32 +1300

    HPS-1632 [ui]: enhance UserInviteSuccessDialog with role selection and user data fallback

--- src/component/dialog/user-invite-success.jsx
+++ src/component/dialog/user-invite-success.jsx
@@ -1,21 +1,24 @@
-import { Fragment, memo } from 'react'
+import { Fragment, memo, useState } from 'react'
 import PropTypes from 'prop-types'
 import { t } from 'i18next'
 import { useNavigate } from 'react-router-dom'
 import { dispatch, useSelector } from 'store'
 import { useSettingContext, Icon, ICON_NAME } from 'hook'
-import { setUserInviteDialog } from 'store/slice'
+import { setUserInviteDialog, setUserInviteContactDetails } from 'store/slice'
 import { PATH_SECURITY } from 'route/path'
-import { useMediaQuery, Grid, Dialog, DialogContent, DialogTitle, DialogActions, Divider, Typography, Avatar, Stack, Chip, Box } from '@mui/material'
+import { useMediaQuery, Grid, Dialog, DialogContent, DialogTitle, DialogActions, Divider, Typography, Avatar, Stack, Chip, Box, Autocomplete, TextField } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-import { GStyledTopBorderDivider, GStyledCloseButton, GStyledLoadingButton, GBackdropPropsOption } from 'theme/style'
+import { GStyledTopBorderDivider, GStyledCloseButton, GStyledLoadingButton, GBackdropPropsOption, GStyledFieldChip } from 'theme/style'
 import { PATH_AFTER_LOGIN } from 'config/global'
 import { TEXT_SIZE } from 'config/layout'
 import { TYPOGRAPHY, FLEX, KEY, FLEX_DIR } from 'constant'
 import { roleCoverUp } from 'util'
 
-const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitSuccessful }) => {
+const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitSuccessful, userData, enableRoleDropdown, title }) => {
  const { userInviteConfirmDetails, userInviteDialog } = useSelector(state => state.user)
+ const { customerRoles } = useSelector(state => state.role)
+ const { customer } = useSelector(state => state.customer)
+ const [role, setRole] = useState([])
  const { themeMode } = useSettingContext()
  const theme = useTheme()
  const navigate = useNavigate()
@@ -38,16 +41,15 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
   navigate(url)
  }
 
+ const handleUpdateUserInviteContactDetails = async (event, newValue) => {
+  await dispatch(setUserInviteContactDetails({ ...userData, roles: role }))
+  console.log(role)
+ }
+
  return (
-  <Dialog disableEnforceFocus maxWidth={KEY.LG} open={userInviteDialog} onClose={handleDialog} BackdropProps={GBackdropPropsOption(themeMode)}>
+  <Dialog disableEnforceFocus open={userInviteDialog} onClose={handleDialog} BackdropProps={GBackdropPropsOption(themeMode)}>
    <GStyledTopBorderDivider mode={themeMode} />
-   <DialogTitle
-    sx={{
-     ...(isDesktop && { minWidth: 500 }),
-     width: '100%',
-     boxSizing: 'border-box',
-     padding: theme.spacing(2)
-    }}>
+   <DialogTitle sx={{ ...(isDesktop && { minWidth: 500 }), width: '100%', boxSizing: 'border-box', padding: theme.spacing(2) }}>
     <Grid container gap={2}>
      {isSubmitSuccessful ? (
       <Grid container display={FLEX.FLEX} justifyContent={FLEX.CENTER}>
@@ -61,7 +63,7 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
        </Avatar>
       </Grid>
      ) : (
-      <Typography variant={TEXT_SIZE.DIALOG_TITLE_VARIANT}>{t('confirm_user_details.label').toUpperCase()} &nbsp;</Typography>
+      <Typography variant={TEXT_SIZE.DIALOG_TITLE_VARIANT}>{title.toUpperCase() || t('confirm_user_details.label').toUpperCase()} &nbsp;</Typography>
      )}
     </Grid>
    </DialogTitle>
@@ -80,7 +82,7 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
           {t('organization_name.label')}
          </Typography>
          <Typography variant={TYPOGRAPHY.BODY1} sx={{ mb: 2 }}>
-          &nbsp;{userInviteConfirmDetails?.customer?.name}
+          &nbsp;{userInviteConfirmDetails?.customer?.name || customer?.name}
          </Typography>
         </Grid>
 
@@ -89,7 +91,7 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
           {t('contact_person_name.label')}
          </Typography>
          <Typography variant={TYPOGRAPHY.BODY1} sx={{ mb: 2 }}>
-          &nbsp;{userInviteConfirmDetails?.name}
+          &nbsp;{userInviteConfirmDetails?.name || userData?.name}
          </Typography>
         </Grid>
 
@@ -98,7 +100,7 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
           {t('email.label')}
          </Typography>
          <Typography variant={TYPOGRAPHY.BODY1} sx={{ mb: 2 }}>
-          &nbsp;{userInviteConfirmDetails?.email}
+          &nbsp;{userInviteConfirmDetails?.email || userData?.email}
          </Typography>
         </Grid>
 
@@ -108,21 +110,45 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
            {t('contact_number.label')}
           </Typography>
           <Typography variant={TYPOGRAPHY.BODY1} sx={{ mb: 2 }}>
-           &nbsp;{userInviteConfirmDetails.phone}
+           &nbsp;{userInviteConfirmDetails.phone || userData?.phone}
           </Typography>
          </Grid>
         )}
 
         <Grid item xs={12} md={6}>
-         <Typography color='text.secondary' variant={TYPOGRAPHY.OVERLINE0}>
-          {userInviteConfirmDetails?.roles?.length > 1 ? t('role.roles.label') : t('role.label')}
-         </Typography>
-         <Stack direction='row' spacing={1} sx={{ mt: 1, flexWrap: 'wrap', gap: 1 }}>
-          &nbsp;
-          {userInviteConfirmDetails?.roles?.map((role, index) => (
-           <Chip key={index} label={<Typography variant={TYPOGRAPHY.H6}>{roleCoverUp(role)}</Typography>} variant='outlined' size='small' sx={{ borderRadius: 0.2 }} />
-          ))}
-         </Stack>
+         {enableRoleDropdown ? (
+          <Autocomplete
+           multiple
+           id='tags-outlined'
+           options={customerRoles}
+           getOptionLabel={option => roleCoverUp(option)}
+           defaultValue={[]}
+           filterSelectedOptions
+           renderInput={params => <TextField {...params} label={t('role.roles.label')} placeholder={customerRoles?.length > role?.length ? t('role.placeholder') : ''} />}
+           renderTags={(value, getTagProps) =>
+            value.map((option, index) => {
+             const { key, ...other } = getTagProps({ index })
+             return <GStyledFieldChip key={option._id} mode={themeMode} label={roleCoverUp(option)} {...other} />
+            })
+           }
+           onChange={(event, newValue) => {
+            setRole(newValue)
+            handleUpdateUserInviteContactDetails(newValue)
+           }}
+          />
+         ) : (
+          <Fragment>
+           <Typography color='text.secondary' variant={TYPOGRAPHY.OVERLINE0}>
+            {userInviteConfirmDetails?.roles?.length > 1 ? t('role.roles.label') : t('role.label')}
+           </Typography>
+           <Stack direction='row' spacing={1} sx={{ mt: 1, flexWrap: 'wrap', gap: 1 }}>
+            &nbsp;
+            {userInviteConfirmDetails?.roles?.map((role, index) => (
+             <Chip key={index} label={<Typography variant={TYPOGRAPHY.H6}>{roleCoverUp(role)}</Typography>} variant='outlined' size='small' sx={{ borderRadius: 0.2 }} />
+            ))}
+           </Stack>
+          </Fragment>
+         )}
         </Grid>
 
         <Grid item xs={12} md={6}>
@@ -130,10 +156,9 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
           {t('enable_portal_access.label')}
          </Typography>
          <Typography variant={TYPOGRAPHY.BODY1} sx={{ mb: 2 }}>
-          &nbsp;{userInviteConfirmDetails?.isInvite ? t('yes.label') : t('no.label')}
+          &nbsp;{userInviteConfirmDetails?.isInvite || userData?.isInvite ? t('yes.label') : t('no.label')}
          </Typography>
         </Grid>
-
        </Grid>
       </Grid>
      </Grid>
@@ -158,7 +183,7 @@ const UserInviteSuccessDialog = ({ action, onConfirm, setIsConfirming, isSubmitS
       <GStyledCloseButton icon={ICON_NAME.CHEVRON_RIGHT} onClick={handleDialog}>
        {t('cancel.label').toUpperCase()}
       </GStyledCloseButton>
-      <Box onClick={onConfirm}>{action}</Box>
+      <Box onClick={() => onConfirm()}>{action}</Box>
      </Fragment>
     )}
    </DialogActions>
@@ -171,7 +196,10 @@ UserInviteSuccessDialog.propTypes = {
  action: PropTypes.node,
  onConfirm: PropTypes.func,
  setIsConfirming: PropTypes.func,
- isSubmitSuccessful: PropTypes.bool
+ isSubmitSuccessful: PropTypes.bool,
+ userData: PropTypes.object,
+ enableRoleDropdown: PropTypes.bool,
+ title: PropTypes.string
 }
 
 export default memo(UserInviteSuccessDialog)
