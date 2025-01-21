02c0d1f8f331cbb66c9eb2e68628e97ec36a671d
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-22 11:21:19 +1300

    HPS-1680 [feat]: add option to add new contacts from user invite form and improve form validation

--- src/section/auth/user-invite/user-invite-form.jsx
+++ src/section/auth/user-invite/user-invite-form.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useCallback, Fragment } from 'react'
-import _ from 'lodash'
+import _, { add } from 'lodash'
 import debounce from 'lodash.debounce'
 import { t } from 'i18next'
 import { useSelector } from 'react-redux'
@@ -10,6 +10,7 @@ import { dispatch } from 'store'
 import { useForm } from 'react-hook-form'
 import {
  addAndInviteSecurityUser,
+ addContactFromUserInvite,
  getCustomer,
  getSecurityUsers,
  getActiveContacts,
@@ -23,7 +24,7 @@ import {
 } from 'store/slice'
 import { UserInviteSchema } from 'schema'
 import { useUserInviteDefaultValues } from 'section/auth'
-import { Grid, Box, Checkbox, Typography } from '@mui/material'
+import { Grid, Box, Checkbox, Typography, FormControlLabel } from '@mui/material'
 import { RHFRequiredTextFieldWrapper, UserInviteSuccessDialog } from 'component'
 import FormProvider, { RHFTextField, RHFAutocomplete, RHFPhoneInput, RHFCheckbox } from 'component/hook-form'
 import { GStyledLoadingButton, GStyledSpanBox, GStyledFieldChip } from 'theme/style'
@@ -46,25 +47,24 @@ const FORM_EL = {
 function UserInviteForm() {
  const [isFormComplete, setIsFormComplete] = useState(false)
  const [isSuccessState, setIsSuccessState] = useState(false)
- const [isConfirming, setIsConfirming]     = useState(false)
- const {
-        customer,
-        activeContacts,
-        securityUserTotalCount,
-        customerRoles,
-        userInviteDialog } = useSelector(state => ({
-                                                    customer              : state.customer.customer,
-                                                    activeContacts        : state.contact.activeContacts,
-                                                    securityUserTotalCount: state.user.securityUserTotalCount,
-                                                    userInviteDialog      : state.user.userInviteDialog,
-                                                    customerRoles         : state.role.customerRoles
-                                                    }), _.isEqual)
+ const [isConfirming, setIsConfirming] = useState(false)
+ const [addAsContact, setAddAsContact] = useState(false)
+ const { customer, activeContacts, securityUserTotalCount, customerRoles, userInviteDialog } = useSelector(
+  state => ({
+   customer: state.customer.customer,
+   activeContacts: state.contact.activeContacts,
+   securityUserTotalCount: state.user.securityUserTotalCount,
+   userInviteDialog: state.user.userInviteDialog,
+   customerRoles: state.role.customerRoles
+  }),
+  _.isEqual
+ )
 
- const { user }      = useAuthContext()
+ const { user } = useAuthContext()
  const { themeMode } = useSettingContext()
- const regEx         = new RegExp(REGEX.ERROR_CODE)
- const isMobile      = useResponsive('down', 'sm')
- const countryCode   = getCountryCode(customer?.mainSite?.address?.country) || KEY.DEFAULT_COUNTRY_CODE
+ const regEx = new RegExp(REGEX.ERROR_CODE)
+ const isMobile = useResponsive('down', 'sm')
+ const countryCode = getCountryCode(customer?.mainSite?.address?.country) || KEY.DEFAULT_COUNTRY_CODE
 
  const fetchCustomer = useCallback(() => {
   if (user?.customer && !customer) {
@@ -118,9 +118,10 @@ function UserInviteForm() {
  } = methods
 
  const { email, contact, name, roles, phone, isInvite } = watch()
+ const isNewContact = contact || addAsContact === true
 
  const checkFormCompletion = useCallback(() => {
-  setIsFormComplete(!!name && !!REGEX.EMAIL.test(email) && !!contact && roles.length > 0)
+  setIsFormComplete((!!name && !!REGEX.EMAIL.test(email) && !!contact) || (addAsContact && roles.length > 0))
  }, [name, email, contact, roles])
 
  useEffect(() => {
@@ -139,7 +140,6 @@ function UserInviteForm() {
   }
  }, [checkFormCompletion])
 
-
  const handleConfirmation = async () => {
   setIsConfirming(true)
   await Promise.all([dispatch(setUserInviteConfirmDetails({ customer, name, email, phone, roles, isInvite })), dispatch(setUserInviteDialog(true))])
@@ -155,38 +155,35 @@ function UserInviteForm() {
  const handleSubmissionError = error => {
   if (error?.errors) {
    error.errors.forEach(({ field, message }) => {
-    setError(field, {
-     type: 'validation',
-     message
-    })
-   })
+    setError(field, { type: 'validation', message })})
    snack(t('responses.error.form_check_errors'), { variant: COLOR.ERROR })
    return
   }
   if (error?.MessageCode && regEx.test(error.MessageCode)) {
    snack('Error in sending form', { variant: COLOR.ERROR })
-   setError(LOCAL_STORAGE_KEY.AFTER_SUBMIT, {
-    type: 'submission',
-    message: error.MessageCode
-   })
+   setError(LOCAL_STORAGE_KEY.AFTER_SUBMIT, { type: 'submission', message: error.MessageCode })
    return
   }
   console.error('Unexpected error:', error)
   snack(t('responses.error.unable_to_process_request'), { variant: COLOR.ERROR })
-  setError(LOCAL_STORAGE_KEY.AFTER_SUBMIT, {
-   type: 'unexpected',
-   message: error?.message || t('responses.error.unexpected_error')
-  })
+  setError(LOCAL_STORAGE_KEY.AFTER_SUBMIT, { type: 'unexpected', message: error?.message || t('responses.error.unexpected_error') })
  }
 
  const onSubmit = async data => {
   try {
    setIsSuccessState(true)
    setIsConfirming(false)
-   const Data = {
-    ...data,
-    customer
+   const Data = {...data, customer}
+
+   if (addAsContact) {
+       await dispatch(addContactFromUserInvite(customer?._id, Data))
    }
+
+   const newContact = activeContacts.find(activeContact => activeContact?.email === Data?.email)
+   if (!newContact) throw new Error('Contact not found, unable to proceed')
+
+   Data.contact = newContact?._id
+
    const response = await dispatch(addAndInviteSecurityUser(Data))
    await delay(2000)
    if (REGEX.SUCCESS_CODE.test(response.status)) {
@@ -221,18 +218,20 @@ function UserInviteForm() {
    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
     <Grid container my={4} direction={{ xs: 'column', md: 'row' }} flex={1} rowSpacing={4} gridAutoFlow={isMobile ? FLEX_DIR.COLUMN : FLEX_DIR.ROW} columnSpacing={2}>
      <Grid item xs={12} sm={6} md={6}>
-      <RHFRequiredTextFieldWrapper condition={!contact}>
+      <RHFRequiredTextFieldWrapper condition={!isNewContact}>
        <RHFAutocomplete
         name={FORM_EL.contact}
-        label={t('contact.label')}
+        label={!addAsContact ? t('contact.label') : name ? `${t('new_contact.label')}:  ${name}` : t('new_contact.label')}
         options={activeContacts}
-        getOptionLabel={option => `${option?.firstName || ''} ${option?.lastName || ''}`}
+        getOptionLabel={option => (!addAsContact ? `${option?.firstName || ''} ${option?.lastName || ''}` : name || '')}
         isOptionEqualToValue={(option, value) => option?._id === value?._id}
         renderOption={(props, option) => (
          <li {...props} key={option?._id}>
           {option?.firstName || ''} {option?.lastName || ''}
          </li>
         )}
+        helperText={errors.contact ? errors.contact.message : t('new_contact.helper_text')}
+        disabled={addAsContact}
        />
       </RHFRequiredTextFieldWrapper>
      </Grid>
@@ -306,10 +305,15 @@ function UserInviteForm() {
       />
      </Grid>
     </Grid>
-    <Grid container direction={{ xs: FLEX_DIR.COLUMN, md: FLEX_DIR.ROW }} justifyContent={FLEX.FLEX_END} sx={{my:5}}>
+    <Grid container direction={{ xs: FLEX_DIR.COLUMN, md: FLEX_DIR.ROW }} justifyContent={FLEX.FLEX_END} sx={{ my: 5 }}>
      <Grid item xs={12} sm={4} md={2} display={FLEX.FLEX} justifyContent={FLEX.FLEX_END}>
       <RHFCheckbox name={FORM_EL.isInvite} label={t('enable_portal_access.label')} error={!!errors.isInvite} helperText={errors.isInvite ? errors.isInvite.message : ''} />
      </Grid>
+     {!contact && (
+      <Grid item xs={12} sm={4} md={2} display={FLEX.FLEX} justifyContent={FLEX.FLEX_END}>
+       <FormControlLabel value={addAsContact} control={<Checkbox />} label={'Add as contact'} labelPlacement='end' onChange={() => setAddAsContact(!addAsContact)} />
+      </Grid>
+     )}
     </Grid>
     <Grid container direction={{ xs: 'column', md: 'row' }} justifyContent={KEY.CENTER}>
      <Grid item xs={12} sm={4} md={4}>
@@ -333,9 +337,10 @@ function UserInviteForm() {
      <UserInviteSuccessDialog
       setIsConfirming={setIsConfirming}
       isSubmitSuccessful={isSubmitSuccessful}
+      addAsContact={addAsContact}
       onConfirm={handleSubmit(onSubmit)}
       action={
-       <GStyledLoadingButton isLoading={isSubmitting} color={KEY.INHERIT} type={KEY.SUBMIT} loading={isSubmitting} variant={KEY.CONTAINED} mode={themeMode}>
+       <GStyledLoadingButton color={KEY.INHERIT} type={KEY.SUBMIT} loading={isSubmitting} variant={KEY.CONTAINED} mode={themeMode}>
         {t('confirm.label').toUpperCase()}
        </GStyledLoadingButton>
       }
