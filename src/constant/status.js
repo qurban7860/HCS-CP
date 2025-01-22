34d3c4ccaf22da359f4d4f10a4cf2e2ecc8dd4a7
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-22 15:37:17 +1300

    [constants]: improve formatting and add invitation status constants

--- src/constant/status.js
+++ src/constant/status.js
@@ -1,31 +1,40 @@
 export const STATUS_BASE = {
- ACTIVE: 'active',
+ ACTIVE  : 'active',
  INACTIVE: 'inactive'
 }
 
 export const STATUS = {
- PURCHASED: 'purchased',
+ PURCHASED     : 'purchased',
  DE_COMMISIONED: 'decommissioned',
- COMMISSIONED: 'commissioned',
- FREIGHT: 'freight',
- SHIPPED: 'shipped',
- INSTALLED: 'installed',
- TRANSFERRED: 'transferred',
- STATUS: 'status'
+ COMMISSIONED  : 'commissioned',
+ FREIGHT       : 'freight',
+ SHIPPED       : 'shipped',
+ INSTALLED     : 'installed',
+ TRANSFERRED   : 'transferred',
+ STATUS        : 'status'
 }
 
 export const SUPPORT_STATUS = {
- OPEN: 'open',
- IN_PROGRESS: 'in progress',
+ OPEN                : 'open',
+ IN_PROGRESS         : 'in progress',
  WAITING_FOR_CUSTOMER: 'waiting for customer',
- WAITING_FOR_SUPPORT: 'waiting for support',
- UNDER_REVIEW: 'under review',
- UNDER_INVESTIGATION: 'under investigation',
- PENDING: 'pending',
- CLOSED: 'closed',
- RESOLVED: 'resolved',
- CANCELLED: 'canceled', // wrong spelling
- COMPLETED: 'completed'
+ WAITING_FOR_SUPPORT : 'waiting for support',
+ UNDER_REVIEW        : 'under review',
+ UNDER_INVESTIGATION : 'under investigation',
+ PENDING             : 'pending',
+ CLOSED              : 'closed',
+ RESOLVED            : 'resolved',
+ CANCELLED           : 'canceled',               // wrong spelling
+ COMPLETED           : 'completed'
+}
+
+export const INVITATION_STATUS = {
+ PENDING  : "PENDING",
+ APPROVED : "APPROVED",
+ REJECTED : "REJECTED",
+ CANCELLED: "CANCELLED",
+ "EXPIRED": "EXPIRED",
+ "NEW"    : "NEW",
 }
 
 export const COMPLETED_STATUSES = [SUPPORT_STATUS.CLOSED, SUPPORT_STATUS.RESOLVED, SUPPORT_STATUS.CANCELLED, SUPPORT_STATUS.COMPLETED]
