017477049f7b574780e76c12f8cb7a0c66e2ca41
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-24 08:02:11 +1300

    🔥 [hotfix]: update snack component styles for improved appearance and consistency

--- src/hook/use-snack/style.jsx
+++ src/hook/use-snack/style.jsx
@@ -1,20 +1,20 @@
 import { m } from 'framer-motion'
 import { styled } from '@mui/material/styles'
 import { MaterialDesignContent } from 'notistack'
+import { ICON } from 'config/layout'
 
 export const StyledSnackContent = styled(MaterialDesignContent)(({ theme }) => ({
   marginLeft: theme.spacing(2),
   '&.notistack-MuiContent': {
-    backgroundColor: theme.palette.common.white,
+    backgroundColor: theme.palette.grey[300],
     padding: theme.spacing(1),
     display: 'flex',
     flexDirection: 'row',
     boxShadow: 'none',
     objectFit: 'cover',
     justifyContent: 'space-between',
-    fontSize: theme.typography.h4.fontSize,
-    fontWeight: theme.typography.fontWeightBold,
-    color: theme.palette.grey[800],
+    fontSize: theme.typography.body1.fontSize,
+    color: theme.palette.grey[900],
     // padding horizontal
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(1),
@@ -48,6 +48,5 @@ export const StyledSnackIconMDiv = styled(m.div)(({ theme, color }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  width: 30,
-  height: 30
+  ...ICON.SIZE_XS,
 }))
--- src/hook/use-snack/use-snack.jsx
+++ src/hook/use-snack/use-snack.jsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react'
 import { SnackbarProvider as NotistackProvider, useSnackbar } from 'notistack'
 import { Slide, IconButton } from '@mui/material'
 import { Icon, ICON_NAME } from 'hook'
+import { ICON } from 'config/layout'
 import { KEY } from 'constant'
 import { StyledSnackContent, StyledSnackIconMDiv } from './style'
 
@@ -33,11 +34,12 @@ export default function SnackProvider({ children }) {
      error: StyledSnackContent,
      info: StyledSnackContent
     }}
+    // TODO: clean this up; use the SnackIcon component instead
     iconVariant={{
-     info: <Icon icon={ICON_NAME.INFO} color='howick.orange' />,
-     success: <Icon icon={ICON_NAME.CHECK_CIRCLE} color={'burnIn.altDark'} />,
-     warning: <Icon icon={ICON_NAME.WARNING} color='warning.main' />,
-     error: <Icon icon={ICON_NAME.ALERT_OCTAGON} color='common.white' />
+     info: <Icon icon={ICON_NAME.INFO} color='howick.orange' sx={{ marginRight: 1, ...ICON.SIZE_XS }} />,
+     success: <Icon icon={ICON_NAME.CHECK_CIRCLE} color={'burnIn.altDark'} sx={{ marginRight: 1,  ...ICON.SIZE_XS }} />,
+     warning: <Icon icon={ICON_NAME.WARNING} color='warning.main' sx={{ marginRight: 1, ...ICON.SIZE_XS }} />,
+     error: <Icon icon={ICON_NAME.ALERT_OCTAGON} color='common.white' sx={{ marginRight: 1, ...ICON.SIZE_XS }} />
     }}
     action={key => (
      <IconButton size='small' onClick={onClose(key)}>
@@ -58,7 +60,7 @@ SnackIcon.propTypes = {
 function SnackIcon({ icon, color }) {
  return (
   <StyledSnackIconMDiv initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.3 }}>
-   <Icon icon={icon} color={color} width={18} />
+   <Icon icon={icon} color={color} sx={{ ...ICON.SIZE_XS }} />
   </StyledSnackIconMDiv>
  )
 }
