44b3b7ed6b4e9f67696dc85c397657f73b6d331c
ballyalley-o <bally.lomibao@howickltd.com>
2025-01-31 10:00:08 +1300

    HPS-1704 [fix]: update FallbackButton to redirect to home while machine logs are restructured

--- src/section/fallback/fallback-button.jsx
+++ src/section/fallback/fallback-button.jsx
@@ -1,16 +1,17 @@
 import { t } from 'i18next'
 import { useSettingContext } from 'hook'
+import { PATH_HOME } from 'route/path'
 import { useTheme } from '@mui/material'
 import { GStyledDefLoadingButton } from 'theme/style'
 import { KEY, SIZE } from 'constant'
-import { PATH_DASHBOARD } from 'route/path'
 
 const FallbackButton = () => {
  const { themeMode } = useSettingContext()
  const theme = useTheme()
  return (
   <GStyledDefLoadingButton
-   onClick={() => window.open(PATH_DASHBOARD.general.app, '_self')}
+   // HPS-1704: redir to home for now while machine logs is being restructured; then change it to dashboard/app
+   onClick={() => window.open(PATH_HOME.root, '_self')}
    size={SIZE.MEDIUM}
    bgColor={themeMode === KEY.LIGHT ? theme.palette.howick.bronze : theme.palette.howick.orange}
    textColor={themeMode === KEY.LIGHT ? theme.palette.common.white : theme.palette.common.black}>
