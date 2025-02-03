54530af2c0d5e1658d9c1eee81530f3fc3ab1770
ballyalley-o <bally.lomibao@howickltd.com>
2025-02-03 14:46:12 +1300

    HPS-1698 [feat]: replace UserInviteForm with TicketCreateForm in ticket creation layout

--- src/section/support/ticket/create/ticket-create.jsx
+++ src/section/support/ticket/create/ticket-create.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from 'react'
 import { t } from 'i18next'
-import { UserInviteForm } from 'section/auth/user-invite'
+import { TicketCreateForm } from 'section/support/ticket'
 import { TableTitleBox } from 'component'
 
 export default function TicketCreateLayout() {
  return (
   <Fragment>
    <TableTitleBox title={t('create_ticket.label')} />
-   <UserInviteForm />
+   <TicketCreateForm />
   </Fragment>
  )
 }
