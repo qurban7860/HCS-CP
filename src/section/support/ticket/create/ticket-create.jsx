51cb7f62825bc7c27b7299024baab9e47565f3bf
ballyalley-o <bally.lomibao@howickltd.com>
2025-02-05 11:57:08 +1300

    HPS-1720 [refax]: replace TableTitleBox with TableStickyTitleBox in TicketCreateLayout

--- src/section/support/ticket/create/ticket-create.jsx
+++ src/section/support/ticket/create/ticket-create.jsx
@@ -1,12 +1,12 @@
 import { Fragment } from 'react'
 import { t } from 'i18next'
 import { TicketCreateForm } from 'section/support/ticket'
-import { TableTitleBox } from 'component'
+import { TableStickyTitleBox } from 'component'
 
 export default function TicketCreateLayout() {
  return (
   <Fragment>
-   <TableTitleBox title={t('create_ticket.label')} />
+   <TableStickyTitleBox title={t('create_ticket.label')} />
    <TicketCreateForm />
   </Fragment>
  )
