e256cafcbc1ae0d9c0e1444d2b4e4b3828a35129
ballyalley-o <bally.lomibao@howickltd.com>
2025-02-11 13:40:24 +1300

    HPS-1727 🦄 [refax] : refactor TicketsTable component to improve readability and streamline rendering logic

--- src/section/support/tickets/tickets-table.jsx
+++ src/section/support/tickets/tickets-table.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import { ICON_NAME } from 'hook'
+import { IconFlexi, ICON_NAME } from 'hook'
 import { useSettingContext } from 'hook'
 import { TableBody, TableCell, Typography } from '@mui/material'
 import { GStyledSupportStatusFieldChip } from 'theme/style'
@@ -10,23 +10,22 @@ import { LABEL, TYPOGRAPHY, SIZE } from 'constant'
 import { normalizer } from 'util'
 import { StyledTableRow } from './style'
 
-const TicketsTable = ({ columns, onViewRow, ticket, mode, index, handleCustomerTicket }) => {
+const TicketsTable = ({ columns, onViewRow, ticket, mode, index  }) => {
  const { themeMode } = useSettingContext()
- const lowercaseRow = {}
-
- const renderStatus = fields => {
+ const renderStatus = _status => {
   return (
-   <GStyledSupportStatusFieldChip status={normalizer(fields?.status?.name)} mode={themeMode} label={<Typography variant={TYPOGRAPHY.OVERLINE2}>{fields?.status?.name}</Typography>} size={SIZE.SMALL} />
+   <GStyledSupportStatusFieldChip status={normalizer(_status?.name)} mode={themeMode} label={<Typography variant={TYPOGRAPHY.OVERLINE2}>{_status?.name}</Typography>} size={SIZE.SMALL} />
   )
  }
 
- const renderKey = (key, fields) => {
+ const renderPriority = _priority => {
+ return ( <IconFlexi icon={_priority?.icon} color={_priority?.color} />)
+ }
+ const renderKey = (key) => {
   return (
    <LinkWrap
     param={key}
-    onClick={e => {
-     handleCustomerTicket(e, fields?.customfield_10069, key)
-    }}
+    onClick={e => {}}
     // disabled for now; enable once Jira auth is refactored to take customer based token #1629
     // openInNewTab={() => openInNewPage(key)}
     tooltipTitle={LABEL.VIEW_IN_JIRA}
@@ -35,23 +34,18 @@ const TicketsTable = ({ columns, onViewRow, ticket, mode, index, handleCustomerT
   )
  }
 
- Object.entries(ticket).forEach(([key, value]) => {
-  if (typeof key === 'string') lowercaseRow[key.toLocaleLowerCase()] = value
- })
-
  const openInNewPage = jiraKey => {
   const url = GLOBAL.JIRA_URL + jiraKey
   window.open(url, '_blank')
  }
 
- const { fields, key, id, self } = ticket
+ const { ticketNo,  status, priority } = ticket
 
  return (
   <Fragment>
    <TableBody>
-    <StyledTableRow index={index} mode={mode} fields={fields} key={key}>
+    <StyledTableRow index={index} mode={mode} fields={ticket}>
      {columns?.map((column, index) => {
-      const cellValue = lowercaseRow?.[column.id.toLocaleLowerCase()] || ''
       return (
        <TableCell
         key={index}
@@ -60,8 +54,8 @@ const TicketsTable = ({ columns, onViewRow, ticket, mode, index, handleCustomerT
          cursor: 'pointer',
          '&:hover': { transform: 'scale(0.97)', transition: 'ease-in-out 0.2s' }
         }}
-        align={column?.numerical ? 'right' : 'left'}>
-        {column.id === 'key' ? renderKey(key, fields) : column.id === 'fields.status.statusCategory.name' ? renderStatus(fields) : column?.value ? column?.value(fields) : cellValue}
+        align={column?.align ? column.align : 'left'}>
+        {column.id === 'ticketNo' ? renderKey(ticketNo) : column.id === 'status.name' ? renderStatus(status)  :  column.id === 'priority.name' ? renderPriority(priority) : column?.value(ticket)}
        </TableCell>
       )
      })}
@@ -72,12 +66,12 @@ const TicketsTable = ({ columns, onViewRow, ticket, mode, index, handleCustomerT
 }
 
 TicketsTable.propTypes = {
- ticket: PropTypes.object,
- mode: PropTypes.string,
- index: PropTypes.number,
+ ticket              : PropTypes.object,
+ mode                : PropTypes.string,
+ index               : PropTypes.number,
  handleCustomerTicket: PropTypes.func,
- onViewRow: PropTypes.func,
- columns: PropTypes.array
+ onViewRow           : PropTypes.func,
+ columns             : PropTypes.array
 }
 
 export default TicketsTable
