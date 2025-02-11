6bb9a26fe9708c84bc77fb329c3875e00cc7582f
ballyalley-o <bally.lomibao@howickltd.com>
2025-02-11 13:31:22 +1300

    HPS-1697 [constants] : update SUPPORT_STATUS to reflect new status values and correct spelling

--- src/constant/status.js
+++ src/constant/status.js
@@ -15,17 +15,10 @@ export const STATUS = {
 }
 
 export const SUPPORT_STATUS = {
- OPEN                : 'open',
- IN_PROGRESS         : 'in progress',
- WAITING_FOR_CUSTOMER: 'waiting for customer',
- WAITING_FOR_SUPPORT : 'waiting for support',
- UNDER_REVIEW        : 'under review',
- UNDER_INVESTIGATION : 'under investigation',
- PENDING             : 'pending',
- CLOSED              : 'closed',
- RESOLVED            : 'resolved',
- CANCELLED           : 'canceled',               // wrong spelling
- COMPLETED           : 'completed'
+ TO_DO      : 'to do',
+ IN_PROGRESS: 'in progress',
+ CANCELLED  : 'canceled',      // wrong spelling
+ DONE       : 'done'
 }
 
 export const INVITATION_STATUS = {
