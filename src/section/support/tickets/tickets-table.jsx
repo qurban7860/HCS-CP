8559280be9e2c33ca5d4e3a5218556358b86799c
ballyalley-o <bally.lomibao@howickltd.com>
2025-02-13 10:43:42 +1300

    HPS-1697 HPS-1727 🦄 [refax]: refactor tickets table

--- src/section/support/tickets/tickets-table.jsx
+++ src/section/support/tickets/tickets-table.jsx
@@ -1,45 +1,61 @@
 import { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import { IconFlexi, ICON_NAME } from 'hook'
-import { useSettingContext } from 'hook'
+import { t } from 'i18next'
+import { useNavigate } from 'react-router-dom'
+import { IconFlexi, ICON_NAME, useSettingContext } from 'hook'
+import { PATH_SUPPORT } from 'route/path'
 import { TableBody, TableCell, Typography } from '@mui/material'
 import { GStyledSupportStatusFieldChip } from 'theme/style'
 import { LinkWrap } from 'component'
 import { GLOBAL } from 'config/global'
-import { LABEL, TYPOGRAPHY, SIZE } from 'constant'
+import { TYPOGRAPHY, SIZE } from 'constant'
 import { normalizer } from 'util'
+
 import { StyledTableRow } from './style'
 
 const TicketsTable = ({ columns, onViewRow, ticket, mode, index  }) => {
- const { themeMode } = useSettingContext()
- const renderStatus = _status => {
+ const { themeMode }  = useSettingContext()
+ const navigate       = useNavigate()
+
+ const renderStatus  = _status => {
   return (
    <GStyledSupportStatusFieldChip status={normalizer(_status?.name)} mode={themeMode} label={<Typography variant={TYPOGRAPHY.OVERLINE2}>{_status?.name}</Typography>} size={SIZE.SMALL} />
   )
  }
 
  const renderPriority = _priority => {
- return ( <IconFlexi icon={_priority?.icon} color={_priority?.color} />)
+   return <IconFlexi icon={_priority?.icon} color={_priority?.color} />
  }
- const renderKey = (key) => {
+
+ const renderIssueType = _issueType => {
+   return <IconFlexi icon={_issueType?.icon}  color={_issueType.color} />
+ }
+
+const openInNewPage = id => {
+   const url = PATH_SUPPORT.tickets.view(id)
+   window.open(url, '_blank')
+}
+
+const handleNavigateTicket = (id) => {
+  navigate(PATH_SUPPORT.tickets.view(id))
+}
+
+ const renderTicketNo = (_tix, id) => {
   return (
    <LinkWrap
-    param={key}
-    onClick={e => {}}
+    param={_tix}
+    onClick={() => handleNavigateTicket(id)}
     // disabled for now; enable once Jira auth is refactored to take customer based token #1629
-    // openInNewTab={() => openInNewPage(key)}
-    tooltipTitle={LABEL.VIEW_IN_JIRA}
-    icon={ICON_NAME.JIRA}
+    openInNewTab={() => openInNewPage(id)}
+    tooltipTitle={t('open_in_new_tab.label', { value: `${GLOBAL.PREFIX}-${_tix}`})}
+    icon={ICON_NAME.OPEN_IN_NEW}
    />
   )
  }
 
- const openInNewPage = jiraKey => {
-  const url = GLOBAL.JIRA_URL + jiraKey
-  window.open(url, '_blank')
- }
 
- const { ticketNo,  status, priority } = ticket
+
+ const { ticketNo,  status, priority, issueType } = ticket
 
  return (
   <Fragment>
@@ -55,7 +71,7 @@ const TicketsTable = ({ columns, onViewRow, ticket, mode, index  }) => {
          '&:hover': { transform: 'scale(0.97)', transition: 'ease-in-out 0.2s' }
         }}
         align={column?.align ? column.align : 'left'}>
-        {column.id === 'ticketNo' ? renderKey(ticketNo) : column.id === 'status.name' ? renderStatus(status)  :  column.id === 'priority.name' ? renderPriority(priority) : column?.value(ticket)}
+        {column.id === 'issueType.name' ? renderIssueType(issueType) :   column.id === 'ticketNo' ? renderTicketNo(ticketNo, ticket?._id) : column.id === 'status.name' ? renderStatus(status)  :  column.id === 'priority.name' ? renderPriority(priority) : column?.value(ticket)}
        </TableCell>
       )
      })}
