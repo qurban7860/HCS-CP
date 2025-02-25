b00fe31ad1a45dae5e240fb3713012dda0fd3ccf
ballyalley-o <bally.lomibao@howickltd.com>
2025-02-26 10:24:59 +1300

    [perf-cleanup]

--- src/section/support/ticket/view/ticket-view.jsx
+++ src/section/support/ticket/view/ticket-view.jsx
@@ -1,29 +1,12 @@
-import { Fragment, useEffect } from 'react'
-import _ from 'lodash'
-import { useParams } from 'react-router-dom'
-import { useAuthContext } from 'auth'
-import { dispatch, useSelector } from 'store'
-import { getTicket } from 'store/slice'
+import { Fragment } from 'react'
+import { useSelector } from 'store'
 import { IconFlexi } from 'hook'
 import { TicketViewForm } from 'section/support/ticket'
-import { Box, Typography } from '@mui/material'
-import { TableStickyTitleBox, BackButton } from 'component'
-import { GStyledSpanBox } from 'theme/style'
+import { TableStickyTitleBox } from 'component'
 import { GLOBAL } from 'config/global'
-import { FLEX, TYPOGRAPHY } from 'constant'
 
 export default function TicketViewLayout() {
- const { user }   = useAuthContext()
  const { ticket } = useSelector(state => state.ticket)
- const { id }     = useParams()
-
- useEffect(() => {
-  const debouncedFetch = _.debounce(() => {
-   dispatch(getTicket(id, user?.customer))
-  }, 300)
-  debouncedFetch()
-  return () => debouncedFetch.cancel()
- }, [dispatch])
 
  return (
   <Fragment>
